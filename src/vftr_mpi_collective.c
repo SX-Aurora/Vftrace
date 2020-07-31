c3178fa3b75923b3ea897114198364fd6332f414
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-07-31 08:14:15 +0200

    added intercom handling for MPI_Allgatherv

--- src/vftr_mpi_collective.c
+++ src/vftr_mpi_collective.c
@@ -100,8 +100,22 @@ int vftr_MPI_Allgatherv(const void *sendbuf, int sendcount,
       int isintercom;
       PMPI_Comm_test_inter(comm, &isintercom);
       if (isintercom) {
-         // TODO: handle intercom
-         ;
+         // Every process of group A sends sendcount data to and
+         // receives recvcounts[i] data from the i-th process in group B
+         int size;
+         PMPI_Comm_remote_size(comm, &size);
+         for (int i=0; i<size; i++) {
+         // translate the i-th rank in the remote group to the global rank
+         int global_peer_rank = vftr_remote2global_rank(comm, i);
+         // Store message info with MPI_COMM_WORLD as communicator
+         // to prevent additional (and thus faulty rank translation)
+         vftr_store_sync_message_info(send, sendcount, sendtype,
+                                      global_peer_rank, -1, MPI_COMM_WORLD,
+                                      tstart, tend);
+         vftr_store_sync_message_info(recv, recvcounts[i], recvtype,
+                                      global_peer_rank, -1, MPI_COMM_WORLD,
+                                      tstart, tend);
+         }
       } else {
          int size;
          PMPI_Comm_size(comm, &size);
