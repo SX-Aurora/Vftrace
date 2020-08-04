6f54d367036b981f019eadf33941ed3fda6ad39b
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-08-04 14:32:24 +0200

    Separated vftr_mpi_waitany c2F layer from testwait sources into standalone files

---
+++ src/vftr_mpi/vftr_waitany_f.h
@@ -0,0 +1,29 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef VFTR_WAITANY_F_H
+#define VFTR_WAITANY_F_H
+
+#ifdef _MPI
+#include <mpi.h>
+
+void vftr_MPI_Waitany_F(MPI_Fint *f_count, MPI_Fint *f_array_of_requests,
+                        MPI_Fint *f_index, MPI_Fint *f_status, MPI_Fint *f_error);
+
+#endif
+#endif
