9113fe1711ade194e8d0e64b3eedc8e9ae443105
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-08-05 10:45:11 +0200

    Separated vftr_mpi_scatterv routine from collective sources into standalone files

---
+++ src/vftr_mpi/vftr_mpi_scatterv.h
@@ -0,0 +1,31 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef VFTR_MPI_SCATTERV_H
+#define VFTR_MPI_SCATTERV_H
+
+#ifdef _MPI
+#include <mpi.h>
+
+int vftr_MPI_Scatterv(const void *sendbuf, const int *sendcounts,
+                      const int *displs, MPI_Datatype sendtype,
+                      void *recvbuf, int recvcount, MPI_Datatype recvtype,
+                      int root, MPI_Comm comm);
+
+#endif
+#endif
