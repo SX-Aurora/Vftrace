6a33f815236c7cf628e68f7d39c1f5c81e023795
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-08-05 13:13:09 +0200

    Split of vftr_mpi_alltoallv_cwrap.c into a seperate source file

---
+++ src/vftr_mpi/c-wrapper/vftr_mpi_alltoallv_cwrap.c
@@ -0,0 +1,32 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifdef _MPI
+#include <mpi.h>
+
+#include "vftr_mpi_alltoallv.h"
+
+int MPI_Alltoallv(const void *sendbuf, const int *sendcounts,
+                  const int *sdispls, MPI_Datatype sendtype, void *recvbuf,
+                  const int *recvcounts, const int *rdispls,
+                  MPI_Datatype recvtype, MPI_Comm comm) {
+   return vftr_MPI_Alltoallv(sendbuf, sendcounts, sdispls, sendtype,
+                             recvbuf, recvcounts, rdispls, recvtype, comm);
+}
+
+#endif
