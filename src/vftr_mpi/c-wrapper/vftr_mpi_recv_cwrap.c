b349be6785e9dcb7e5bc2da8b2ae2e68f3531438
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-08-05 13:13:09 +0200

    Split of vftr_mpi_recv_cwrap.c into a seperate source file

---
+++ src/vftr_mpi/c-wrapper/vftr_mpi_recv_cwrap.c
@@ -0,0 +1,29 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifdef _MPI
+#include <mpi.h>
+
+#include "vftr_mpi_recv.h"
+
+int MPI_Recv(void *buf, int count, MPI_Datatype datatype,
+             int source, int tag, MPI_Comm comm, MPI_Status *status) {
+   return vftr_MPI_Recv(buf, count, datatype, source, tag, comm, status);
+}
+
+#endif
