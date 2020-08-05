1e70d8b96f4078c64f5253784653b6c05660ae50
MeisterEule <christian.weiss@emea.nec.com>
2020-08-05 08:59:45 +0200

    Finalize test outputs (setup_2 & filewrite_1)

---
+++ test/run/vftr_setup_test_2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+ref_out_dir=ref_output
+ref_in_dir=ref_input
+testname=vftr_setup_test_2
+outfile=$testname.out
+
+rm -f $outfile
+
+./test_vftrace $testname
+diff $ref_out_dir/$outfile $outfile
+
