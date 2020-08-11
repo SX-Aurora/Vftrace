1986a459fc28e31c07b1735ecc043381fb7e4600
Felix Uhl <uhl_felix@web.de>
2020-08-11 09:50:06 +0200

    renamed tests to prevent double execution of some scripts

---
+++ test/c-mpi/send_recv-1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+vftr_binary=send_recv
+nprocs=2
+nb=1024
+
+export VFTR_SAMPLING="Yes"
+export VFTR_MPI_LOG="Yes"
+export VFTR_PRECISE="MPI_*"
+
+mpirun -np ${nprocs} ./${vftr_binary} ${nb}
