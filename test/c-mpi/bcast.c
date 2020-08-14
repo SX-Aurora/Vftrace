e235c21fde1bfe9646cd722bc2039b40562ea5b0
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-08-14 09:53:33 +0200

    prettify

--- test/c-mpi/bcast.c
+++ test/c-mpi/bcast.c
@@ -14,7 +14,6 @@ int main(int argc, char** argv) {
    int my_rank;
    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-
    // require cmd-line argument
    if (argc < 2) {
       printf("./bcast <msgsize in ints>\n");
