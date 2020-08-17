4018eca95979c4cc426b55b4cdaf69d77cb3f2d6
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-08-17 08:39:48 +0200

    Added gatherv with intercommunicator test for C-MPI

---
+++ test/c-mpi/gatherv_intercom-1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+vftr_binary=gatherv_intercom
+nprocs=6
+nb=4
+
+export VFTR_SAMPLING="Yes"
+export VFTR_MPI_LOG="Yes"
+export VFTR_PRECISE="MPI_*"
+
+mpirun -np ${nprocs} ./${vftr_binary} ${nb} || exit 1
