9a79798c107ffc7e1d3cc507c973188de6313c75
Felix Uhl <uhl_felix@web.de>
2020-08-20 13:55:49 +0200

    added sendrecv test for f-mpi

---
+++ test/mpi/f-mpi/sendrecv.F90
@@ -0,0 +1,78 @@
+PROGRAM sendrecv
+
+   USE, INTRINSIC :: ISO_FORTRAN_ENV
+   USE mpi
+
+   IMPLICIT NONE
+
+   CHARACTER(LEN=64) :: cmdargstr = ""
+
+   INTEGER :: comm_size
+   INTEGER :: my_rank
+
+   INTEGER :: nints = 0
+   INTEGER, DIMENSION(:), ALLOCATABLE :: sbuffer
+   INTEGER, DIMENSION(:), ALLOCATABLE :: rbuffer
+
+   INTEGER :: recvstatus(MPI_STATUS_SIZE)
+
+   INTEGER :: destrank, sourcerank
+
+   LOGICAL :: valid_data
+
+   INTEGER :: ierr
+
+   CALL MPI_Init(ierr)
+
+   ! Get the number of processes
+   CALL MPI_Comm_size(MPI_COMM_WORLD, comm_size, ierr);
+   ! Get rank of processes
+   CALL MPI_Comm_rank(MPI_COMM_WORLD, my_rank, ierr);
+
+   ! Write information
+   IF (comm_size < 2) THEN
+      WRITE(UNIT=OUTPUT_UNIT, FMT="(A)") "At least two ranks are required"
+      WRITE(UNIT=OUTPUT_UNIT, FMT="(A)") "RUN again with '-np 2'"
+      CALL FLUSH(OUTPUT_UNIT)
+      CALL MPI_Finalize(ierr)
+      STOP
+   END IF
+
+   ! require cmd-line argument
+   IF (COMMAND_ARGUMENT_COUNT() < 1) THEN
+      WRITE(UNIT=OUTPUT_UNIT, FMT="(A)") "./send_recv <msgsize in integers>"
+      STOP 1
+   END IF
+
+   ! Allocating send/recv buffer
+   CALL GET_COMMAND_ARGUMENT(1,cmdargstr)
+   READ(UNIT=cmdargstr, FMT=*) nints
+   ALLOCATE(sbuffer(nints))
+   sbuffer(:) = my_rank
+   ALLOCATE(rbuffer(nints))
+   rbuffer(:) = -1
+
+   ! Messaging
+   destrank = MOD((my_rank+1),comm_size)
+   sourcerank = my_rank - 1
+   IF (sourcerank < 0) THEN
+      sourcerank = comm_size - 1
+   END IF
+
+   CALL MPI_Sendrecv(sbuffer, nints, MPI_INTEGER, destrank, 0, &
+                     rbuffer, nints, MPI_INTEGER, sourcerank, 0, &
+                     MPI_COMM_WORLD, recvstatus, ierr)
+
+   valid_data = .TRUE.
+   IF (ANY(rbuffer /= sourcerank)) THEN
+      WRITE(UNIT=OUTPUT_UNIT, FMT="(A,I4,A,I4)") "Rank ", my_rank, " received faulty data from rank", sourcerank
+      valid_data = .FALSE.
+   END IF
+
+   DEALLOCATE(sbuffer)
+   DEALLOCATE(rbuffer)
+
+   CALL MPI_Finalize(ierr)
+
+   IF (.NOT.valid_data) STOP 1
+END PROGRAM sendrecv
