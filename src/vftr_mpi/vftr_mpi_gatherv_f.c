444bff9786bc474e863d0be81903d7e858c9a31c
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-08-24 07:57:51 +0200

    proper handling of recvcounts and displacement vectors in fortran version of gatherv

--- src/vftr_mpi/vftr_mpi_gatherv_f.c
+++ src/vftr_mpi/vftr_mpi_gatherv_f.c
@@ -30,16 +30,33 @@ void vftr_MPI_Gatherv_F(void *sendbuf, MPI_Fint *sendcount, MPI_Fint *f_sendtype
                         MPI_Fint *f_error) {
 
    MPI_Comm c_comm = PMPI_Comm_f2c(*f_comm);
+
+   // determine if root process
+   int isroot;
+   int isintercom;
+   PMPI_Comm_test_inter(c_comm, &isintercom);
+   if (isintercom) {
+      isroot = MPI_ROOT == (int) (*root);
+   } else {
+      int myrank;
+      PMPI_Comm_rank(c_comm, &myrank);
+      isroot = myrank == (int) (*root);
+   }
+
    int size;
    PMPI_Comm_size(c_comm, &size);
    MPI_Datatype c_sendtype = PMPI_Type_f2c(*f_sendtype);
-   int *c_recvcounts = (int*) malloc(size*sizeof(int));
-   for (int i=0; i<size; i++) {
-      c_recvcounts[i] = (int) f_recvcounts[i];
-   }
-   int *c_displs = (int*) malloc(size*sizeof(int));
-   for (int i=0; i<size; i++) {
-      c_displs[i] = (int) f_displs[i];
+   int *c_recvcounts = NULL ;
+   int *c_displs = NULL ;
+   if (isroot) {
+      c_recvcounts = (int*) malloc(size*sizeof(int));
+      for (int i=0; i<size; i++) {
+         c_recvcounts[i] = (int) f_recvcounts[i];
+      }
+      c_displs = (int*) malloc(size*sizeof(int));
+      for (int i=0; i<size; i++) {
+         c_displs[i] = (int) f_displs[i];
+      }
    }
    MPI_Datatype c_recvtype = PMPI_Type_f2c(*f_recvtype);
 
@@ -57,8 +74,10 @@ void vftr_MPI_Gatherv_F(void *sendbuf, MPI_Fint *sendcount, MPI_Fint *f_sendtype
                                   (int)(*root),
                                   c_comm);
 
-   free(c_recvcounts);
-   free(c_displs);
+   if (isroot) {
+      free(c_recvcounts);
+      free(c_displs);
+   }
 
    *f_error = (MPI_Fint) (c_error);
 }
