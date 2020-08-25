5cbe0faf80903cdc7d9e07264652b3c31365ad65
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-08-25 09:20:00 +0200

    adjusted init finalize testscript to handle the f08 logs properly

--- test/mpi/testscripts/init_finalize-3.sh
+++ test/mpi/testscripts/init_finalize-3.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 vftr_binary=init_finalize
 nprocs=4
 
@@ -12,13 +11,13 @@ mpirun -np ${nprocs} ./${vftr_binary} || exit 1
 cat ${vftr_binary}_0.log
 
 n=$(cat ${vftr_binary}_0.log | \
-    grep -i "MPI_Init<" | wc -l)
+    grep -i "MPI_Init<\|MPI_Init_f08" | wc -l)
 if [[ ${n} -le 0 ]] ; then
    exit 1;
 fi
 
 n=$(cat ${vftr_binary}_0.log | \
-    grep -i "MPI_Finalize<" | wc -l)
+    grep -i "MPI_Finalize<\|MPI_Finalize_f08" | wc -l)
 if [[ ${n} -le 0 ]] ; then
    exit 1;
 fi
