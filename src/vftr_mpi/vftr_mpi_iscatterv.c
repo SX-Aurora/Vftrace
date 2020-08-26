abbc47de642a0afe062badce3974c54ae412a8fb
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-08-26 12:16:56 +0200

    added iscatterv with intercommunicators test for c-mpi

---
+++ src/vftr_mpi/vftr_mpi_iscatterv.c
@@ -0,0 +1,139 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifdef _MPI
+#include <mpi.h>
+
+#include <stdlib.h>
+
+#include "vftr_timer.h"
+#include "vftr_async_messages.h"
+#include "vftr_mpi_pcontrol.h"
+#include "vftr_mpi_buf_addr_const.h"
+
+int vftr_MPI_Iscatterv(const void *sendbuf, const int *sendcounts,
+                       const int *displs, MPI_Datatype sendtype,
+                       void *recvbuf, int recvcount, MPI_Datatype recvtype,
+                       int root, MPI_Comm comm, MPI_Request *request) {
+
+   // disable profiling based on the Pcontrol level
+   if (vftrace_Pcontrol_level == 0) {
+      return PMPI_Iscatterv(sendbuf, sendcounts, displs, sendtype,
+                           recvbuf, recvcount, recvtype, root, comm, request);
+   } else {
+      long long tstart = vftr_get_runtime_usec();
+      int retVal = PMPI_Iscatterv(sendbuf, sendcounts, displs, sendtype,
+                                 recvbuf, recvcount, recvtype, root, comm, request);
+  
+      // determine if inter or intra communicator
+      int isintercom;
+      PMPI_Comm_test_inter(comm, &isintercom);
+      if (isintercom) {
+         // in intercommunicators the behaviour is more complicated
+         // There are two groups A and B
+         // In group A the root process is located.
+         if (root == MPI_ROOT) {
+            // The root process get the special process wildcard MPI_ROOT
+            // get the size of group B
+            int size;
+            PMPI_Comm_remote_size(comm, &size);
+            int *tmpsendcounts = (int*) malloc(sizeof(int)*size);
+            MPI_Datatype *tmpsendtype = (MPI_Datatype*) malloc(sizeof(MPI_Datatype)*size);
+            int *tmppeer_ranks= (int*) malloc(sizeof(int)*size);
+            // messages to be received
+            for (int i=0; i<size; i++) {
+               tmpsendcounts[i] = sendcounts[i];
+               tmpsendtype[i] = sendtype;
+               // translate the i-th rank in group B to the global rank
+               tmppeer_ranks[i] = vftr_remote2global_rank(comm, i);
+            }
+            // Register request with MPI_COMM_WORLD as communicator
+            // to prevent additional (and thus faulty rank translation)
+            vftr_register_collective_request(send, size, tmpsendcounts, tmpsendtype,
+                                             tmppeer_ranks, MPI_COMM_WORLD,
+                                             *request, tstart);
+            // cleanup temporary arrays
+            free(tmpsendcounts);
+            tmpsendcounts = NULL;
+            free(tmpsendtype);
+            tmpsendtype = NULL;
+            free(tmppeer_ranks);
+            tmppeer_ranks = NULL;
+         } else if (root == MPI_PROC_NULL) {
+            // All other processes from group A pass wildcard MPI_PROC NULL
+            // They do not participate in intercommunicator bcasts
+            ;
+         } else {
+            // All other processes must be located in group B
+            // root is the rank-id in group A Therefore no problems with
+            // rank translation should arise
+            vftr_register_collective_request(recv, 1, &recvcount, &recvtype, &root,
+                                             comm, *request, tstart);
+         }
+      } else {
+         // in intracommunicators the expected behaviour is to
+         // bcast from root to all other processes in the communicator
+         int rank;
+         PMPI_Comm_rank(comm, &rank);
+         if (rank == root) {
+            int size;
+            PMPI_Comm_size(comm, &size);
+            // if recvbuf is special address MPI_IN_PLACE
+            // recvcount and recvtype are ignored.
+            // Use sendcount and sendtype for statistics
+            if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
+               recvcount = sendcounts[rank];
+               recvtype = sendtype;
+            }
+
+            // allocate memory for the temporary arrays
+            // to register communication request
+            int *tmpsendcounts = (int*) malloc(sizeof(int)*size);
+            MPI_Datatype *tmpsendtype = (MPI_Datatype*) malloc(sizeof(MPI_Datatype)*size);
+            int *tmppeer_ranks= (int*) malloc(sizeof(int)*size);
+            // messeges to be received
+            for (int i=0; i<size; i++) {
+               tmpsendcounts[i] = sendcounts[i];
+               tmpsendtype[i] = sendtype;
+               tmppeer_ranks[i] = i;
+            }
+            vftr_register_collective_request(send, size, tmpsendcounts, tmpsendtype,
+                                             tmppeer_ranks, comm, *request, tstart);
+            // cleanup temporary arrays
+            free(tmpsendcounts);
+            tmpsendcounts = NULL;
+            free(tmpsendtype);
+            tmpsendtype = NULL;
+            free(tmppeer_ranks);
+            tmppeer_ranks = NULL;
+
+            // self communication of root process
+            vftr_register_collective_request(recv, 1, &recvcount, &recvtype, &root,
+                                             comm, *request, tstart);
+
+         } else {
+            vftr_register_collective_request(recv, 1, &recvcount, &recvtype, &root,
+                                             comm, *request, tstart);
+         }
+      }
+  
+      return retVal;
+   }
+}
+
+#endif
