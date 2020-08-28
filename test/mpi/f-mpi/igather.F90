7f4faddf8737095f424f76bde376fb426d18ed9c
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-08-28 08:28:12 +0200

    added igather with and without intercommunicators functionality and tests for f-mpi

---
+++ test/mpi/f-mpi/igather.F90
@@ -0,0 +1,89 @@
+PROGRAM gather
+
+   USE, INTRINSIC :: ISO_FORTRAN_ENV
+   USE mpi
+
+   IMPLICIT NONE
+
+   CHARACTER(LEN=64) :: cmdargstr = ""
+
+   INTEGER :: comm_size
+   INTEGER :: my_rank
+
+   INTEGER :: nints = 0
+   INTEGER, DIMENSION(:), ALLOCATABLE :: sbuffer
+   INTEGER, DIMENSION(:,:), ALLOCATABLE :: rbuffer
+
+   INTEGER, PARAMETER :: rootrank = 0
+   INTEGER :: irank
+
+   LOGICAL :: valid_data
+
+   INTEGER :: myrequest
+   INTEGER :: mystatus(MPI_STATUS_SIZE)
+
+   INTEGER :: ierr
+
+   CALL MPI_Init(ierr)
+
+   ! Get the number of processes
+   CALL MPI_Comm_size(MPI_COMM_WORLD, comm_size, ierr);
+   ! Get rank of processes
+   CALL MPI_Comm_rank(MPI_COMM_WORLD, my_rank, ierr);
+
+   ! Write information
+   IF (comm_size < 2) THEN
+      WRITE(UNIT=OUTPUT_UNIT, FMT="(A)") "At least two ranks are required"
+      WRITE(UNIT=OUTPUT_UNIT, FMT="(A)") "RUN again with '-np 2'"
+      CALL FLUSH(OUTPUT_UNIT)
+      CALL MPI_Finalize(ierr)
+      STOP
+   END IF
+
+   ! require cmd-line argument
+   IF (COMMAND_ARGUMENT_COUNT() < 1) THEN
+      WRITE(UNIT=OUTPUT_UNIT, FMT="(A)") "./gather <msgsize in integers>"
+      STOP 1
+   END IF
+
+   ! Allocating send/recv buffer
+   CALL GET_COMMAND_ARGUMENT(1,cmdargstr)
+   READ(UNIT=cmdargstr, FMT=*) nints
+   ALLOCATE(sbuffer(nints))
+   sbuffer(:) = my_rank
+   IF (my_rank == rootrank) THEN
+      ALLOCATE(rbuffer(nints,comm_size))
+      rbuffer(:,:) = -1
+   END IF
+
+   ! Message
+   CALL MPI_Igather(sbuffer, nints, MPI_INTEGER, &
+                    rbuffer, nints, MPI_INTEGER, &
+                    rootrank, MPI_COMM_WORLD, &
+                    myrequest, ierr)
+   CALL MPI_Wait(myrequest, mystatus, ierr)
+
+   IF (my_rank == rootrank) THEN
+      WRITE(UNIT=OUTPUT_UNIT, FMT="(A,I4)") &
+         "Gathering messages from all ranks on rank ", my_rank
+   END IF
+
+   ! validate data
+   valid_data = .TRUE.
+   IF (my_rank == rootrank) THEN
+      DO irank = 0, comm_size - 1
+         IF (ANY(rbuffer(:,irank+1) /= irank)) THEN
+            WRITE(UNIT=OUTPUT_UNIT, FMT="(A,I4,A)") &
+               "Rank ", my_rank, " received faulty data from rank ", irank
+            valid_data = .FALSE.
+         END IF
+      END DO
+      DEALLOCATE(rbuffer)
+   END IF
+
+   DEALLOCATE(sbuffer)
+
+   CALL MPI_Finalize(ierr)
+
+   IF (.NOT.valid_data) STOP 1
+END PROGRAM gather
