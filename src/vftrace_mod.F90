d3e56aea34f731e1d5dcf6806ea7d73cd2d723a5
Felix Uhl <uhl_felix@web.de>
2020-09-01 07:55:35 +0200

    corrected string passing between fortran-regions and c-regions

--- src/vftrace_mod.F90
+++ src/vftrace_mod.F90
@@ -40,24 +40,6 @@ module vftrace
 
    interface
 
-      ! Mark the entry of an instrumented region
-      ! name ist the Region name to be used in the profile
-      subroutine vftrace_region_begin(name) &
-         bind(c, name="vftrace_region_begin")
-         use iso_c_binding, only : c_char
-         implicit none
-         character(c_char), intent(in) :: name(*)
-      end subroutine vftrace_region_begin
- 
-      ! Mark end of instrumented region
-      ! name ist the Region name to be used in the profile
-      subroutine vftrace_region_end(name) &
-         bind(c, name="vftrace_region_end")
-         use iso_c_binding, only : c_char
-         implicit none
-         character(c_char), intent(in) :: name(*)
-      end subroutine vftrace_region_end
-        
       ! pause and resume sampling via vftrace in user code
       subroutine vftrace_pause() &
          bind(c, name="vftrace_pause")
@@ -71,7 +53,28 @@ module vftrace
 
    end interface
 
-   ! interface for non public routines
+   ! interface for non public regions routines
+   interface
+      ! Mark the entry of an instrumented region
+      ! name ist the Region name to be used in the profile
+      subroutine vftrace_region_begin_C(name) &
+         bind(c, name="vftrace_region_begin")
+         use iso_c_binding, only : c_char
+         implicit none
+         character(c_char), intent(in) :: name(*)
+      end subroutine vftrace_region_begin_C
+ 
+      ! Mark end of instrumented region
+      ! name ist the Region name to be used in the profile
+      subroutine vftrace_region_end_C(name) &
+         bind(c, name="vftrace_region_end")
+         use iso_c_binding, only : c_char
+         implicit none
+         character(c_char), intent(in) :: name(*)
+      end subroutine vftrace_region_end_C
+   end interface
+
+   ! interface for non public stack routines
    ! used for passing string pointer between fortran and C
    interface
       function vftrace_get_stack_C_charptr() &
@@ -91,6 +94,40 @@ module vftrace
    end interface
 
 contains
+
+   subroutine vftrace_region_begin(name)
+      use iso_c_binding, only : c_char, c_null_char
+      implicit none
+      character(len=*), intent(in) :: name
+      integer :: name_len
+      character(kind=c_char,len=:), allocatable :: c_name
+      integer :: i
+      name_len = len(adjustl(trim(name)))
+      ! null terminator space 
+      name_len = name_len + 1
+      allocate(character(len=name_len) :: c_name)
+      c_name(:) = adjustl(trim(name))
+      c_name(name_len-1:name_len) = c_null_char
+      call vftrace_region_begin_C(c_name)
+      deallocate(c_name)
+   end subroutine vftrace_region_begin
+
+   subroutine vftrace_region_end(name)
+      use iso_c_binding, only : c_char, c_null_char
+      implicit none
+      character(len=*), intent(in) :: name
+      integer :: name_len
+      character(kind=c_char,len=:), allocatable :: c_name
+      integer :: i
+      name_len = len(adjustl(trim(name)))
+      ! null terminator space 
+      name_len = name_len + 1
+      allocate(character(len=name_len) :: c_name)
+      c_name(:) = adjustl(trim(name))
+      c_name(name_len-1:name_len) = c_null_char
+      call vftrace_region_end_C(c_name)
+      deallocate(c_name)
+   end subroutine vftrace_region_end
 
    function vftrace_get_stack()
       use iso_c_binding, only : c_int, &
