3dbf8ceccca4e5c3d7b0c8307cc6da7c0349ddcf
Felix Uhl <uhl_felix@web.de>
2020-09-02 11:56:03 +0200

    added MPI_Iscan functionality c-wrapper and test for c

---
+++ src/vftr_mpi/vftr_mpi_iscan.c
@@ -0,0 +1,107 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifdef _MPI
+#include <mpi.h>
+
+#include "vftr_timer.h"
+#include "vftr_async_messages.h"
+#include "vftr_mpi_pcontrol.h"
+#include "vftr_mpi_buf_addr_const.h"
+
+int vftr_MPI_Iscan(const void *sendbuf, void *recvbuf, int count,
+                   MPI_Datatype datatype, MPI_Op op, MPI_Comm comm,
+                   MPI_Request *request) {
+
+   // disable profiling based on the Pcontrol level
+   if (vftrace_Pcontrol_level == 0) {
+      return PMPI_Iscan(sendbuf, recvbuf, count, datatype, op, comm, request);
+   } else {
+      long long tstart = vftr_get_runtime_usec();
+      int retVal = PMPI_Iscan(sendbuf, recvbuf, count, datatype, op, comm, request);
+
+      // Only intra-communicators, as the standard specifically states
+      // that the scan operation is invalid for intercommunicators
+      int size;
+      PMPI_Comm_size(comm, &size);
+      int rank;
+      PMPI_Comm_rank(comm, &rank);
+      // For rank 0 additional self communication occours
+      if (rank == 0) {
+         vftr_register_collective_request(send, 1, &count, &datatype,
+                                          &rank, comm, *request, tstart);
+         vftr_register_collective_request(recv, 1, &count, &datatype,
+                                          &rank, comm, *request, tstart);
+      } 
+      if (size > 1) {
+         // the precise communication pattern for a scan operation
+         // strongly depends on the MPI-implementation
+         // and is not specified by the standard!
+         // There are two ovious possibilities
+         // 1. Rank 0 sends to 1
+         //    Rank 1 receives from 0 and sends to 2
+         //    Rank 2 receives from 1 and sends to 3
+         //    ...
+         //    Rank (n-1) receives from (n-2) and sends to n
+         //    Rank n receives from (n-1)
+         //
+         //    In this pattern every rank performs its operation and
+         //    hands the result to the next rank.
+         //    This pattern will be recorded here,
+         //    as it has less communication overall and seems more lightweight.
+         //    The actually used pattern might introduce optimizations or be a mixture of both patterns, 
+         //    which cannot be considered here.
+         //
+         // 2. Rank 0 sends to ranks 1 to n
+         //    Rank 1 sends to ranks 2 to n and receives from rank 0
+         //    Rank 2 sends to ranks 3 to n and receives from ranks 0 to 1
+         //    Rank 3 sends to ranks 4 to n and receives from ranks 0 to 2
+         //    ...
+         //    Rank (n-1) sends to rank n and receives from ranks 0 to (n-2)
+         //    Rank n receives from ranks 0 to (n-1)
+         //
+         //    This pattern will not be recorded as it involves
+         //    a lot of different communications and seems to
+         //    scale worse than pattern 1!
+         if (rank == 0) {
+            // rank 0 only sends to 1
+            int tmprank = rank+1;
+            vftr_register_collective_request(send, 1, &count, &datatype,
+                                             &tmprank, comm, *request, tstart);
+         } else if (rank == size - 1) {
+            // the last rank will only receive from the rank before it
+            int tmprank = rank-1;
+            vftr_register_collective_request(recv, 1, &count, &datatype,
+                                             &tmprank, comm, *request, tstart);
+         } else {
+            // all other ranks will receive from the rank before it
+            // and send to the rank after it
+            int tmprank = rank-1;
+            vftr_register_collective_request(recv, 1, &count, &datatype,
+                                             &tmprank, comm, *request, tstart);
+            tmprank = rank+1;
+            vftr_register_collective_request(send, 1, &count, &datatype,
+                                             &tmprank, comm, *request, tstart);
+         }
+      }
+
+      return retVal;
+   }
+}
+
+#endif
