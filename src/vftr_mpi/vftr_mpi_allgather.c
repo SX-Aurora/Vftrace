f24a5acd1c12319dc55c2660eeaf2354ecb2c090
Felix Uhl <uhl_felix@web.de>
2020-09-13 10:15:34 +0200

    added optional synchronization time estimateion for vftr_mpi_allgather.c

--- src/vftr_mpi/vftr_mpi_allgather.c
+++ src/vftr_mpi/vftr_mpi_allgather.c
@@ -20,6 +20,8 @@
 #include <mpi.h>
 
 #include "vftr_timer.h"
+#include "vftr_regions.h"
+#include "vftr_environment.h"
 #include "vftr_sync_messages.h"
 #include "vftr_mpi_pcontrol.h"
 #include "vftr_mpi_buf_addr_const.h"
@@ -33,6 +35,13 @@ int vftr_MPI_Allgather(const void *sendbuf, int sendcount,
       return PMPI_Allgather(sendbuf, sendcount, sendtype, recvbuf,
                             recvcount, recvtype, comm);
    } else {
+      // Estimate synchronization time
+      if (vftr_environment->mpi_show_sync_time->value) {
+         vftr_internal_region_begin("MPI_Allgather_sync");
+         PMPI_Barrier(comm);
+         vftr_internal_region_end("MPI_Allather_sync");
+      }   
+      
       long long tstart = vftr_get_runtime_usec();
       int retVal = PMPI_Allgather(sendbuf, sendcount, sendtype, recvbuf,
                                   recvcount, recvtype, comm);
