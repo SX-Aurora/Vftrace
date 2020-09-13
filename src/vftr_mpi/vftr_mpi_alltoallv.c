507ec5eab5d0256b12456142289a4c08d095d024
Felix Uhl <uhl_felix@web.de>
2020-09-13 10:15:34 +0200

    added optional synchronization time estimateion for vftr_mpi_alltoallv.c

--- src/vftr_mpi/vftr_mpi_alltoallv.c
+++ src/vftr_mpi/vftr_mpi_alltoallv.c
@@ -20,6 +20,8 @@
 #include <mpi.h>
 
 #include "vftr_timer.h"
+#include "vftr_regions.h"
+#include "vftr_environment.h"
 #include "vftr_sync_messages.h"
 #include "vftr_mpi_pcontrol.h"
 #include "vftr_mpi_buf_addr_const.h"
@@ -34,6 +36,13 @@ int vftr_MPI_Alltoallv(const void *sendbuf, const int *sendcounts,
       return PMPI_Alltoallv(sendbuf, sendcounts, sdispls, sendtype,
                             recvbuf, recvcounts, rdispls, recvtype, comm);
    } else {
+      // Estimate synchronization time
+      if (vftr_environment->mpi_show_sync_time->value) {
+         vftr_internal_region_begin("MPI_Alltoallv_sync");
+         PMPI_Barrier(comm);
+         vftr_internal_region_end("MPI_Alltoallv_sync");
+      }
+
       long long tstart = vftr_get_runtime_usec();
       int retVal = PMPI_Alltoallv(sendbuf, sendcounts, sdispls, sendtype,
                                   recvbuf, recvcounts, rdispls, recvtype, comm);
