320cd4dbff7af6e9c0311b84d252d10b1f11b469
Felix Uhl <uhl_felix@web.de>
2020-09-23 15:14:50 +0200

    passing invalid count and datatypes to Iallgather with in_place buffer in iallgather_inplace test

--- test/mpi/c-mpi/iallgather_inplace.c
+++ test/mpi/c-mpi/iallgather_inplace.c
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
 
    // Messaging
    MPI_Request myrequest;
-   MPI_Iallgather(sbuffer, nints, MPI_INT,
+   MPI_Iallgather(sbuffer, 0, MPI_DATATYPE_NULL,
                   rbuffer, nints, MPI_INT, 
                   MPI_COMM_WORLD, &myrequest);
    printf("Gathering messages from all ranks on rank %d\n", my_rank);
