f56ce9439cd07a255306236f0c1fdef1ffaea3bf
Felix Uhl <uhl_felix@web.de>
2020-09-24 14:30:08 +0200

    added estimation of mpi bookkeeping overhead to mpi_put

--- src/vftr_mpi/vftr_mpi_put.c
+++ src/vftr_mpi/vftr_mpi_put.c
@@ -40,6 +40,7 @@ int vftr_MPI_Put(const void *origin_addr, int origin_count,
                             target_datatype, win);
       long long tend = vftr_get_runtime_usec();
 
+      long long t2start = tend;
       // Need to figure out the partner rank in a known communicator to store info
       MPI_Group local_group;
       PMPI_Win_get_group(win, &local_group);
@@ -56,6 +57,10 @@ int vftr_MPI_Put(const void *origin_addr, int origin_count,
 
       vftr_store_sync_message_info(send, origin_count, origin_datatype,
                                    global_rank, -1, MPI_COMM_WORLD, tstart, tend);
+      long long t2end = vftr_get_runtime_usec();
+
+      vftr_mpi_overhead_usec += t2end - t2start;
+
       return retVal;
    }
 }
