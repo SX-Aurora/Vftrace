610a5ae04a64f5d0b065385457da18eb438d82bf
Felix Uhl <uhl_felix@web.de>
2020-09-24 14:30:08 +0200

    added estimation of mpi bookkeeping overhead to mpi_isend

--- src/vftr_mpi/vftr_mpi_isend.c
+++ src/vftr_mpi/vftr_mpi_isend.c
@@ -34,7 +34,11 @@ int vftr_MPI_Isend(const void *buf, int count, MPI_Datatype datatype,
       long long tstart = vftr_get_runtime_usec();
       int retVal = PMPI_Isend(buf, count, datatype, dest, tag, comm, request);
 
+      long long t2start = vftr_get_runtime_usec();
       vftr_register_P2P_request(send, count, datatype, dest, tag, comm, *request, tstart);
+      long long t2end = vftr_get_runtime_usec();
+
+      vftr_mpi_overhead_usec += t2end - t2start;
 
       return retVal;
    }
