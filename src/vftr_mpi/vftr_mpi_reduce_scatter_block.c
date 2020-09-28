99d8a3edcdf495d5071713f36d36166a4a038052
Felix Uhl <uhl_felix@web.de>
2020-09-28 12:19:52 +0200

    Added MPI_Reduce_scatter_block functionality with C-wrapper and the three required tests (normal, inplace, intercom)

---
+++ src/vftr_mpi/vftr_mpi_reduce_scatter_block.c
@@ -0,0 +1,149 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifdef _MPI
+#include <mpi.h>
+
+#include "vftr_timer.h"
+#include "vftr_regions.h"
+#include "vftr_environment.h"
+#include "vftr_sync_messages.h"
+#include "vftr_mpi_pcontrol.h"
+#include "vftr_mpi_buf_addr_const.h"
+
+int vftr_MPI_Reduce_scatter_block(const void *sendbuf, void *recvbuf, int recvcount,
+                                  MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
+
+   // disable profiling based on the Pcontrol level
+   if (vftrace_Pcontrol_level == 0) {
+      return PMPI_Reduce_scatter_block(sendbuf, recvbuf, recvcount, datatype, op, comm);
+   } else {
+      // Estimate synchronization time
+      if (vftr_environment->mpi_show_sync_time->value) {
+         vftr_internal_region_begin("mpi_reduce_scatter_block_sync");
+         PMPI_Barrier(comm);
+         vftr_internal_region_end("mpi_reduce_scatter_block_sync");
+      }
+
+      long long tstart = vftr_get_runtime_usec();
+      int retVal = PMPI_Reduce_scatter_block(sendbuf, recvbuf, recvcount, datatype, op, comm);
+      long long tend = vftr_get_runtime_usec();
+  
+      // determine if inter or intra communicator
+      int isintercom;
+      PMPI_Comm_test_inter(comm, &isintercom);
+      if (isintercom) {
+         // Every process of group A performs the reduction within the group A
+         // and stores the result on everyp process of group B and vice versa
+         int size;
+         PMPI_Comm_size(comm, &size);
+         int remotesize;
+         PMPI_Comm_remote_size(comm, &remotesize);
+         for (int i=0; i<remotesize; i++) {
+            // The sends and receives are not strictly true
+            // as the number of peer processes with wich each
+            // process communicates strongly depends on the unerlying reduction algorithm
+            // There are multiple possibilities how to deal with this
+            // 1. Reduce the data to one rank, and scatter it from there
+            // 2. Every process functions as the root process of individual remote reduce operations
+            //
+            // To record the communication pattern of 1 a root process would need to be selected.
+            // Therefore We selected number 2.
+            //
+            //
+            // In both groups the sendbuffer size is the same.
+            // Additionally it is required by the standard that
+            // the send buffer size for MPI_Reduce_scatter_block with intracommunicators
+            // is divisible by both group sizes.
+            // Therefore the amount of data send to each remore process is
+            // (local_group_size * recvcount) / remote_group_size
+            //
+            // translate the i-th rank in the remote group to the global rank
+            int global_peer_rank = vftr_remote2global_rank(comm, i);
+            // Store message info with MPI_COMM_WORLD as communicator
+            // to prevent additional (and thus faulty rank translation)
+            vftr_store_sync_message_info(send, (size * recvcount) / remotesize, datatype,
+                                         global_peer_rank, -1, MPI_COMM_WORLD,
+                                         tstart, tend);
+            vftr_store_sync_message_info(recv, recvcount, datatype,
+                                         global_peer_rank, -1, MPI_COMM_WORLD,
+                                         tstart, tend);
+         }
+      } else {
+         int size;
+         PMPI_Comm_size(comm, &size);
+         // if sendbuf is special address MPI_IN_PLACE
+         if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
+            // For the in-place option no self communication is executed
+            int rank;
+            PMPI_Comm_rank(comm, &rank);
+
+            for (int i=0; i<rank; i++) {
+               // The sends and receives are not strictly true
+               // as the number of peer processes with wich each
+               // process communicates strongly depends on the unerlying reduction algorithm
+               // There are multiple possibilities how to deal with this
+               // 1. Reduce the data to one rank, and scatter it from there
+               // 2. Every process functions as the root process of individual reduce operations
+               //
+               // To record the communication pattern of 1 a root process would need to be selected.
+               // Therefore We selected number 2.
+               vftr_store_sync_message_info(send, recvcount, datatype, i, -1,
+                                            comm, tstart, tend);
+               vftr_store_sync_message_info(recv, recvcount, datatype, i, -1,
+                                            comm, tstart, tend);
+            }
+            for (int i=rank+1; i<size; i++) {
+               // The sends and receives are not strictly true
+               // as the number of peer processes with wich each
+               // process communicates strongly depends on the unerlying reduction algorithm
+               // There are multiple possibilities how to deal with this
+               // 1. Reduce the data to one rank, and scatter it from there
+               // 2. Every process functions as the root process of individual reduce operations
+               //
+               // To record the communication pattern of 1 a root process would need to be selected.
+               // Therefore We selected number 2.
+               vftr_store_sync_message_info(send, recvcount, datatype, i, -1,
+                                            comm, tstart, tend);
+               vftr_store_sync_message_info(recv, recvcount, datatype, i, -1,
+                                            comm, tstart, tend);
+            }
+         } else {
+            for (int i=0; i<size; i++) {
+               // The sends and receives are not strictly true
+               // as the number of peer processes with wich each
+               // process communicates strongly depends on the unerlying reduction algorithm
+               // There are multiple possibilities how to deal with this
+               // 1. Reduce the data to one rank, and scatter it from there
+               // 2. Every process functions as the root process of individual reduce operations
+               //
+               // To record the communication pattern of 1 a root process would need to be selected.
+               // Therefore We selected number 2.
+               vftr_store_sync_message_info(send, recvcount, datatype, i, -1,
+                                            comm, tstart, tend);
+               vftr_store_sync_message_info(recv, recvcount, datatype, i, -1,
+                                            comm, tstart, tend);
+            }
+         }
+      }
+  
+      return retVal;
+   }
+}
+
+#endif
