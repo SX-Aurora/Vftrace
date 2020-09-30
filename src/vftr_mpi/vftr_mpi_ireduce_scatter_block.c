9811f30cca05796ae5af317aaa163cf278ba6f4c
Felix Uhl <uhl_felix@web.de>
2020-09-30 09:31:25 +0200

    adjusted logging of ireduce_scatter_block to match reduce scatter for identical receivecounts

--- src/vftr_mpi/vftr_mpi_ireduce_scatter_block.c
+++ src/vftr_mpi/vftr_mpi_ireduce_scatter_block.c
@@ -22,7 +22,7 @@
 #include "vftr_timer.h"
 #include "vftr_regions.h"
 #include "vftr_environment.h"
-#include "vftr_sync_messages.h"
+#include "vftr_async_messages.h"
 #include "vftr_mpi_pcontrol.h"
 #include "vftr_mpi_buf_addr_const.h"
 
@@ -52,84 +52,102 @@ int vftr_MPI_Ireduce_scatter_block(const void *sendbuf, void *recvbuf, int recvc
          // 1. Reduce the data to one rank, and scatter it from there
          // 2. Every process functions as the root process of individual remote reduce operations
          //
-         // To record the communication pattern of 1 a root process would need to be selected.
-         // Therefore We selected number 2.
-         //
-         //
-         // In both groups the sendbuffer size is the same.
-         // Additionally it is required by the standard that
-         // the send buffer size for MPI_Reduce_scatter_block with intracommunicators
-         // is divisible by both group sizes.
-         // Therefore the amount of data send to each remore process is
-         // (local_group_size * recvcount) / remote_group_size
-         //
-         // translate the i-th rank in the remote group to the global rank
+         // The Standard states in an advice to implementors, that MPI_Reduce_scatter behaves like
+         // an MPI_Reduce followed by MPI_Scatterv. As root process we arbitrarily select 0 of each group
          int size;
          PMPI_Comm_size(comm, &size);
+         int rank;
+         PMPI_Comm_rank(comm, &rank);
          int remotesize;
          PMPI_Comm_remote_size(comm, &remotesize);
-         int *tmpcount = (int*) malloc(sizeof(int)*remotesize);
-         MPI_Datatype *tmptype = (MPI_Datatype*) malloc(sizeof(MPI_Datatype)*remotesize);
-         int *peer_ranks = (int*) malloc(sizeof(int)*remotesize);
-         //messages to be send
-         for (int i=0; i<remotesize; i++) {
-            tmpcount[i] = (size * recvcount) / remotesize;
-            tmptype[i] = datatype;
-            // translate the i-th rank in the remote group to the global rank
-            peer_ranks[i] = vftr_remote2global_rank(comm, i);
-         }
-         // Register request with MPI_COMM_WORLD as communicator
-         // to prevent additional (and thus faulty rank translation)
-         vftr_register_collective_request(send, remotesize, tmpcount, tmptype, peer_ranks,
-                                          MPI_COMM_WORLD, *request, tstart);
-         // messages to be received
-         for (int i=0; i<remotesize; i++) {
-            tmpcount[i] = recvcount;
-            tmptype[i] = datatype;
-            // translate the i-th rank in the remote group to the global rank
-            peer_ranks[i] = vftr_remote2global_rank(comm, i);
-         }
-         // Register request with MPI_COMM_WORLD as communicator
-         // to prevent additional (and thus faulty rank translation)
-         vftr_register_collective_request(recv, size, tmpcount, tmptype, peer_ranks,
-                                          MPI_COMM_WORLD, *request, tstart);
-         // cleanup temporary arrays
-         free(tmpcount);
-         tmpcount = NULL;
-         free(tmptype);
-         tmptype = NULL;
-         free(peer_ranks);
-         peer_ranks = NULL;
 
+         // In both groups the sendbuffer size is the same.
+         int count = size*recvcount;
+         // determine global ranks for the local and remote root rank
+         int remote_root_rank = vftr_remote2global_rank(comm, 0);
+         int local_root_rank = vftr_local2global_rank(comm, 0);
+         if (rank == 0) {
+            // allocate memory for the temporary arrays
+            // to register communication request
+            int *tmpcount = (int*) malloc(sizeof(int)*(remotesize+1));
+            MPI_Datatype *tmptype = (MPI_Datatype*) malloc(sizeof(MPI_Datatype)*(remotesize+1));
+            int *peer_ranks = (int*) malloc(sizeof(int)*(remotesize+1));
+            // messages received from remote ranks for reduction
+            for (int i=0; i<remotesize; i++) {
+               tmpcount[i] = count;
+               tmptype[i] = datatype;
+               // Register message info with MPI_COMM_WORLD as communicator
+               // to prevent additional (and thus faulty rank translation)
+               peer_ranks[i] = vftr_remote2global_rank(comm, i);
+            }
+            // scattered message received by itself 
+            tmpcount[remotesize] = recvcount;
+            tmptype[remotesize] = datatype;
+            // Register message info with MPI_COMM_WORLD as communicator
+            // to prevent additional (and thus faulty rank translation)
+            peer_ranks[remotesize] = local_root_rank;
+            vftr_register_collective_request(recv, remotesize+1, tmpcount, tmptype, peer_ranks,
+                                             MPI_COMM_WORLD, *request, tstart);
+            if (size != remotesize) {
+               // only reallocate if the two groups are of different size
+               free(tmpcount);
+               tmpcount = NULL;
+               free(tmptype);
+               tmptype = NULL;
+               free(peer_ranks);
+               peer_ranks = NULL;
+   
+               // allocate memory for the temporary arrays
+               // to register communication request
+               tmpcount = (int*) malloc(sizeof(int)*(size+1));
+               tmptype = (MPI_Datatype*) malloc(sizeof(MPI_Datatype)*(size+1));
+               peer_ranks = (int*) malloc(sizeof(int)*(size+1));
+            }
+
+            // process 0 scatters the reduction results to all members of its group
+            for (int i=0; i<size; i++) {
+               tmpcount[i] = recvcount;
+               tmptype[i] = datatype;
+               // Register message info with MPI_COMM_WORLD as communicator
+               // to prevent additional (and thus faulty rank translation)
+               peer_ranks[i] = vftr_local2global_rank(comm, i);
+            }
+            // send the complete sendbuffer to process 0 of the remote group
+            tmpcount[size] = count;
+            tmptype[size] = datatype;
+            // Register message info with MPI_COMM_WORLD as communicator
+            // to prevent additional (and thus faulty rank translation)
+            peer_ranks[size] = remote_root_rank;
+            vftr_register_collective_request(send, size+1, tmpcount, tmptype, peer_ranks,
+                                             MPI_COMM_WORLD, *request, tstart);
+            free(tmpcount);
+            tmpcount = NULL;
+            free(tmptype);
+            tmptype = NULL;
+            free(peer_ranks);
+            peer_ranks = NULL;
+         } else {
+            // send the complete sendbuffer to process 0 of the remote group
+            // Register message info with MPI_COMM_WORLD as communicator
+            // to prevent additional (and thus faulty rank translation)
+            vftr_register_collective_request(send, 1, &count, &datatype,
+                                             &remote_root_rank,
+                                             MPI_COMM_WORLD, *request, tstart);
+            // receive scattered reduction result from process 0 of own group
+            // Register message info with MPI_COMM_WORLD as communicator
+            // to prevent additional (and thus faulty rank translation)
+            vftr_register_collective_request(recv, 1, &recvcount, &datatype,
+                                             &local_root_rank,
+                                             MPI_COMM_WORLD, *request, tstart);
+         }
       } else {
          int size;
          PMPI_Comm_size(comm, &size);
+         int rank;
+         PMPI_Comm_rank(comm, &rank);
          // if sendbuf is special address MPI_IN_PLACE
          if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
             if (size > 1) {
-               // For the in-place option no self communication is executed
-               int rank;
-               PMPI_Comm_rank(comm, &rank);
-
-               // allocate memory for the temporary arrays
-               // to register communication request
-               int *tmpcount = (int*) malloc(sizeof(int)*(size-1));
-               MPI_Datatype *tmptype = (MPI_Datatype*) malloc(sizeof(MPI_Datatype)*(size-1));
-               int *peer_ranks = (int*) malloc(sizeof(int)*(size-1));
-               // messages to be send
-               int idx = 0;
-               for (int i=0; i<rank; i++) {
-                  tmpcount[idx] = recvcount;
-                  tmptype[idx] = datatype;
-                  peer_ranks[idx] = i;
-                  idx++;
-               }
-               for (int i=rank+1; i<size; i++) {
-                  tmpcount[idx] = recvcount;
-                  tmptype[idx] = datatype;
-                  peer_ranks[idx] = i;
-                  idx++;
-               }
                // The sends and receives are not strictly true
                // as the number of peer processes with wich each
                // process communicates strongly depends on the unerlying reduction algorithm
@@ -137,32 +155,50 @@ int vftr_MPI_Ireduce_scatter_block(const void *sendbuf, void *recvbuf, int recvc
                // 1. Reduce the data to one rank, and scatter it from there
                // 2. Every process functions as the root process of individual reduce operations
                //
-               // To record the communication pattern of 1 a root process would need to be selected.
-               // Therefore We selected number 2.
-               vftr_register_collective_request(send, size-1, tmpcount, tmptype, peer_ranks,
-                                                comm, *request, tstart);
-               vftr_register_collective_request(recv, size-1, tmpcount, tmptype, peer_ranks,
-                                                comm, *request, tstart);
-               // cleanup temporary arrays
-               free(tmpcount);
-               tmpcount = NULL;
-               free(tmptype);
-               tmptype = NULL;
-               free(peer_ranks);
-               peer_ranks = NULL;
+               // The Standard states in an advice to implementors, that MPI_Reduce_scatter behaves like
+               // an MPI_Reduce followed by MPI_Scatterv. As root process we arbitrarily select 0 of each group
+               //
+               // every rank needs to know the total size of the sendbuffers
+               int count = size * recvcount;
+               if (rank == 0) {
+                  // allocate memory for the temporary arrays
+                  // to register communication request
+                  int *tmpcount = (int*) malloc(sizeof(int)*(size-1));
+                  MPI_Datatype *tmptype = (MPI_Datatype*) malloc(sizeof(MPI_Datatype)*(size-1));
+                  int *peer_ranks = (int*) malloc(sizeof(int)*(size-1));
+                  // messages to be received for reduction
+                  for (int i=1; i<size; i++) {
+                     tmpcount[i-1] = count;
+                     tmptype[i-1] = datatype;
+                     peer_ranks[i-1] = i;
+                  }
+                  vftr_register_collective_request(recv, size-1, tmpcount, tmptype, peer_ranks,
+                                                   comm, *request, tstart);
+                  // messages to be scatterd after reduction
+                  for (int i=1; i<size; i++) {
+                     // only need to adjust tmpcount to recvcount
+                     // tmptype and peer_ranks are already correct
+                     tmpcount[i-1] = recvcount;
+                  }
+                  vftr_register_collective_request(send, size-1, tmpcount, tmptype, peer_ranks,
+                                                   comm, *request, tstart);
+                  free(tmpcount);
+                  tmpcount = NULL;
+                  free(tmptype);
+                  tmptype = NULL;
+                  free(peer_ranks);
+                  peer_ranks = NULL;
+               } else {
+                  int root = 0;
+                  // send to root process for reduction
+                  vftr_register_collective_request(send, 1, &count, &datatype, &root,
+                                                   comm, *request, tstart);
+                  // receive result of redcution scattered to every process
+                  vftr_register_collective_request(recv, 1, &recvcount, &datatype, &root,
+                                                   comm, *request, tstart);
+               }
             }
          } else {
-            // allocate memory for the temporary arrays
-            // to register communication request
-            int *tmpcount = (int*) malloc(sizeof(int)*size);
-            MPI_Datatype *tmptype = (MPI_Datatype*) malloc(sizeof(MPI_Datatype)*size);
-            int *peer_ranks = (int*) malloc(sizeof(int)*size);
-            // messages to be send
-            for (int i=0; i<size; i++) {
-               tmpcount[i] = recvcount;
-               tmptype[i] = datatype;
-               peer_ranks[i] = i;
-            }
             // The sends and receives are not strictly true
             // as the number of peer processes with wich each
             // process communicates strongly depends on the unerlying reduction algorithm
@@ -170,19 +206,54 @@ int vftr_MPI_Ireduce_scatter_block(const void *sendbuf, void *recvbuf, int recvc
             // 1. Reduce the data to one rank, and scatter it from there
             // 2. Every process functions as the root process of individual reduce operations
             //
-            // To record the communication pattern of 1 a root process would need to be selected.
-            // Therefore We selected number 2.
-            vftr_register_collective_request(send, size, tmpcount, tmptype, peer_ranks,
-                                             comm, *request, tstart);
-            vftr_register_collective_request(recv, size, tmpcount, tmptype, peer_ranks,
-                                             comm, *request, tstart);
-            // cleanup temporary arrays
-            free(tmpcount);
-            tmpcount = NULL;
-            free(tmptype);
-            tmptype = NULL;
-            free(peer_ranks);
-            peer_ranks = NULL;
+            // The Standard states in an advice to implementors, that MPI_Reduce_scatter behaves like
+            // an MPI_Reduce followed by MPI_Scatterv. As root process we arbitrarily select 0 of each group
+            //
+            // every rank needs to know the total size of the sendbuffers
+            int count = size * recvcount;
+            if (rank == 0) {
+               // allocate memory for the temporary arrays
+               // to register communication request
+               int *tmpcount = (int*) malloc(sizeof(int)*(size+1));
+               MPI_Datatype *tmptype = (MPI_Datatype*) malloc(sizeof(MPI_Datatype)*(size+1));
+               int *peer_ranks = (int*) malloc(sizeof(int)*(size+1));
+               // messages to be received for reduction
+               for (int i=0; i<size; i++) {
+                  tmpcount[i] = count;
+                  tmptype[i] = datatype;
+                  peer_ranks[i] = i;
+               }
+               // scattered message received by itself
+               tmpcount[size] = recvcount;
+               tmptype[size] = datatype;
+               peer_ranks[size] = rank;
+               vftr_register_collective_request(recv, size+1, tmpcount, tmptype, peer_ranks,
+                                                comm, *request, tstart);
+               // messages to be scatterd after reduction
+               for (int i=0; i<size; i++) {
+                  // only need to adjust tmpcount to recvcount
+                  // tmptype and peer_ranks are already correct
+                  tmpcount[i] = recvcount;
+               }
+               // message send to itself process for reduction
+               tmpcount[size] = count;
+               vftr_register_collective_request(send, size+1, tmpcount, tmptype, peer_ranks,
+                                                comm, *request, tstart);
+               free(tmpcount);
+               tmpcount = NULL;
+               free(tmptype);
+               tmptype = NULL;
+               free(peer_ranks);
+               peer_ranks = NULL;
+            } else {
+               int root = 0;
+               // send to root process for reduction
+               vftr_register_collective_request(send, 1, &count, &datatype, &root,
+                                                comm, *request, tstart);
+               // receive result of redcution scattered to every process
+               vftr_register_collective_request(recv, 1, &recvcount, &datatype, &root,
+                                                comm, *request, tstart);
+            }
          }
       }
   
--- test/mpi/testscripts/ireduce_scatter_block.sh
+++ test/mpi/testscripts/ireduce_scatter_block.sh
@@ -16,72 +16,133 @@ do
 
    # check each rank for the correct message communication
    # patterns in the vfd file
+   sbufsize=$(bc <<< "${nprocs}*${nb}")
    for irank in $(seq 0 1 $(bc <<< "${nprocs}-1"));
    do
       ../../../tools/tracedump ${vftr_binary}_${irank}.vfd
 
-      for jrank in $(seq 0 1 $(bc <<< "${nprocs}-1"));
-      do
-         ipeer=$(bc <<< "${jrank} + 1")
-         # Validate sending
-         # Get actually used message size
-         count=$(../../../tools/tracedump ${vftr_binary}_${irank}.vfd | \
-                 awk '$2=="send" && $3!="end"{getline;print;}' | \
-                 sed 's/=/ /g' | \
-                 sort -nk 9 | \
-                 awk '{print $2}' | \
-                 head -n ${ipeer} | tail -n 1)
-         # get peer process
-         peer=$(../../../tools/tracedump ${vftr_binary}_${irank}.vfd | \
-                awk '$2=="send" && $3!="end"{getline;print;}' | \
-                sed 's/=/ /g' | \
-                sort -nk 9 | \
-                awk '{print $9}' | \
-                head -n ${ipeer} | tail -n 1)
-         # Check if actually used message size is consistent
-         # with expected message size
-         if [[ "${count}" -ne "${nb}" ]] ; then
-            echo "Message send size from rank ${irank} to ${jrank} is ${count}!"
-            echo "Was expecting message size of ${nb}!"
-            exit 1;
-         fi
-         # Check if actually used peer process is consistent
-         # with expected peer process
-         if [[ "${peer}" -ne "${jrank}" ]] ; then
-            echo "Message send from rank ${irank} to ${peer}!"
-            echo "Was expecting sending to rank ${jrank}!"
-            exit 1;
-         fi
-         
-         # validate receiving
-         # Get actually used message size
-         count=$(../../../tools/tracedump ${vftr_binary}_${irank}.vfd | \
-                 awk '$2=="recv" && $3!="end"{getline;print;}' | \
-                 sed 's/=/ /g' | \
-                 sort -nk 9 | \
-                 awk '{print $2}' | \
-                 head -n ${ipeer} | tail -n 1)
-         # get peer process
-         peer=$(../../../tools/tracedump ${vftr_binary}_${irank}.vfd | \
-                awk '$2=="recv" && $3!="end"{getline;print;}' | \
-                sed 's/=/ /g' | \
-                sort -nk 9 | \
-                awk '{print $9}' | \
-                head -n ${ipeer} | tail -n 1)
-         # Check if actually used message size is consistent
-         # with expected message size
-         if [[ "${count}" -ne "${nb}" ]] ; then
-            echo "Message recv size from rank ${jrank} to ${irank} is ${count}!"
-            echo "Was expecting message size of ${nb}!"
-            exit 1;
-         fi
-         # Check if actually used peer process is consistent
-         # with expected peer process
-         if [[ "${peer}" -ne "${jrank}" ]] ; then
-            echo "Message received from rank ${peer} by ${irank}!"
-            echo "Was expecting receiving from rank ${jrank}!"
-            exit 1;
-         fi
-      done
+      # The 0-th rank performs a reduction and
+      # scatters the result to all other ranks
+      if [[ ${irank} -eq "0" ]] ; then
+         for jrank in $(seq 0 1 $(bc <<< "${nprocs}-1"));
+         do
+            ipeer=$(bc <<< ${jrank}+1)
+            # Validate reduction receive
+            # Get actually used message size
+            count=$(../../../tools/tracedump ${vftr_binary}_${irank}.vfd | \
+                    awk '$2=="recv" && $3!="end"{getline;print;}' | \
+                    sed 's/=/ /g' | \
+                    sort -nk 9 | sort -rsnk 2 | \
+                    awk '{print $2}' | \
+                    head -n ${ipeer} | tail -n 1)
+            # get peer process
+            peer=$(../../../tools/tracedump ${vftr_binary}_${irank}.vfd | \
+                   awk '$2=="recv" && $3!="end"{getline;print;}' | \
+                   sed 's/=/ /g' | \
+                   sort -nk 9 | sort -rsnk 2 | \
+                   awk '{print $9}' | \
+                   head -n ${ipeer} | tail -n 1)
+            # Check if actually used message size is consistent
+            # with expected message size
+            if [[ "${count}" -ne "${sbufsize}" ]] ; then
+               echo "Message recv size from rank ${jrank} by ${irank} is ${count}!"
+               echo "Was expecting message size of ${sbufsize}!"
+               exit 1;
+            fi
+            # Check if actually used peer process is consistent
+            # with expected peer process
+            if [[ "${peer}" -ne "${jrank}" ]] ; then
+               echo "Message received from rank ${peer} by ${irank}!"
+               echo "Was expecting recieving from rank ${jrank}!"
+               exit 1;
+            fi
+
+            # validate scatter send
+            count=$(../../../tools/tracedump ${vftr_binary}_${irank}.vfd | \
+                    awk '$2=="send" && $3!="end"{getline;print;}' | \
+                    sed 's/=/ /g' | \
+                    sort -nk 9 | sort -snk 2 | \
+                    awk '{print $2}' | \
+                    head -n ${ipeer} | tail -n 1)
+            peer=$(../../../tools/tracedump ${vftr_binary}_${irank}.vfd | \
+                   awk '$2=="send" && $3!="end"{getline;print;}' | \
+                   sed 's/=/ /g' | \
+                   sort -nk 9 | sort -snk 2 | \
+                   awk '{print $9}' | \
+                   head -n ${ipeer} | tail -n 1)
+            # Check if actually used message size is consistent
+            # with expected message size
+            if [[ "${count}" -ne "${nb}" ]] ; then
+               echo "Message send size from rank ${irank} to ${jrank} is ${count}!"
+               echo "Was expecting message size of ${nb}!"
+               exit 1;
+            fi
+            # Check if actually used peer process is consistent
+            # with expected peer process
+            if [[ "${peer}" -ne "${jrank}" ]] ; then
+               echo "Message send from rank ${irank} to ${peer}!"
+               echo "Was expecting sending to rank ${jrank}!"
+               exit 1;
+            fi
+         done
+      fi
+
+      jrank=0
+      ipeer=1
+      # Validate send to root rank for reduction
+      count=$(../../../tools/tracedump ${vftr_binary}_${irank}.vfd | \
+              awk '$2=="send" && $3!="end"{getline;print;}' | \
+              sed 's/=/ /g' | \
+              sort -nk 9 | sort -srnk 2 | \
+              awk '{print $2}' | \
+              head -n ${ipeer} | tail -n 1)
+      peer=$(../../../tools/tracedump ${vftr_binary}_${irank}.vfd | \
+             awk '$2=="send" && $3!="end"{getline;print;}' | \
+             sed 's/=/ /g' | \
+             sort -nk 9 | sort -srnk 2 | \
+             awk '{print $9}' | \
+             head -n ${ipeer} | tail -n 1)
+      # Check if actually used message size is consistent
+      # with expected message size
+      if [[ "${count}" -ne "${sbufsize}" ]] ; then
+         echo "Message send size from rank ${irank} to ${jrank} is ${count}!"
+         echo "Was expecting message size of ${sbufsize}!"
+         exit 1;
+      fi
+      # Check if actually used peer process is consistent
+      # with expected peer process
+      if [[ "${peer}" -ne "${jrank}" ]] ; then
+         echo "Message send from rank ${irank} to ${peer}!"
+         echo "Was expecting sending to rank ${jrank}!"
+         exit 1;
+      fi
+
+      # validate receive from root rank as scatter
+      count=$(../../../tools/tracedump ${vftr_binary}_${irank}.vfd | \
+              awk '$2=="recv" && $3!="end"{getline;print;}' | \
+              sed 's/=/ /g' | \
+              sort -nk 9 | sort -snk 2 | \
+              awk '{print $2}' | \
+              head -n ${ipeer} | tail -n 1)
+      peer=$(../../../tools/tracedump ${vftr_binary}_${irank}.vfd | \
+             awk '$2=="recv" && $3!="end"{getline;print;}' | \
+             sed 's/=/ /g' | \
+             sort -nk 9 | sort -snk 2 | \
+             awk '{print $9}' | \
+             head -n ${ipeer} | tail -n 1)
+      # Check if actually used message size is consistent
+      # with expected message size
+      if [[ "${count}" -ne "${nb}" ]] ; then
+         echo "Message recv size from rank ${irank} by ${jrank} is ${count}!"
+         echo "Was expecting message size of ${nb}!"
+         exit 1;
+      fi
+      # Check if actually used peer process is consistent
+      # with expected peer process
+      if [[ "${peer}" -ne "${jrank}" ]] ; then
+         echo "Message recv from rank ${peer} by ${irank}!"
+         echo "Was expecting receiving from rank ${jrank}!"
+         exit 1;
+      fi
    done
 done
