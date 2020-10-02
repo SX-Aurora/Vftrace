499ed425b8d99e5a17e216010047bdecf27e66e9
Felix Uhl <uhl_felix@web.de>
2020-10-02 10:08:40 +0200

    MPI-overhead measurement in vftr_mpi_reduce_scatter_block.c

--- src/vftr_mpi/vftr_mpi_reduce_scatter_block.c
+++ src/vftr_mpi/vftr_mpi_reduce_scatter_block.c
@@ -44,6 +44,7 @@ int vftr_MPI_Reduce_scatter_block(const void *sendbuf, void *recvbuf, int recvco
       int retVal = PMPI_Reduce_scatter_block(sendbuf, recvbuf, recvcount, datatype, op, comm);
       long long tend = vftr_get_runtime_usec();
   
+      long long t2start = tend;
       // determine if inter or intra communicator
       int isintercom;
       PMPI_Comm_test_inter(comm, &isintercom);
@@ -175,6 +176,9 @@ int vftr_MPI_Reduce_scatter_block(const void *sendbuf, void *recvbuf, int recvco
                                          comm, tstart, tend);
          }
       }
+      long long t2end = vftr_get_runtime_usec();
+
+      vftr_mpi_overhead_usec += t2end - t2start;
   
       return retVal;
    }
