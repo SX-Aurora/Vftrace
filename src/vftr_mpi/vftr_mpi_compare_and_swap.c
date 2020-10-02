8ae3a389d28be8c80e7f4ef3d03827ab21a504c2
Felix Uhl <uhl_felix@web.de>
2020-10-02 10:42:58 +0200

    corrected typo

--- src/vftr_mpi/vftr_mpi_compare_and_swap.c
+++ src/vftr_mpi/vftr_mpi_compare_and_swap.c
@@ -58,7 +58,7 @@ int vftr_MPI_Compare_and_swap(const void *origin_addr, const void *compare_addr,
                                    -1, MPI_COMM_WORLD, tstart, tend);
       long long t2end = vftr_get_runtime_usec();
 
-      vftr_mpi_overhead_used += t2end - t2start;
+      vftr_mpi_overhead_usec += t2end - t2start;
 
       return retVal;
    }
