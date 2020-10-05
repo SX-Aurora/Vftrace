0ed6edc019f2ebc0b0fd3f73b940aab2342d4843
Felix Uhl <uhl_felix@web.de>
2020-10-05 09:04:01 +0200

    Added MPI_Issend C-,F,F08-wrapper and the corresponding tests

---
+++ src/vftr_mpi/vftr_mpi_issend.c
@@ -0,0 +1,47 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifdef _MPI
+#include <mpi.h>
+
+#include "vftr_timer.h"
+#include "vftr_async_messages.h"
+#include "vftr_mpi_pcontrol.h"
+
+int vftr_MPI_Issend(const void *buf, int count, MPI_Datatype datatype,
+                    int dest, int tag, MPI_Comm comm,
+                    MPI_Request *request) {
+
+   // disable profiling based on the Pcontrol level
+   if (vftrace_Pcontrol_level == 0) {
+      return PMPI_Issend(buf, count, datatype, dest, tag, comm, request);
+   } else {
+      long long tstart = vftr_get_runtime_usec();
+      int retVal = PMPI_Issend(buf, count, datatype, dest, tag, comm, request);
+
+      long long t2start = vftr_get_runtime_usec();
+      vftr_register_P2P_request(send, count, datatype, dest, tag, comm, *request, tstart);
+      long long t2end = vftr_get_runtime_usec();
+
+      vftr_mpi_overhead_usec += t2end - t2start;
+
+      return retVal;
+   }
+}
+
+#endif
