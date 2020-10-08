8b4d8a67014fec137c45b726ff2c4c8d92c1eccc
MeisterEule <christian.weiss@emea.nec.com>
2020-10-08 13:14:24 +0200

    Fix vftr_find_function call

--- src/vftr_filewrite.c
+++ src/vftr_filewrite.c
@@ -566,7 +566,7 @@ void evaluate_display_function (char *func_name, display_function_t **display_fu
     char func_name_sync[strlen(func_name)+5];
     int n_indices, *stack_indices = NULL, *func_indices = NULL;	
     int n_indices_sync, *func_indices_sync = NULL, *stack_indices_sync = NULL;;
-    vftr_find_function (func_name, &stack_indices, &func_indices, &n_indices, true, STACK_INFO);
+    vftr_find_function (func_name, &func_indices, &stack_indices, &n_indices, true, FUNC_TABLE);
     (*display_func)->n_indices = n_indices;
     (*display_func)->stack_indices = (int*)malloc (n_indices * sizeof(int));
     memcpy ((*display_func)->stack_indices, stack_indices, n_indices * sizeof(int));
@@ -576,8 +576,8 @@ void evaluate_display_function (char *func_name, display_function_t **display_fu
     if (display_sync_time) {
     	strcpy (func_name_sync, func_name);
     	strcat (func_name_sync, "_sync");
-    	vftr_find_function (func_name_sync, &stack_indices_sync,
-			    &func_indices_sync, &n_indices_sync, true, STACK_INFO);
+    	vftr_find_function (func_name_sync, &func_indices_sync,
+			    &stack_indices_sync, &n_indices_sync, true, STACK_INFO);
     	if (n_indices_sync > 0 && n_indices != n_indices_sync) {
     	    printf ("Error: Number of synchronize regions does not match total number of regions: %d %d\n",
     	    	n_indices, n_indices_sync);
