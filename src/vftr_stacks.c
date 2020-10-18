ebd8f81821527e93af16ab28b3ce4af4ef86fcc2
MeisterEule <christian.weiss@emea.nec.com>
2020-10-18 11:14:44 +0200

    Fix broken merge

--- src/vftr_stacks.c
+++ src/vftr_stacks.c
@@ -865,8 +865,6 @@ void vftr_print_function_stack (FILE *fp, int rank, char *func_name,
 	vftr_print_stacktree (fp, stack_tree->origin, 0, imbalances,
 			      n_spaces_max, fmt_calls, fmt_t, fmt_imba, fmt_stackid, &total_time);
 	vftr_print_html_output (func_name, stack_tree->origin);
-	vftr_print_stacktree (fp, stack_tree->origin, 0, &total_time, imbalances);
-	vftr_print_html_output (NULL, func_name, stack_tree->origin);
 	free (stack_tree);
 	fprintf (fp, "Total(%s): %lf sec. \n", func_name, (double)total_time * 1e-6);
 }
