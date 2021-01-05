2289ee26568b7596306ca372bdbc4eff9e5369a4
MeisterEule <chrweiss2718@gmail.com>
2021-01-05 08:55:59 +0100

    Checkbox to switch to n_calls (not yet perfect)

--- tools/crystalball/crystalball.py
+++ tools/crystalball/crystalball.py
@@ -45,25 +45,45 @@ def do_prediction():
     print ("Require at least one log file as argument!")
 
 def update_crystalball(wcontrol):
-  for f in wcontrol.open_files:
-    overview, func_dict = read_functions.create_dictionary(f)
-    ttk.Label(scrollable_frame, text = f + ":").pack()
-    ttk.Label(scrollable_frame, text = str(overview)).pack()
-    ttk.Label(scrollable_frame, text = "-------------------------").pack()
-    all_dicts.append(func_dict)
+  global global_dict_created
+  global global_dict
+  global n_calls_checked
+  print  ("Update crystalball: ", n_calls_checked.get())
+  if not global_dict_created:
+    for f in wcontrol.open_files:
+      overview, func_dict = read_functions.create_dictionary(f)
+      ttk.Label(scrollable_frame, text = f + ":").pack()
+      ttk.Label(scrollable_frame, text = str(overview)).pack()
+      ttk.Label(scrollable_frame, text = "-------------------------").pack()
+      all_dicts.append(func_dict)
 
-  global_dict = read_functions.synchronize_dictionaries(wcontrol.x_values, all_dicts)
+    global_dict = read_functions.synchronize_dictionaries(wcontrol.x_values, all_dicts)
+    global_dict_created = True
+
   axes = []
   func_names = []
   for i, prog_entry in enumerate(global_dict.values()):
-    l, = ax.plot(prog_entry.x, prog_entry.t)
-    axes.append(l)
+    print ("HUHU: ", n_calls_checked.get())
+    if n_calls_checked.get() == 1:
+      l, = ax.plot(prog_entry.x, prog_entry.n_calls)
+      axes.append(l)
+    else:
+      l, = ax.plot(prog_entry.x, prog_entry.t)
+      axes.append(l)
     func_names.append(prog_entry.func_name)
     if i == 5: break
   ax.legend(axes, func_names)
   ax.set_xlabel (wcontrol.xlabel)
-  if wcontrol.ylabel != '': ax.set_ylabel (wcnotrol.ylabel)
+  if wcontrol.ylabel != '':
+    ax.set_ylabel (wcnotrol.ylabel)
+  elif n_calls_checked.get() == 1:
+    ax.set_ylabel ("#Calls")
+  else:
+    ax.set_ylabel ("t[s]")
   plot_canvas.draw()
+
+def switch_to_ncalls():
+  if wcontrol.valid: update_crystalball(wcontrol)
     
 def create_sample():
   wcontrol.open_window()
@@ -73,6 +93,9 @@ def create_sample():
 window = tk.Tk()
 window.protocol ("WM_DELETE_WINDOW", window.destroy)
 window.wm_title ("The crystal ball performance predictor")
+
+global_dict_created = False
+global_dict = {}
 
 wcontrol = control_window.control_window(window)
 menubar = tk.Menu(window)
@@ -108,18 +131,27 @@ frame_plot = tk.Frame (master = window)
 frame_plot.grid(row=0, column=1)
 
 fig, ax = plt.subplots()
-ax.set_ylabel ("t[s]")
 axes = []
 func_names = []
 
 
 plot_canvas = FigureCanvasTkAgg(fig, master = frame_plot)
+#plot_canvas = FigureCanvasTk(fig, master = frame_plot)
 plot_canvas.draw()
 plot_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-toolbar = NavigationToolbar2Tk(plot_canvas, frame_plot)
-toolbar.update()
+#toolbar = NavigationToolbar2Tk(plot_canvas, frame_plot)
+#toolbar.update()
 plot_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+checkbox_frame = tk.Frame(frame_plot)
+#check_calls_label = tk.Label(checkbox_frame, text = "Show n_calls")
+#check_calls_label.grid(row=0, column=0)
+n_calls_checked = tk.IntVar()
+check_calls_box = tk.Checkbutton(checkbox_frame, text = "Show n_calls",
+                                 variable = n_calls_checked, onvalue = 1, offvalue = 0,
+                                 command = switch_to_ncalls).grid(row=0, column=1)
+checkbox_frame.pack()
+
 
 
 window.mainloop()
