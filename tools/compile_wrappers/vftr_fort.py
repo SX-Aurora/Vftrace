59dd5dad50bfe8edfac64958a9bd3db7577ab216
MeisterEule <chrweiss2718@gmail.com>
2021-01-26 12:52:57 +0100

    Create python script to automatically insert vftrace calls
    Missing: Insertion of use statements

---
+++ tools/compile_wrappers/vftr_fort.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+filename_in = sys.argv[1]
+filename_out = filename_in + ".vftr"
+allocate_pattern = re.compile("^[ ]*allocate[\ ,\(]", re.IGNORECASE)
+deallocate_pattern = re.compile("^[ ]*deallocate[\ ,\(]", re.IGNORECASE)
+
+def split_line (tot_string, is_alloc, is_dealloc):
+   # Remove white spaces and ampersands (&)
+   tot_string = re.sub(r"\s+", "", tot_string) # This also removes intermediate newlines, for some reason
+   tot_string = re.sub(r"&+", "", tot_string)
+   # Get everything in between the outer brackets (...)
+   tot_string = tot_string[tot_string.find("(")+1:-2]
+   # Get all the fields
+   if is_alloc:
+     fields = tot_string.split("),")
+   elif is_dealloc:
+     fields = tot_string.split(",")
+   # Check if there is a "STAT" argument in the last element of the list. If so, we return the list reduced by one element.
+   if re.search ("STAT", fields[-1], re.IGNORECASE):
+     fields.pop()
+   return fields
+
+def construct_vftrace_allocate_call (field):
+  # Input: name(n1,n2,... 
+  # Split at the commas. The first element is "<name>(<first_dim>", so we split again at the bracket.
+  tmp = field.split(",")
+  tmp2 = tmp[0].split("(")
+  name = tmp2[0]
+  dims = [tmp2[1]] + tmp[1:]
+  dim_string = ""
+  for i, dim in enumerate(dims):
+    # If there is a colon ("x1:x2") in the string, the dimension size is x2 - x1 + 1.
+    #Otherwise, the dimension bounds start at 1, and the size is dim.
+    if ":" in dim:
+      tmp = dim.split(":")
+      dim_string += "(" + tmp[1] + "-" + tmp[0] + "+1)"
+    else:
+      dim_string += dim
+    if i + 1 < len(dims):
+      dim_string += "*"
+  return "call vftrace_allocate(" + name + ", " + dim_string + ", storage_size(" + name + ")/8)\n"
+
+def construct_vftrace_deallocate_call (field):
+  # The input is simply the field name.
+  return "call vftrace_deallocate(" + field + ")\n"
+
+
+with open(filename_in, "r") as f_in, open(filename_out, "w") as f_out:
+  all_lines = f_in.readlines()
+  n_lines = len(all_lines)
+  for i_line, line in enumerate(all_lines):
+    is_alloc = allocate_pattern.match(line)
+    is_dealloc = deallocate_pattern.match(line)
+    if is_alloc or is_dealloc:
+      # Concatenate line breaks indicated by ampersands "&"
+      tot_string = line
+      line_tmp = line
+      i = i_line
+      while "&" in line_tmp: 
+        i = i + 1
+        if i < n_lines:
+          line_tmp = all_lines[i]
+        else:
+          break
+        tot_string += line_tmp
+      fields = split_line(tot_string, is_alloc, is_dealloc)
+      for field in fields:
+        if is_alloc:
+          f_out.write (construct_vftrace_allocate_call(field))
+        elif is_dealloc:
+          f_out.write (construct_vftrace_deallocate_call(field))
+
+    f_out.write (line)
