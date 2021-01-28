3d41840e84c0f9cb7dc3702e78b491d7055afb57
MeisterEule <chrweiss2718@gmail.com>
2021-01-28 15:29:09 +0100

    Accept more keywords in function definitions

--- tools/compile_wrappers/vftr_fort.py
+++ tools/compile_wrappers/vftr_fort.py
@@ -7,8 +7,8 @@ filename_in = sys.argv[1]
 filename_out = filename_in + ".vftr"
 allocate_pattern = re.compile("^[ ]*allocate[\ ,\(]", re.IGNORECASE)
 deallocate_pattern = re.compile("^[ ]*deallocate[\ ,\(]", re.IGNORECASE)
-subroutine_pattern = re.compile(r"^[ ]*(pure)?(elemental)?[ ]*subroutine[\s]+[\S]", re.IGNORECASE)
-function_pattern = re.compile(r"^[ ]*(pure)?(elemental)?[ ]*function[\s]+[\S]", re.IGNORECASE)
+subroutine_pattern = re.compile(r"^[\S\s]*(pure)?(elemental)?[\S\s]*subroutine[\s]+[\S]", re.IGNORECASE)
+function_pattern = re.compile(r"^[\S\s]*(pure)?(elemental)?[\S\s]*function[\s]+[\S]", re.IGNORECASE)
 
 #def split_alloc_argument (arg, check_any_bracket=False):
 def split_alloc_argument (arg, ignore_percent=False):
