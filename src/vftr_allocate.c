ede4f8865a4a0d6631e260dbb224d9224e9fe195
MeisterEule <christian.weiss@emea.nec.com>
2021-02-09 08:38:13 +0100

    Include function name in field registration

--- src/vftr_allocate.c
+++ src/vftr_allocate.c
@@ -24,6 +24,7 @@
 #include <string.h>
 
 #include "vftr_setup.h"
+#include "vftr_stacks.h"
 
 typedef struct allocate_list {
    char *name;
@@ -36,24 +37,31 @@ typedef struct allocate_list {
 } allocate_list_t;
 
 #define INIT_ALLOC_LIST 1000
-allocate_list_t *vftr_allocated_fields[1000];
+allocate_list_t *vftr_allocated_fields[INIT_ALLOC_LIST];
 int vftr_n_allocated_fields = 0;
 int vftr_max_allocated_fields = 0;
 
-void vftr_allocate_new_field (const char *name) {
+/**********************************************************************/
+
+void vftr_allocate_new_field (const char *name, const char *caller_function) {
    //vftr_allocated_fields[vftr_n_allocated_fields] = (allocate_list_t*) malloc (sizeof(allocate_list_t));
    allocate_list_t *new_field = (allocate_list_t*) malloc (sizeof(allocate_list_t));
    new_field->name = strdup(name);
-   new_field->caller = NULL;
+   new_field->caller = strdup(caller_function);
    new_field->n_calls = 0;
    new_field->allocated_memory = 0;
-   new_field->id = vftr_jenkins_murmur_64_hash (strlen(name), (uint8_t*)name);
+   char name_and_caller[strlen(name) + strlen(caller_function)];
+   snprintf (name_and_caller, sizeof(name_and_caller), "%s%s", name, caller_function);
+   //new_field->id = vftr_jenkins_murmur_64_hash (strlen(name), (uint8_t*)name);
+   new_field->id = vftr_jenkins_murmur_64_hash (strlen(name_and_caller), (uint8_t*)name_and_caller);
    //if (vftr_mpirank == 0) printf ("THIS ID: %lld\n", new_field->id);
    new_field->open = true;
    vftr_allocated_fields[vftr_max_allocated_fields++] = new_field;
 }
 
-int vftr_allocate_find_field (const char *name) {
+/**********************************************************************/
+
+int vftr_allocate_find_field (const char *name, const char *caller_function) {
    uint64_t this_id = vftr_jenkins_murmur_64_hash (strlen(name), (uint8_t*)name);
    for (int i = vftr_max_allocated_fields - 1; i >=0; i--) {
       //if (vftr_mpirank == 0) printf ("Compare IDs: %llu %llu\n", this_id, vftr_allocated_fields[i]->id);
@@ -62,10 +70,14 @@ int vftr_allocate_find_field (const char *name) {
    return -1;
 }
 
+/**********************************************************************/
+
 void vftr_allocate_count (int index, long long alloc_size) {
    vftr_allocated_fields[index]->n_calls++;
    vftr_allocated_fields[index]->allocated_memory += alloc_size;
 }
+
+/**********************************************************************/
 
 void vftr_allocate_set_open_state (int index) {
    if (vftr_allocated_fields[index]->open) {
@@ -75,40 +87,33 @@ void vftr_allocate_set_open_state (int index) {
    }
 }
 
+/**********************************************************************/
+
 //void vftrace_allocate (const char *s, const int *dims, const int *n, const int *element_size) {
 void vftrace_allocate (const char *s, const int *n_elements, const int *element_size) {
-   //if (vftr_mpirank == 0) {
-   //   printf ("REGISTER ALLOCATE: %s ", s);
-   //   for (int i = 0; i < *n; i++) {
-   //     printf ("%d ", dims[i]);
-   //   }
-   //   printf ("\n");
-   //}
-   //vftr_allocate_new_field (s);
-   int index = vftr_allocate_find_field (s);
-   //if (vftr_mpirank == 0) printf ("Already there? %s\n", index >= 0 ? "YES" : "NO");
+   int index = vftr_allocate_find_field (s, vftr_fstack->name);
    if (index < 0) {
-      vftr_allocate_new_field (s);
+      vftr_allocate_new_field (s, vftr_fstack->name);
       index = vftr_max_allocated_fields - 1;
    } else {
       vftr_allocate_set_open_state (index);
    }
    long long this_alloc = (long long)((*element_size) * (*n_elements));
-   //for (int i = 0; i < *n; i++) {
-   //  this_alloc *= dims[i];
-   //}
    vftr_allocate_count (index, this_alloc);
    vftr_n_allocated_fields++; 
    
-   //printf ("REGISTER: %s %d %d %d\n", s, *n, dims[0], dims[1]);
 }
+
+/**********************************************************************/
 
 void vftrace_deallocate (const char *s) {
    //if (vftr_mpirank == 0) printf ("CALL VFTRACE_DEALLOCATE: %s\n", s);
-   int index = vftr_allocate_find_field (s);
+   int index = vftr_allocate_find_field (s, vftr_fstack->name);
    vftr_allocated_fields[index]->open = false;
    vftr_n_allocated_fields--;
 }
+
+/**********************************************************************/
 
 void vftr_allocate_finalize () {
    if (vftr_mpirank == 0) {
@@ -116,15 +121,16 @@ void vftr_allocate_finalize () {
       printf ("Registered fields: %d\n", vftr_max_allocated_fields);
       printf ("Unresolved allocations: %d\n", vftr_n_allocated_fields);
       printf ("***************************************************\n");
-      printf ("%s %s %s %s\n", "Field name", "MB total", "calls", "MB / call");
+      printf ("%s %s %s %s %s\n", "Field name", "Called by", "MB total", "calls", "MB / call");
       for (int i = 0; i < vftr_max_allocated_fields; i++) {
         double mb = (double)vftr_allocated_fields[i]->allocated_memory / 1024 / 1024;
-        printf ("%s %lf %d %lf\n", vftr_allocated_fields[i]->name, mb, vftr_allocated_fields[i]->n_calls, mb / vftr_allocated_fields[i]->n_calls);
+        printf ("%s %s %lf %d %lf\n", vftr_allocated_fields[i]->name, vftr_allocated_fields[i]->caller, 
+                mb, vftr_allocated_fields[i]->n_calls, mb / vftr_allocated_fields[i]->n_calls);
       }
    }
 }
 
-
+/**********************************************************************/
 
 
 
