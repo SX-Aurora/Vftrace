8d23302726c59e6b9bc80426731330aff4595bde
MeisterEule <christian.weiss@emea.nec.com>
2021-05-21 07:13:08 +0000

    Fix environment

--- src/vftr_environment.c
+++ src/vftr_environment.c
@@ -317,7 +317,7 @@ char *vftr_profile_sorting_method_string () {
 /**********************************************************************/
 
 void vftr_read_environment () {
-    vftr_n_env_variables = 33;
+    vftr_n_env_variables = 34;
     vftr_env_variable_names = (char**)malloc(vftr_n_env_variables * sizeof(char*));
     vftr_env_counter = 0;
     
