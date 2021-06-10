45a79692b81352320c773839b5fc1dbbd6a2994b
MeisterEule <christian.weiss@emea.nec.com>
2021-06-10 14:12:47 +0200

    Fix environment_1

--- test/units/environment_1.sh
+++ test/units/environment_1.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -x
-vftr_binary=environment_1
-
-output_file=environment_1.out
+test_name=environment_1
+output_file=$test_name.out
 ref_file=ref_output/$output_file
 
 if [ "x$HAS_MPI" == "xYES" ]; then
-   ${MPI_EXEC} ${MPI_OPTS} ${NP} 1 ./${vftr_binary} ${output_file}
+   ${MPI_EXEC} ${MPI_OPTS} ${NP} 1 ./${test_name} > ${output_file}
 else
-   ./${vftr_binary} ${output_file}
+   ./${test_name} > ${output_file}
 fi
 
 if [ "$?" == "0" ]; then
