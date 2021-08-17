3381df29cb744af404e6d733256ae2e511d25093
MeisterEule <christian.weiss@emea.nec.com>
2021-08-17 10:17:44 +0200

    Add second test and functionality

--- test/user_functions/cshow_stack2.sh
+++ test/user_functions/cshow_stack2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-vftr_binary=cshow_stack
+vftr_binary=cshow_stack2
 nprocs=1
 
 if [ "x$HAS_MPI" == "xYES" ]; then
@@ -13,11 +13,11 @@ grep "Stack trees traced by user: 2" ${vftr_binary}_0.log
 if [ $? -ne "0" ]; then
   exit 1;
 fi
-grep "3: func2<func1<main<init" ${vftr_binary}_0.log
+grep "2: func1<main<init" ${vftr_binary}_0.log
 if [ $? -ne "0" ]; then
   exit 1;
 fi
-grep "4: func2<main<init" ${vftr_binary}_0.log
+grep "3: func2<main<init" ${vftr_binary}_0.log
 if [ $? -ne "0" ]; then
   exit 1;
 fi
