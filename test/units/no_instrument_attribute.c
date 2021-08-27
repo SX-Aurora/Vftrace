4d28d8f351087b7d78deafcc12bc5a685b570893
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-08-27 10:38:24 +0200

    made no instrument attribute check MPI-ready

--- test/units/no_instrument_attribute.c
+++ test/units/no_instrument_attribute.c
@@ -2,6 +2,12 @@ __attribute__((no_instrument_function))
 int not_instrumented_function () {
    return 0;
 }
-int main() {
+int main(int argc, int **argv) {
+#ifdef _MPI
+   MPI_Init(&argc, &argv);
+#endif
    not_instrumented_function();
+#ifdef _MPI
+   MPI_Finalize();
+#endif
 }
