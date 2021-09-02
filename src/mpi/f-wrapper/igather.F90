39fce72630dcf6704fd37213a66f282ae66222d3
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 09:47:00 +0200

    added vftrace-mpi-logging status switch to igather fortran-wrappers.

--- src/mpi/f-wrapper/igather.F90
+++ src/mpi/f-wrapper/igather.F90
@@ -21,6 +21,10 @@ SUBROUTINE MPI_IGATHER(SENDBUF, SENDCOUNT, SENDTYPE, &
                        ROOT, COMM, REQUEST, ERROR)
    USE vftr_mpi_igather_f2c_finterface, &
       ONLY : vftr_MPI_Igather_f2c
+   USE vftr_mpi_logging_F, &
+      ONLY : vftr_no_mpi_logging_F
+   USE mpi, &
+      ONLY : PMPI_IGATHER
    IMPLICIT NONE
    INTEGER SENDBUF
    INTEGER SENDCOUNT
@@ -33,9 +37,15 @@ SUBROUTINE MPI_IGATHER(SENDBUF, SENDCOUNT, SENDTYPE, &
    INTEGER REQUEST
    INTEGER ERROR
 
-   CALL vftr_MPI_Igather_f2c(SENDBUF, SENDCOUNT, SENDTYPE, &
-                             RECVBUF, RECVCOUNT, RECVTYPE, &
-                             ROOT, COMM, REQUEST, ERROR)
+   IF (vftr_no_mpi_logging_F()) THEN
+      CALL PMPI_IGATHER(SENDBUF, SENDCOUNT, SENDTYPE, &
+                        RECVBUF, RECVCOUNT, RECVTYPE, &
+                        ROOT, COMM, REQUEST, ERROR)
+   ELSE
+      CALL vftr_MPI_Igather_f2c(SENDBUF, SENDCOUNT, SENDTYPE, &
+                                RECVBUF, RECVCOUNT, RECVTYPE, &
+                                ROOT, COMM, REQUEST, ERROR)
+   END IF
 
 END SUBROUTINE MPI_IGATHER
 
