f45606e262c089496c4f24b2a4f155dab9118694
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 10:55:49 +0200

    added vftrace-mpi-logging status switch to get_accumulate c-wrappers.

--- src/mpi/c-wrapper/get_accumulate.c
+++ src/mpi/c-wrapper/get_accumulate.c
@@ -19,6 +19,7 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "vftr_mpi_get_accumulate.h"
 
 int MPI_Get_accumulate(const void *origin_addr, int origin_count,
@@ -26,10 +27,17 @@ int MPI_Get_accumulate(const void *origin_addr, int origin_count,
                        int result_count, MPI_Datatype result_datatype,
                        int target_rank, MPI_Aint target_disp, int target_count,
                        MPI_Datatype target_datatype, MPI_Op op, MPI_Win win) {
-   return vftr_MPI_Get_accumulate(origin_addr, origin_count, origin_datatype,
-                                  result_addr, result_count, result_datatype,
-                                  target_rank, target_disp, target_count, target_datatype,
-                                  op, win);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Get_accumulate(origin_addr, origin_count, origin_datatype,
+                                 result_addr, result_count, result_datatype,
+                                 target_rank, target_disp, target_count, target_datatype,
+                                 op, win);
+   } else {
+      return vftr_MPI_Get_accumulate(origin_addr, origin_count, origin_datatype,
+                                     result_addr, result_count, result_datatype,
+                                     target_rank, target_disp, target_count, target_datatype,
+                                     op, win);
+   }
 }
 
 #endif
