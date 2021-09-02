8b9e35aead1ad0fd56b9dfd2445930ae37dc1f3a
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 10:55:51 +0200

    added vftrace-mpi-logging status switch to ireduce_scatter_block c-wrappers.

--- src/mpi/c-wrapper/ireduce_scatter_block.c
+++ src/mpi/c-wrapper/ireduce_scatter_block.c
@@ -19,12 +19,19 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "vftr_mpi_ireduce_scatter_block.h"
 
 int MPI_Ireduce_scatter_block(const void *sendbuf, void *recvbuf, int recvcount,
                               MPI_Datatype datatype, MPI_Op op, MPI_Comm comm,
                               MPI_Request *request) {
-   return vftr_MPI_Ireduce_scatter_block(sendbuf, recvbuf, recvcount, datatype, op, comm, request);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Ireduce_scatter_block(sendbuf, recvbuf, recvcount,
+                                        datatype, op, comm, request);
+   } else {
+      return vftr_MPI_Ireduce_scatter_block(sendbuf, recvbuf, recvcount,
+                                            datatype, op, comm, request);
+   }
 }
 
 #endif
