33aef3a970a1baa642543d679dcf789004ea5e58
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 10:55:48 +0200

    added vftrace-mpi-logging status switch to alltoallv c-wrappers.

--- src/mpi/c-wrapper/alltoallv.c
+++ src/mpi/c-wrapper/alltoallv.c
@@ -19,14 +19,20 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "vftr_mpi_alltoallv.h"
 
 int MPI_Alltoallv(const void *sendbuf, const int *sendcounts,
                   const int *sdispls, MPI_Datatype sendtype, void *recvbuf,
                   const int *recvcounts, const int *rdispls,
                   MPI_Datatype recvtype, MPI_Comm comm) {
-   return vftr_MPI_Alltoallv(sendbuf, sendcounts, sdispls, sendtype,
-                             recvbuf, recvcounts, rdispls, recvtype, comm);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Alltoallv(sendbuf, sendcounts, sdispls, sendtype,
+                            recvbuf, recvcounts, rdispls, recvtype, comm);
+   } else {
+      return vftr_MPI_Alltoallv(sendbuf, sendcounts, sdispls, sendtype,
+                                recvbuf, recvcounts, rdispls, recvtype, comm);
+   }
 }
 
 #endif
