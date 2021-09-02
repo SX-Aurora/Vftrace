b6f70da38ba28d1fa9528ee0ab99da936a61f7a4
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 09:47:01 +0200

    added vftrace-mpi-logging status switch to iscatter fortran-wrappers.

--- src/mpi/f-wrapper/iscatter.F90
+++ src/mpi/f-wrapper/iscatter.F90
@@ -21,6 +21,10 @@ SUBROUTINE MPI_ISCATTER(SENDBUF, SENDCOUNT, SENDTYPE, &
                         ROOT, COMM, REQUEST, ERROR)
    USE vftr_mpi_iscatter_f2c_finterface, &
       ONLY : vftr_MPI_Iscatter_f2c
+   USE vftr_mpi_logging_F, &
+      ONLY : vftr_no_mpi_logging_F
+   USE mpi, &
+      ONLY : PMPI_ISCATTER
    IMPLICIT NONE
    INTEGER SENDBUF
    INTEGER SENDCOUNT
@@ -33,9 +37,15 @@ SUBROUTINE MPI_ISCATTER(SENDBUF, SENDCOUNT, SENDTYPE, &
    INTEGER REQUEST
    INTEGER ERROR
 
-   CALL vftr_MPI_Iscatter_f2c(SENDBUF, SENDCOUNT, SENDTYPE, &
-                              RECVBUF, RECVCOUNT, RECVTYPE, &
-                              ROOT, COMM, REQUEST, ERROR)
+   IF (vftr_no_mpi_logging_F()) THEN
+      CALL PMPI_ISCATTER(SENDBUF, SENDCOUNT, SENDTYPE, &
+                         RECVBUF, RECVCOUNT, RECVTYPE, &
+                         ROOT, COMM, REQUEST, ERROR)
+   ELSE
+      CALL vftr_MPI_Iscatter_f2c(SENDBUF, SENDCOUNT, SENDTYPE, &
+                                 RECVBUF, RECVCOUNT, RECVTYPE, &
+                                 ROOT, COMM, REQUEST, ERROR)
+   END IF
 
 END SUBROUTINE MPI_ISCATTER
 
