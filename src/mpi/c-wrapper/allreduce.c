7cea767c0dbe41b70d7a0720370d909f021d0cbe
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 10:55:48 +0200

    added vftrace-mpi-logging status switch to allreduce c-wrappers.

--- src/mpi/c-wrapper/allreduce.c
+++ src/mpi/c-wrapper/allreduce.c
@@ -19,11 +19,16 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "vftr_mpi_allreduce.h"
 
 int MPI_Allreduce(const void *sendbuf, void *recvbuf, int count,
                   MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
-   return vftr_MPI_Allreduce(sendbuf, recvbuf, count, datatype, op, comm);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Allreduce(sendbuf, recvbuf, count, datatype, op, comm);
+   } else {
+      return vftr_MPI_Allreduce(sendbuf, recvbuf, count, datatype, op, comm);
+   }
 }
 
 #endif
