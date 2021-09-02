b57906202b85f042f7b0ea10a49ad0f1e1a1c889
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 10:55:51 +0200

    added vftrace-mpi-logging status switch to isend c-wrappers.

--- src/mpi/c-wrapper/isend.c
+++ src/mpi/c-wrapper/isend.c
@@ -19,12 +19,17 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "vftr_mpi_isend.h"
 
 int MPI_Isend(const void *buf, int count, MPI_Datatype datatype,
               int dest, int tag, MPI_Comm comm,
               MPI_Request *request) {
-   return vftr_MPI_Isend(buf, count, datatype, dest, tag, comm, request);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Isend(buf, count, datatype, dest, tag, comm, request);
+   } else {
+      return vftr_MPI_Isend(buf, count, datatype, dest, tag, comm, request);
+   }
 }
 
 #endif
