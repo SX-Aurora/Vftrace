f5ada0667be09cb344451c95654b0bb86a4ee474
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 09:47:01 +0200

    added vftrace-mpi-logging status switch to request_free fortran-wrappers.

--- src/mpi/f-wrapper/request_free.F90
+++ src/mpi/f-wrapper/request_free.F90
@@ -19,11 +19,19 @@
 SUBROUTINE MPI_REQUEST_FREE(REQUEST, ERROR)
    USE vftr_mpi_request_free_f2c_finterface, &
       ONLY : vftr_MPI_Request_free_f2c
+   USE vftr_mpi_logging_F, &
+      ONLY : vftr_no_mpi_logging_F
+   USE mpi, &
+      ONLY : PMPI_REQUEST_FREE
    IMPLICIT NONE
    INTEGER REQUEST
    INTEGER ERROR
 
-   CALL vftr_MPI_Request_free_f2c(REQUEST, ERROR)
+   IF (vftr_no_mpi_logging_F()) THEN
+      CALL PMPI_REQUEST_FREE(REQUEST, ERROR)
+   ELSE
+      CALL vftr_MPI_Request_free_f2c(REQUEST, ERROR)
+   END IF
 
 END SUBROUTINE MPI_REQUEST_FREE
 
