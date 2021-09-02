00b7f8e3a3412eefbe0b126593d3d10919f31c70
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 10:55:49 +0200

    added vftrace-mpi-logging status switch to bsend c-wrappers.

--- src/mpi/c-wrapper/bsend.c
+++ src/mpi/c-wrapper/bsend.c
@@ -19,11 +19,16 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "vftr_mpi_bsend.h"
 
 int MPI_Bsend(const void *buf, int count, MPI_Datatype datatype,
              int dest, int tag, MPI_Comm comm) {
-   return vftr_MPI_Bsend(buf, count, datatype, dest, tag, comm);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Bsend(buf, count, datatype, dest, tag, comm);
+   } else {
+      return vftr_MPI_Bsend(buf, count, datatype, dest, tag, comm);
+   }
 }
 
 #endif
