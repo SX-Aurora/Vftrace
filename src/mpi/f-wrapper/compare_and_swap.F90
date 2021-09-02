c3897eefccc0eddc1cc135b9ac6d53b1fcc878ab
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 09:46:59 +0200

    added vftrace-mpi-logging status switch to compare_and_swap fortran-wrappers.

--- src/mpi/f-wrapper/compare_and_swap.F90
+++ src/mpi/f-wrapper/compare_and_swap.F90
@@ -21,7 +21,11 @@ SUBROUTINE MPI_COMPARE_AND_SWAP(ORIGIN_ADDR, COMPARE_ADDR, RESULT_ADDR, &
                                 WIN, ERROR)
    USE vftr_mpi_compare_and_swap_f2c_finterface, &
       ONLY : vftr_MPI_Compare_and_swap_f2c
-   USE mpi, ONLY : MPI_ADDRESS_KIND
+   USE vftr_mpi_logging_F, &
+      ONLY : vftr_no_mpi_logging_F
+   USE mpi, &
+      ONLY : PMPI_COMPARE_AND_SWAP, &
+             MPI_ADDRESS_KIND
    IMPLICIT NONE
    INTEGER ORIGIN_ADDR
    INTEGER COMPARE_ADDR
@@ -32,9 +36,15 @@ SUBROUTINE MPI_COMPARE_AND_SWAP(ORIGIN_ADDR, COMPARE_ADDR, RESULT_ADDR, &
    INTEGER WIN
    INTEGER ERROR
 
-   CALL vftr_MPI_Compare_and_swap_f2c(ORIGIN_ADDR, COMPARE_ADDR, RESULT_ADDR, &
-                                      DATATYPE, TARGET_RANK, TARGET_DISP, &
-                                      WIN, ERROR)
+   IF (vftr_no_mpi_logging_F()) THEN
+      CALL PMPI_COMPARE_AND_SWAP(ORIGIN_ADDR, COMPARE_ADDR, RESULT_ADDR, &
+                                 DATATYPE, TARGET_RANK, TARGET_DISP, &
+                                 WIN, ERROR)
+   ELSE
+      CALL vftr_MPI_Compare_and_swap_f2c(ORIGIN_ADDR, COMPARE_ADDR, RESULT_ADDR, &
+                                         DATATYPE, TARGET_RANK, TARGET_DISP, &
+                                         WIN, ERROR)
+   END IF
 
 END SUBROUTINE MPI_COMPARE_AND_SWAP
 
