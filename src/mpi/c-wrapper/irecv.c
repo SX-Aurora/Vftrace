180e8113c2bb5056a68f669078f859c637ee7614
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 10:55:50 +0200

    added vftrace-mpi-logging status switch to irecv c-wrappers.

--- src/mpi/c-wrapper/irecv.c
+++ src/mpi/c-wrapper/irecv.c
@@ -19,11 +19,16 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "vftr_mpi_irecv.h"
 
 int MPI_Irecv(void *buf, int count, MPI_Datatype datatype,
               int source, int tag, MPI_Comm comm, MPI_Request *request) {
-   return vftr_MPI_Irecv(buf, count, datatype, source, tag, comm, request);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Irecv(buf, count, datatype, source, tag, comm, request);
+   } else {
+      return vftr_MPI_Irecv(buf, count, datatype, source, tag, comm, request);
+   }
 }
 
 #endif
