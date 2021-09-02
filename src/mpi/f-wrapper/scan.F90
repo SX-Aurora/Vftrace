4a45c1a1047e50952e901247a062a5b3403a3880
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 09:47:02 +0200

    added vftrace-mpi-logging status switch to scan fortran-wrappers.

--- src/mpi/f-wrapper/scan.F90
+++ src/mpi/f-wrapper/scan.F90
@@ -20,6 +20,10 @@ SUBROUTINE MPI_SCAN(SENDBUF, RECVBUF, COUNT, DATATYPE, &
                     OP, COMM, ERROR)
    USE vftr_mpi_scan_f2c_finterface, &
       ONLY : vftr_MPI_Scan_f2c
+   USE vftr_mpi_logging_F, &
+      ONLY : vftr_no_mpi_logging_F
+   USE mpi, &
+      ONLY : PMPI_SCAN
    IMPLICIT NONE
    INTEGER SENDBUF
    INTEGER RECVBUF
@@ -29,8 +33,13 @@ SUBROUTINE MPI_SCAN(SENDBUF, RECVBUF, COUNT, DATATYPE, &
    INTEGER COMM
    INTEGER ERROR
 
-   CALL vftr_MPI_Scan_f2c(SENDBUF, RECVBUF, COUNT, DATATYPE, &
-                          OP, COMM, ERROR)
+   IF (vftr_no_mpi_logging_F()) THEN
+      CALL PMPI_SCAN(SENDBUF, RECVBUF, COUNT, DATATYPE, &
+                     OP, COMM, ERROR)
+   ELSE
+      CALL vftr_MPI_Scan_f2c(SENDBUF, RECVBUF, COUNT, DATATYPE, &
+                             OP, COMM, ERROR)
+   END IF
 
 END SUBROUTINE MPI_SCAN
 
