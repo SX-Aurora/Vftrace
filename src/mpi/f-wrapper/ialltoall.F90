c10da713bdbf988ce2c27afe0558fb03045950bd
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 09:47:00 +0200

    added vftrace-mpi-logging status switch to ialltoall fortran-wrappers.

--- src/mpi/f-wrapper/ialltoall.F90
+++ src/mpi/f-wrapper/ialltoall.F90
@@ -21,6 +21,10 @@ SUBROUTINE MPI_IALLTOALL(SENDBUF, SENDCOUNT, SENDTYPE, &
                          COMM, REQUEST,ERROR)
    USE vftr_mpi_ialltoall_f2c_finterface, &
       ONLY : vftr_MPI_Ialltoall_f2c
+   USE vftr_mpi_logging_F, &
+      ONLY : vftr_no_mpi_logging_F
+   USE mpi, &
+      ONLY : PMPI_IALLTOALL
    IMPLICIT NONE
    INTEGER SENDBUF
    INTEGER SENDCOUNT
@@ -32,9 +36,15 @@ SUBROUTINE MPI_IALLTOALL(SENDBUF, SENDCOUNT, SENDTYPE, &
    INTEGER REQUEST
    INTEGER ERROR
 
-   CALL vftr_MPI_Ialltoall_f2c(SENDBUF, SENDCOUNT, SENDTYPE, &
-                               RECVBUF, RECVCOUNT, RECVTYPE, &
-                               COMM, REQUEST, ERROR)
+   IF (vftr_no_mpi_logging_F()) THEN
+      CALL PMPI_IALLTOALL(SENDBUF, SENDCOUNT, SENDTYPE, &
+                          RECVBUF, RECVCOUNT, RECVTYPE, &
+                          COMM, REQUEST,ERROR)
+   ELSE
+      CALL vftr_MPI_Ialltoall_f2c(SENDBUF, SENDCOUNT, SENDTYPE, &
+                                  RECVBUF, RECVCOUNT, RECVTYPE, &
+                                  COMM, REQUEST, ERROR)
+   END IF
 
 END SUBROUTINE MPI_IALLTOALL
 
