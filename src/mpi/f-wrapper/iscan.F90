5f184e6eeced866b760f426045bd1e172d7b8c00
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 09:47:01 +0200

    added vftrace-mpi-logging status switch to iscan fortran-wrappers.

--- src/mpi/f-wrapper/iscan.F90
+++ src/mpi/f-wrapper/iscan.F90
@@ -20,6 +20,10 @@ SUBROUTINE MPI_ISCAN(SENDBUF, RECVBUF, COUNT, DATATYPE, &
                      OP, COMM, REQUEST, ERROR)
    USE vftr_mpi_iscan_f2c_finterface, &
       ONLY : vftr_MPI_Iscan_f2c
+   USE vftr_mpi_logging_F, &
+      ONLY : vftr_no_mpi_logging_F
+   USE mpi, &
+      ONLY : PMPI_ISCAN
    IMPLICIT NONE
    INTEGER SENDBUF
    INTEGER RECVBUF
@@ -30,8 +34,13 @@ SUBROUTINE MPI_ISCAN(SENDBUF, RECVBUF, COUNT, DATATYPE, &
    INTEGER REQUEST
    INTEGER ERROR
 
-   CALL vftr_MPI_Iscan_f2c(SENDBUF, RECVBUF, COUNT, DATATYPE, &
-                           OP, COMM, REQUEST, ERROR)
+   IF (vftr_no_mpi_logging_F()) THEN
+      CALL PMPI_ISCAN(SENDBUF, RECVBUF, COUNT, DATATYPE, &
+                      OP, COMM, REQUEST, ERROR)
+   ELSE
+      CALL vftr_MPI_Iscan_f2c(SENDBUF, RECVBUF, COUNT, DATATYPE, &
+                              OP, COMM, REQUEST, ERROR)
+   END IF
 
 END SUBROUTINE MPI_ISCAN
 
