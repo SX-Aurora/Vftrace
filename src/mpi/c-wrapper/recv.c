ed3c995fa25037c5d1adb135d2358b3a91ad8622
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 10:55:51 +0200

    added vftrace-mpi-logging status switch to recv c-wrappers.

--- src/mpi/c-wrapper/recv.c
+++ src/mpi/c-wrapper/recv.c
@@ -19,11 +19,16 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "vftr_mpi_recv.h"
 
 int MPI_Recv(void *buf, int count, MPI_Datatype datatype,
              int source, int tag, MPI_Comm comm, MPI_Status *status) {
-   return vftr_MPI_Recv(buf, count, datatype, source, tag, comm, status);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Recv(buf, count, datatype, source, tag, comm, status);
+   } else {
+      return vftr_MPI_Recv(buf, count, datatype, source, tag, comm, status);
+   }
 }
 
 #endif
