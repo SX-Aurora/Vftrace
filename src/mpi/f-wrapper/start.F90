9765c27e3d34e93f500db217836be4b875a522d0
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 09:47:02 +0200

    added vftrace-mpi-logging status switch to start fortran-wrappers.

--- src/mpi/f-wrapper/start.F90
+++ src/mpi/f-wrapper/start.F90
@@ -19,11 +19,19 @@
 SUBROUTINE MPI_START(REQUEST, ERROR)
    USE vftr_mpi_start_f2c_finterface, &
       ONLY : vftr_MPI_Start_f2c
+   USE vftr_mpi_logging_F, &
+      ONLY : vftr_no_mpi_logging_F
+   USE mpi, &
+      ONLY : PMPI_START
    IMPLICIT NONE
    INTEGER REQUEST
    INTEGER ERROR
 
-   CALL vftr_MPI_Start_f2c(REQUEST, ERROR)
+   IF (vftr_no_mpi_logging_F()) THEN
+      CALL PMPI_START(REQUEST, ERROR)
+   ELSE
+      CALL vftr_MPI_Start_f2c(REQUEST, ERROR)
+   END IF
 
 END SUBROUTINE MPI_START
 
