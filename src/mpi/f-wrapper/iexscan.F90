db43d60e2d943ffab80e9cea0b8ed468f471b0cd
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 09:47:00 +0200

    added vftrace-mpi-logging status switch to iexscan fortran-wrappers.

--- src/mpi/f-wrapper/iexscan.F90
+++ src/mpi/f-wrapper/iexscan.F90
@@ -20,6 +20,10 @@ SUBROUTINE MPI_IEXSCAN(SENDBUF, RECVBUF, COUNT, DATATYPE, &
                        OP, COMM, REQUEST, ERROR)
    USE vftr_mpi_iexscan_f2c_finterface, &
       ONLY : vftr_MPI_Iexscan_f2c
+   USE vftr_mpi_logging_F, &
+      ONLY : vftr_no_mpi_logging_F
+   USE mpi, &
+      ONLY : PMPI_IEXSCAN
    IMPLICIT NONE
    INTEGER SENDBUF
    INTEGER RECVBUF
@@ -30,8 +34,13 @@ SUBROUTINE MPI_IEXSCAN(SENDBUF, RECVBUF, COUNT, DATATYPE, &
    INTEGER REQUEST
    INTEGER ERROR
 
-   CALL vftr_MPI_Iexscan_f2c(SENDBUF, RECVBUF, COUNT, DATATYPE, &
-                             OP, COMM, REQUEST, ERROR)
+   IF (vftr_no_mpi_logging_F()) THEN
+      CALL PMPI_IEXSCAN(SENDBUF, RECVBUF, COUNT, DATATYPE, &
+                        OP, COMM, REQUEST, ERROR)
+   ELSE
+      CALL vftr_MPI_Iexscan_f2c(SENDBUF, RECVBUF, COUNT, DATATYPE, &
+                                OP, COMM, REQUEST, ERROR)
+   END IF
 
 END SUBROUTINE MPI_IEXSCAN
 
