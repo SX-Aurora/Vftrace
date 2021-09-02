fbb6273427e6a8c0725da811bd90bc957a028086
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 10:55:52 +0200

    added vftrace-mpi-logging status switch to scan c-wrappers.

--- src/mpi/c-wrapper/scan.c
+++ src/mpi/c-wrapper/scan.c
@@ -19,11 +19,16 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "vftr_mpi_scan.h"
 
 int MPI_Scan(const void *sendbuf, void *recvbuf, int count,
              MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
-   return vftr_MPI_Scan(sendbuf, recvbuf, count, datatype, op, comm);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Scan(sendbuf, recvbuf, count, datatype, op, comm);
+   } else {
+      return vftr_MPI_Scan(sendbuf, recvbuf, count, datatype, op, comm);
+   }
 }
 
 #endif
