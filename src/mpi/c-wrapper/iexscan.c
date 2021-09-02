9bea1b558abb683fb0261b6aeb05c847853c227c
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 10:55:50 +0200

    added vftrace-mpi-logging status switch to iexscan c-wrappers.

--- src/mpi/c-wrapper/iexscan.c
+++ src/mpi/c-wrapper/iexscan.c
@@ -19,12 +19,17 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "vftr_mpi_iexscan.h"
 
 int MPI_Iexscan(const void *sendbuf, void *recvbuf, int count,
                 MPI_Datatype datatype, MPI_Op op, MPI_Comm comm,
                 MPI_Request *request) {
-   return vftr_MPI_Iexscan(sendbuf, recvbuf, count, datatype, op, comm, request);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Iexscan(sendbuf, recvbuf, count, datatype, op, comm, request);
+   } else {
+      return vftr_MPI_Iexscan(sendbuf, recvbuf, count, datatype, op, comm, request);
+   }
 }
 
 #endif
