9e578c8b691684b007666efe89fa10f9df6aa11d
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 09:47:00 +0200

    added vftrace-mpi-logging status switch to iallgatherv fortran-wrappers.

--- src/mpi/f-wrapper/iallgatherv.F90
+++ src/mpi/f-wrapper/iallgatherv.F90
@@ -21,6 +21,10 @@ SUBROUTINE MPI_IALLGATHERV(SENDBUF, SENDCOUNT, SENDTYPE, &
                            RECVTYPE, COMM, REQUEST, ERROR)
    USE vftr_mpi_iallgatherv_f2c_finterface, &
       ONLY : vftr_MPI_Iallgatherv_f2c
+   USE vftr_mpi_logging_F, &
+      ONLY : vftr_no_mpi_logging_F
+   USE mpi, &
+      ONLY : PMPI_IALLGATHERV
    IMPLICIT NONE
    INTEGER ::  SENDBUF
    INTEGER ::  SENDCOUNT
@@ -33,9 +37,15 @@ SUBROUTINE MPI_IALLGATHERV(SENDBUF, SENDCOUNT, SENDTYPE, &
    INTEGER ::  REQUEST
    INTEGER ::  ERROR
 
-   CALL vftr_MPI_Iallgatherv_f2c(SENDBUF, SENDCOUNT, SENDTYPE, &
-                                 RECVBUF, RECVCOUNTS, DISPLS, &
-                                 RECVTYPE, COMM, REQUEST, ERROR)
+   IF (vftr_no_mpi_logging_F()) THEN
+      CALL PMPI_IALLGATHERV(SENDBUF, SENDCOUNT, SENDTYPE, &
+                            RECVBUF, RECVCOUNTS, DISPLS, &
+                            RECVTYPE, COMM, REQUEST, ERROR)
+   ELSE
+      CALL vftr_MPI_Iallgatherv_f2c(SENDBUF, SENDCOUNT, SENDTYPE, &
+                                    RECVBUF, RECVCOUNTS, DISPLS, &
+                                    RECVTYPE, COMM, REQUEST, ERROR)
+   END IF
 
 END SUBROUTINE MPI_IALLGATHERV
 
