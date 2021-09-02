25b41dfdedbc278d2d8f5d9f1d4ad2a17de3e112
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 09:47:01 +0200

    added vftrace-mpi-logging status switch to raccumulate fortran-wrappers.

--- src/mpi/f-wrapper/raccumulate.F90
+++ src/mpi/f-wrapper/raccumulate.F90
@@ -21,7 +21,11 @@ SUBROUTINE MPI_RACCUMULATE(ORIGIN_ADDR, ORIGIN_COUNT, ORIGIN_DATATYPE, &
                            TARGET_DATATYPE, OP, WIN, REQUEST, ERROR)
    USE vftr_mpi_raccumulate_f2c_finterface, &
       ONLY : vftr_MPI_Raccumulate_f2c
-   USE mpi, ONLY: MPI_ADDRESS_KIND
+   USE vftr_mpi_logging_F, &
+      ONLY : vftr_no_mpi_logging_F
+   USE mpi, &
+      ONLY: PMPI_RACCUMULATE, &
+            MPI_ADDRESS_KIND
    IMPLICIT NONE
    INTEGER ORIGIN_ADDR
    INTEGER ORIGIN_COUNT
@@ -35,9 +39,15 @@ SUBROUTINE MPI_RACCUMULATE(ORIGIN_ADDR, ORIGIN_COUNT, ORIGIN_DATATYPE, &
    INTEGER REQUEST
    INTEGER ERROR
 
-   CALL vftr_MPI_Raccumulate_f2c(ORIGIN_ADDR, ORIGIN_COUNT, ORIGIN_DATATYPE, &
-                                 TARGET_RANK, TARGET_DISP, TARGET_COUNT, &
-                                 TARGET_DATATYPE, OP, WIN, REQUEST, ERROR)
+   IF (vftr_no_mpi_logging_F()) THEN
+      CALL PMPI_RACCUMULATE(ORIGIN_ADDR, ORIGIN_COUNT, ORIGIN_DATATYPE, &
+                            TARGET_RANK, TARGET_DISP, TARGET_COUNT, &
+                            TARGET_DATATYPE, OP, WIN, REQUEST, ERROR)
+   ELSE
+      CALL vftr_MPI_Raccumulate_f2c(ORIGIN_ADDR, ORIGIN_COUNT, ORIGIN_DATATYPE, &
+                                    TARGET_RANK, TARGET_DISP, TARGET_COUNT, &
+                                    TARGET_DATATYPE, OP, WIN, REQUEST, ERROR)
+   END IF
 
 END SUBROUTINE MPI_RACCUMULATE
 
