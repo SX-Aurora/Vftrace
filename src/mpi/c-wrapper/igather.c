f37f620d41b2670471127dce12c124883aadcff3
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-02 10:55:50 +0200

    added vftrace-mpi-logging status switch to igather c-wrappers.

--- src/mpi/c-wrapper/igather.c
+++ src/mpi/c-wrapper/igather.c
@@ -19,14 +19,20 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "vftr_mpi_igather.h"
 
 int MPI_Igather(const void *sendbuf, int sendcount,
                 MPI_Datatype sendtype, void *recvbuf, int recvcount,
                 MPI_Datatype recvtype, int root, MPI_Comm comm,
                 MPI_Request *request) {
-   return vftr_MPI_Igather(sendbuf, sendcount, sendtype, recvbuf, recvcount,
-                           recvtype, root, comm, request);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Igather(sendbuf, sendcount, sendtype, recvbuf, recvcount,
+                          recvtype, root, comm, request);
+   } else {
+      return vftr_MPI_Igather(sendbuf, sendcount, sendtype, recvbuf, recvcount,
+                              recvtype, root, comm, request);
+   }
 }
 
 #endif
