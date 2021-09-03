732bafa52bf8af72c3fd70f0b84736e11fc7391f
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-03 14:55:08 +0200

    removed no-mpi-log switch from vftr_mpi-handling function, as it is now part of the wrappers

--- src/mpi/vftr_mpi/vftr_mpi_waitall.c
+++ src/mpi/vftr_mpi/vftr_mpi_waitall.c
@@ -28,45 +28,40 @@
 int vftr_MPI_Waitall(int count, MPI_Request array_of_requests[],
                      MPI_Status array_of_statuses[]) {
 
-   // disable profiling based on the Pcontrol level
-   if (vftrace_Pcontrol_level == 0) {
-      return PMPI_Waitall(count, array_of_requests, array_of_statuses);
-   } else {
-      if (count <= 0) {return MPI_SUCCESS;}
+   if (count <= 0) {return MPI_SUCCESS;}
    
-      // loop while at least one request is not completed
-      int *req_completed = (int*) malloc(count*sizeof(int));
+   // loop while at least one request is not completed
+   int *req_completed = (int*) malloc(count*sizeof(int));
+   for (int ireq=0; ireq<count; ireq++) {
+      req_completed[ireq] = false;
+   }
+   int retVal;
+   int tmpflag;
+   bool all_completed = false;
+   while (!all_completed) {
+      all_completed = true;
+      // loop over all requests
       for (int ireq=0; ireq<count; ireq++) {
-         req_completed[ireq] = false;
-      }
-      int retVal;
-      int tmpflag;
-      bool all_completed = false;
-      while (!all_completed) {
-         all_completed = true;
-         // loop over all requests
-         for (int ireq=0; ireq<count; ireq++) {
-            if (!req_completed[ireq]) {
-               // check if the communication associated with the request
-               // is completed
-               PMPI_Request_get_status(array_of_requests[ireq],
-                                       &tmpflag,
-                                       MPI_STATUS_IGNORE);
-               // if not completed 
-               req_completed[ireq] = tmpflag;
-               if (!(req_completed[ireq])) {
-                  all_completed = false;
-               }
+         if (!req_completed[ireq]) {
+            // check if the communication associated with the request
+            // is completed
+            PMPI_Request_get_status(array_of_requests[ireq],
+                                    &tmpflag,
+                                    MPI_STATUS_IGNORE);
+            // if not completed 
+            req_completed[ireq] = tmpflag;
+            if (!(req_completed[ireq])) {
+               all_completed = false;
             }
          }
-         vftr_clear_completed_requests();
       }
-
-      free(req_completed);
-      req_completed = NULL;
-   
-      return PMPI_Waitall(count, array_of_requests, array_of_statuses);
+      vftr_clear_completed_requests();
    }
+
+   free(req_completed);
+   req_completed = NULL;
+   
+   return PMPI_Waitall(count, array_of_requests, array_of_statuses);
 }
 
 #endif
