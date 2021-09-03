7afa304444b9bb1c12a16244c768718aa5a5e046
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-03 14:55:08 +0200

    removed no-mpi-log switch from vftr_mpi-handling function, as it is now part of the wrappers

--- src/mpi/vftr_mpi/vftr_mpi_testany.c
+++ src/mpi/vftr_mpi/vftr_mpi_testany.c
@@ -27,54 +27,48 @@
   
 int vftr_MPI_Testany(int count, MPI_Request array_of_requests[],
                      int *index, int *flag, MPI_Status *status) {
-
-   // disable profiling based on the Pcontrol level
-   if (vftrace_Pcontrol_level == 0) {
-      return PMPI_Testany(count, array_of_requests, index, flag, status);
-   } else {
-      // First check if the request array contains at least one active handle
-      bool activereqs = false;
-      for (int ireq=0; ireq<count; ireq++) {
-         if (vftr_mpi_request_is_active(array_of_requests[ireq])) {
-            activereqs = true;
-            break;
-         }
+   // First check if the request array contains at least one active handle
+   bool activereqs = false;
+   for (int ireq=0; ireq<count; ireq++) {
+      if (vftr_mpi_request_is_active(array_of_requests[ireq])) {
+         activereqs = true;
+         break;
       }
-      // if no active request is found return with the following settings
-      if (!activereqs) {
-         *flag = true;
-         *index = MPI_UNDEFINED;
-         if (status != MPI_STATUS_IGNORE) {
-            vftr_empty_mpi_status(status);
-         }
-         return MPI_SUCCESS;
-      }
-
-      // initialize the index to the default failure value
-      *index = MPI_UNDEFINED;
-      // loop over all requests and terminate the loop
-      // on the first completed communication
-      int retVal = 0;
-      for (int ireq=0; ireq<count; ireq++) {
-         retVal = PMPI_Request_get_status(array_of_requests[ireq],
-                                          flag,
-                                          status);
-         // terminate if a completed communication is found
-         if (flag) {
-            // record the index of the completed communication
-            *index = ireq;
-            // clear completed communication from the list of open requests
-            vftr_clear_completed_requests();
-            // Mark the request as inactive, or deallocate it.
-            retVal = PMPI_Test(array_of_requests+ireq,
-                               flag,
-                               status);
-            break;
-         }
-      }
-
-      return retVal;
    }
+   // if no active request is found return with the following settings
+   if (!activereqs) {
+      *flag = true;
+      *index = MPI_UNDEFINED;
+      if (status != MPI_STATUS_IGNORE) {
+         vftr_empty_mpi_status(status);
+      }
+      return MPI_SUCCESS;
+   }
+
+   // initialize the index to the default failure value
+   *index = MPI_UNDEFINED;
+   // loop over all requests and terminate the loop
+   // on the first completed communication
+   int retVal = 0;
+   for (int ireq=0; ireq<count; ireq++) {
+      retVal = PMPI_Request_get_status(array_of_requests[ireq],
+                                       flag,
+                                       status);
+      // terminate if a completed communication is found
+      if (flag) {
+         // record the index of the completed communication
+         *index = ireq;
+         // clear completed communication from the list of open requests
+         vftr_clear_completed_requests();
+         // Mark the request as inactive, or deallocate it.
+         retVal = PMPI_Test(array_of_requests+ireq,
+                            flag,
+                            status);
+         break;
+      }
+   }
+
+   return retVal;
 }
 
 #endif
