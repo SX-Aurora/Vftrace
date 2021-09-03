32637106e128407cc72017cb106688dc110c27cc
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-03 14:55:07 +0200

    removed no-mpi-log switch from vftr_mpi-handling function, as it is now part of the wrappers

--- src/mpi/vftr_mpi/vftr_mpi_fetch_and_op.c
+++ src/mpi/vftr_mpi/vftr_mpi_fetch_and_op.c
@@ -26,43 +26,36 @@
 int vftr_MPI_Fetch_and_op(const void *origin_addr, void *result_addr,
                           MPI_Datatype datatype, int target_rank,
                           MPI_Aint target_disp, MPI_Op op, MPI_Win win) {
+   long long tstart = vftr_get_runtime_usec();
+   int retVal = PMPI_Fetch_and_op(origin_addr, result_addr, datatype,
+                                  target_rank, target_disp, op, win);
+   long long tend = vftr_get_runtime_usec();
 
-   // disable profiling based on the Pcontrol level
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Fetch_and_op(origin_addr, result_addr, datatype,
-                               target_rank, target_disp, op, win);
-   } else {
-      long long tstart = vftr_get_runtime_usec();
-      int retVal = PMPI_Fetch_and_op(origin_addr, result_addr, datatype,
-                                     target_rank, target_disp, op, win);
-      long long tend = vftr_get_runtime_usec();
+   long long t2start = tend;
+   // Need to figure out the partner rank in a known communicator to store info
+   MPI_Group local_group;
+   PMPI_Win_get_group(win, &local_group);
 
-      long long t2start = tend;
-      // Need to figure out the partner rank in a known communicator to store info
-      MPI_Group local_group;
-      PMPI_Win_get_group(win, &local_group);
+   MPI_Group global_group;
+   PMPI_Comm_group(MPI_COMM_WORLD, &global_group);
 
-      MPI_Group global_group;
-      PMPI_Comm_group(MPI_COMM_WORLD, &global_group);
+   int global_rank;
+   PMPI_Group_translate_ranks(local_group,
+                              1,
+                              &target_rank,
+                              global_group,
+                              &global_rank);
 
-      int global_rank;
-      PMPI_Group_translate_ranks(local_group,
-                                 1,
-                                 &target_rank,
-                                 global_group,
-                                 &global_rank);
+   vftr_store_sync_message_info(recv, 1, datatype, global_rank,
+                                -1, MPI_COMM_WORLD, tstart, tend);
+   vftr_store_sync_message_info(send, 1, datatype, global_rank,
+                                -1, MPI_COMM_WORLD, tstart, tend);
 
-      vftr_store_sync_message_info(recv, 1, datatype, global_rank,
-                                   -1, MPI_COMM_WORLD, tstart, tend);
-      vftr_store_sync_message_info(send, 1, datatype, global_rank,
-                                   -1, MPI_COMM_WORLD, tstart, tend);
+   long long t2end = vftr_get_runtime_usec();
 
-      long long t2end = vftr_get_runtime_usec();
+   vftr_mpi_overhead_usec += t2end - t2start;
 
-      vftr_mpi_overhead_usec += t2end - t2start;
-
-      return retVal;
-   }
+   return retVal;
 }
 
 #endif
