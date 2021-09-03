9d736079ed8a10a6fdfe8e8fd77f908171454877
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-03 14:55:08 +0200

    removed no-mpi-log switch from vftr_mpi-handling function, as it is now part of the wrappers

--- src/mpi/vftr_mpi/vftr_mpi_waitany.c
+++ src/mpi/vftr_mpi/vftr_mpi_waitany.c
@@ -27,62 +27,56 @@
   
 int vftr_MPI_Waitany(int count, MPI_Request array_of_requests[],
                      int *index, MPI_Status *status) {
-
-   // disable profiling based on the Pcontrol level
-   if (vftrace_Pcontrol_level == 0) {
-      return PMPI_Waitany(count, array_of_requests, index, status);
-   } else {
-      if (count <= 0) {return MPI_SUCCESS;}
+   if (count <= 0) {return MPI_SUCCESS;}
    
-      // First check if the request array contains at least one active handle
-      bool activereqs = false;
+   // First check if the request array contains at least one active handle
+   bool activereqs = false;
+   for (int ireq=0; ireq<count; ireq++) {
+      if (vftr_mpi_request_is_active(array_of_requests[ireq])) {
+         activereqs = true;
+         break;
+      }
+   }
+   // if no active request is found return with the following settings
+   if (!activereqs) {
+      *index = MPI_UNDEFINED;
+      if (status == MPI_STATUS_IGNORE) {
+         vftr_empty_mpi_status(status);
+      }
+      return MPI_SUCCESS;
+   }
+   
+   // loop until at least one communication of the requests is completed
+   int retVal;
+   int completed_req = false;
+   while (!completed_req) {
+      // loop over all requests
       for (int ireq=0; ireq<count; ireq++) {
-         if (vftr_mpi_request_is_active(array_of_requests[ireq])) {
-            activereqs = true;
+         int flag;
+         // check if the communication associated with the request
+         // is completed
+         retVal = PMPI_Request_get_status(array_of_requests[ireq],
+                                          &flag,
+                                          status);
+         completed_req = completed_req || flag;
+         // either the communication is completed, or not
+         // other communications might be completed in the background
+         // clear those from the list of open requests
+         vftr_clear_completed_requests();
+         // if this request corresponds to a completed communication
+         // leave the loop
+         if (flag) {
+            // record the index of the finished request
+            *index = ireq;
             break;
          }
       }
-      // if no active request is found return with the following settings
-      if (!activereqs) {
-         *index = MPI_UNDEFINED;
-         if (status == MPI_STATUS_IGNORE) {
-            vftr_empty_mpi_status(status);
-         }
-         return MPI_SUCCESS;
-      }
-   
-      // loop until at least one communication of the requests is completed
-      int retVal;
-      int completed_req = false;
-      while (!completed_req) {
-         // loop over all requests
-         for (int ireq=0; ireq<count; ireq++) {
-            int flag;
-            // check if the communication associated with the request
-            // is completed
-            retVal = PMPI_Request_get_status(array_of_requests[ireq],
-                                             &flag,
-                                             status);
-            completed_req = completed_req || flag;
-            // either the communication is completed, or not
-            // other communications might be completed in the background
-            // clear those from the list of open requests
-            vftr_clear_completed_requests();
-            // if this request corresponds to a completed communication
-            // leave the loop
-            if (flag) {
-               // record the index of the finished request
-               *index = ireq;
-               break;
-            }
-         }
-      }
-   
-      // Properly set the request and status variable
-      retVal = PMPI_Wait(array_of_requests+(*index), status);
-   
-      return retVal;
    }
+   
+   // Properly set the request and status variable
+   retVal = PMPI_Wait(array_of_requests+(*index), status);
+   
+   return retVal;
 }
 
 #endif
