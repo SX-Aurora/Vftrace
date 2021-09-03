7ff6fdf9f27c7d45ea9fe882f4cbd43723dda8dc
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-03 06:45:17 +0200

    added vftrace-mpi-logging status switch to alltoallv F08-wrapper.

--- src/mpi/f08-wrapper/alltoallv.F90
+++ src/mpi/f08-wrapper/alltoallv.F90
@@ -21,8 +21,12 @@ SUBROUTINE MPI_Alltoallv_f08(sendbuf, sendcounts, sdispls, sendtype, &
                              comm, error)
    USE vftr_mpi_alltoallv_f082c_f08interface, &
       ONLY : vftr_MPI_Alltoallv_f082c
-   USE mpi_f08, ONLY : MPI_Datatype, &
-                       MPI_Comm
+   USE vftr_mpi_logging_f08, &
+      ONLY : vftr_no_mpi_logging_f08
+   USE mpi_f08, &
+      ONLY : PMPI_Alltoallv_f08, &
+             MPI_Datatype, &
+             MPI_Comm
    IMPLICIT NONE
    INTEGER, INTENT(IN) :: sendbuf
    INTEGER, INTENT(IN) :: sendcounts(*)
@@ -36,9 +40,15 @@ SUBROUTINE MPI_Alltoallv_f08(sendbuf, sendcounts, sdispls, sendtype, &
    INTEGER, OPTIONAL, INTENT(OUT) :: error
    INTEGER :: tmperror
 
-   CALL vftr_MPI_Alltoallv_f082c(sendbuf, sendcounts, sdispls, sendtype%MPI_VAL, &
-                                 recvbuf, recvcounts, rdispls, recvtype%MPI_VAL, &
-                                 comm%MPI_VAL, tmperror)
+   IF (vftr_no_mpi_logging_f08()) THEN
+      CALL PMPI_Alltoallv_f08(sendbuf, sendcounts, sdispls, sendtype, &
+                              recvbuf, recvcounts, rdispls, recvtype, &
+                              comm, tmperror)
+   ELSE
+      CALL vftr_MPI_Alltoallv_f082c(sendbuf, sendcounts, sdispls, sendtype%MPI_VAL, &
+                                    recvbuf, recvcounts, rdispls, recvtype%MPI_VAL, &
+                                    comm%MPI_VAL, tmperror)
+   END IF
    IF (PRESENT(error)) error = tmperror
 
 END SUBROUTINE MPI_Alltoallv_f08
