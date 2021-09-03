226fb143f55b677e0ed58e680cec4ee2044b8ab1
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-03 14:55:08 +0200

    removed no-mpi-log switch from vftr_mpi-handling function, as it is now part of the wrappers

--- src/mpi/vftr_mpi/vftr_mpi_send_init.c
+++ src/mpi/vftr_mpi/vftr_mpi_send_init.c
@@ -24,21 +24,15 @@
 
 int vftr_MPI_Send_init(const void *buf, int count, MPI_Datatype datatype,
                        int dest, int tag, MPI_Comm comm, MPI_Request *request) {
+   int retVal = PMPI_Send_init(buf, count, datatype, dest, tag, comm, request);
 
-   // disable profiling based on the Pcontrol level
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Send_init(buf, count, datatype, dest, tag, comm, request);
-   } else {
-      int retVal = PMPI_Send_init(buf, count, datatype, dest, tag, comm, request);
+   long long t2start = vftr_get_runtime_usec();
+   vftr_register_persistent_request(send, count, datatype, dest, tag, comm, *request);
+   long long t2end = vftr_get_runtime_usec();
 
-      long long t2start = vftr_get_runtime_usec();
-      vftr_register_persistent_request(send, count, datatype, dest, tag, comm, *request);
-      long long t2end = vftr_get_runtime_usec();
+   vftr_mpi_overhead_usec += t2end - t2start;
 
-      vftr_mpi_overhead_usec += t2end - t2start;
-
-      return retVal;
-   }
+   return retVal;
 }
 
 #endif
