ec63f3403a52b5b1af58bcae4b11e2f18e2475d1
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-03 14:55:08 +0200

    removed no-mpi-log switch from vftr_mpi-handling function, as it is now part of the wrappers

--- src/mpi/vftr_mpi/vftr_mpi_testsome.c
+++ src/mpi/vftr_mpi/vftr_mpi_testsome.c
@@ -28,72 +28,65 @@
 int vftr_MPI_Testsome(int incount, MPI_Request array_of_requests[],
                       int *outcount, int array_of_indices[],
                       MPI_Status array_of_statuses[]) {
-
-   // disable profiling based on the Pcontrol level
-   if (vftrace_Pcontrol_level == 0) {
-      return PMPI_Testsome(incount, array_of_requests, outcount,
-                           array_of_indices, array_of_statuses);
-   } else {
-      // First check if the request array contains at least one active handle
-      bool activereqs = false;
-      for (int ireq=0; ireq<incount; ireq++) {
-         if (vftr_mpi_request_is_active(array_of_requests[ireq])) {
-            activereqs = true;
-            break;
-         }
+   // First check if the request array contains at least one active handle
+   bool activereqs = false;
+   for (int ireq=0; ireq<incount; ireq++) {
+      if (vftr_mpi_request_is_active(array_of_requests[ireq])) {
+         activereqs = true;
+         break;
       }
-      // if no active request is found return with the following settings
-      if (!activereqs) {
-         *outcount = MPI_UNDEFINED;
-         return MPI_SUCCESS;
-      }
+   }
+   // if no active request is found return with the following settings
+   if (!activereqs) {
+      *outcount = MPI_UNDEFINED;
+      return MPI_SUCCESS;
+   }
 
-      int retVal = MPI_SUCCESS;
-      int tmpretVal;
-      // loop over all requests and check for completion
-      *outcount = 0;
-      for (int ireq=0; ireq<incount; ireq++) {
-         int flag;
+   int retVal = MPI_SUCCESS;
+   int tmpretVal;
+   // loop over all requests and check for completion
+   *outcount = 0;
+   for (int ireq=0; ireq<incount; ireq++) {
+      int flag;
+      if (array_of_statuses == MPI_STATUSES_IGNORE) {
+         tmpretVal = PMPI_Request_get_status(array_of_requests[ireq],
+                                             &flag,
+                                             MPI_STATUS_IGNORE);
+      } else {
+         tmpretVal = PMPI_Request_get_status(array_of_requests[ireq],
+                                             &flag,
+                                             array_of_statuses+ireq);
+      }
+      if (tmpretVal != MPI_SUCCESS) {
+         // if something goes wrong inform the
+         // user to check the status variable
+         retVal = MPI_ERR_IN_STATUS;
+      }
+      if (flag) {
+         // record completed communications for return
+         (*outcount)++;
+         array_of_indices[(*outcount)-1] = ireq;
+         // remove completed communications from the list of open requests
+         vftr_clear_completed_requests();
+         // Mark the request as inactive, or deallocate it.
          if (array_of_statuses == MPI_STATUSES_IGNORE) {
-            tmpretVal = PMPI_Request_get_status(array_of_requests[ireq],
-                                                &flag,
-                                                MPI_STATUS_IGNORE);
+            tmpretVal = PMPI_Test(array_of_requests+ireq,
+                                  &flag,
+                                  MPI_STATUS_IGNORE);
          } else {
-            tmpretVal = PMPI_Request_get_status(array_of_requests[ireq],
-                                                &flag,
-                                                array_of_statuses+ireq);
+            tmpretVal = PMPI_Test(array_of_requests+ireq,
+                                  &flag,
+                                  array_of_statuses+ireq);
          }
          if (tmpretVal != MPI_SUCCESS) {
             // if something goes wrong inform the
             // user to check the status variable
             retVal = MPI_ERR_IN_STATUS;
          }
-         if (flag) {
-            // record completed communications for return
-            (*outcount)++;
-            array_of_indices[(*outcount)-1] = ireq;
-            // remove completed communications from the list of open requests
-            vftr_clear_completed_requests();
-            // Mark the request as inactive, or deallocate it.
-            if (array_of_statuses == MPI_STATUSES_IGNORE) {
-               tmpretVal = PMPI_Test(array_of_requests+ireq,
-                                     &flag,
-                                     MPI_STATUS_IGNORE);
-            } else {
-               tmpretVal = PMPI_Test(array_of_requests+ireq,
-                                     &flag,
-                                     array_of_statuses+ireq);
-            }
-            if (tmpretVal != MPI_SUCCESS) {
-               // if something goes wrong inform the
-               // user to check the status variable
-               retVal = MPI_ERR_IN_STATUS;
-            }
-         }
       }
-
-      return retVal;
    }
+
+   return retVal;
 }
 
 #endif
