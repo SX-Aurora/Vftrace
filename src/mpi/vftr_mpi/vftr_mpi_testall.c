fa882cdbc6fbb27465518426538fd34a23888d1a
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-03 14:55:08 +0200

    removed no-mpi-log switch from vftr_mpi-handling function, as it is now part of the wrappers

--- src/mpi/vftr_mpi/vftr_mpi_testall.c
+++ src/mpi/vftr_mpi/vftr_mpi_testall.c
@@ -26,42 +26,36 @@
   
 int vftr_MPI_Testall(int count, MPI_Request array_of_requests[],
                      int *flag, MPI_Status array_of_statuses[]) {
+   int retVal;
+   int tmpflag;
 
-   // disable profiling based on the Pcontrol level
-   if (vftrace_Pcontrol_level == 0) {
-      return PMPI_Testall(count, array_of_requests, flag, array_of_statuses);
-   } else {
-      int retVal;
-      int tmpflag;
-
-      // set the return flag to true
-      *flag = true;
-      // It will be returned true if all communications are completed
-      for (int ireq=0; ireq<count; ireq++) {
-         if (array_of_statuses == MPI_STATUSES_IGNORE) {
-            retVal = PMPI_Request_get_status(array_of_requests[ireq],
-                                             &tmpflag,
-                                             MPI_STATUS_IGNORE);
-         } else {
-            retVal = PMPI_Request_get_status(array_of_requests[ireq],
-                                             &tmpflag,
-                                             array_of_statuses+ireq);
-         }
-         // accumulate truthness of the individual requests
-         // as soon as one if false everything is
-         *flag = (*flag) && tmpflag;
+   // set the return flag to true
+   *flag = true;
+   // It will be returned true if all communications are completed
+   for (int ireq=0; ireq<count; ireq++) {
+      if (array_of_statuses == MPI_STATUSES_IGNORE) {
+         retVal = PMPI_Request_get_status(array_of_requests[ireq],
+                                          &tmpflag,
+                                          MPI_STATUS_IGNORE);
+      } else {
+         retVal = PMPI_Request_get_status(array_of_requests[ireq],
+                                          &tmpflag,
+                                          array_of_statuses+ireq);
       }
-      // clear completed communications from the list of open requests
-      vftr_clear_completed_requests();
-
-      if (flag) {
-      // If all communications are completed
-      // run Testall to modify the requests appropriately
-         retVal = PMPI_Testall(count, array_of_requests, flag, array_of_statuses);
-      }
-
-      return retVal;
+      // accumulate truthness of the individual requests
+      // as soon as one if false everything is
+      *flag = (*flag) && tmpflag;
    }
+   // clear completed communications from the list of open requests
+   vftr_clear_completed_requests();
+
+   if (flag) {
+   // If all communications are completed
+   // run Testall to modify the requests appropriately
+      retVal = PMPI_Testall(count, array_of_requests, flag, array_of_statuses);
+   }
+
+   return retVal;
 }
 
 #endif
