1a14b9fa150349a4407cb3e7ce999597e356809f
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-03 06:45:17 +0200

    added vftrace-mpi-logging status switch to barrier F08-wrapper.

--- src/mpi/f08-wrapper/barrier.F90
+++ src/mpi/f08-wrapper/barrier.F90
@@ -19,13 +19,21 @@
 SUBROUTINE MPI_Barrier_f08(comm, error)
    USE vftr_mpi_barrier_f082c_f08interface, &
       ONLY : vftr_MPI_Barrier_f082c
-   USE mpi_f08, ONLY : MPI_Comm
+   USE vftr_mpi_logging_f08, &
+      ONLY : vftr_no_mpi_logging_f08
+   USE mpi_f08, &
+      ONLY : PMPI_Barrier_f08, &
+             MPI_Comm
    IMPLICIT NONE
    TYPE(MPI_Comm), INTENT(IN) :: comm
    INTEGER, OPTIONAL, INTENT(OUT) :: error
    INTEGER :: tmperror
 
-   CALL vftr_MPI_Barrier_f082c(comm%MPI_VAL, tmperror)
+   IF (vftr_no_mpi_logging_f08()) THEN
+      CALL PMPI_Barrier_f08(comm, tmperror)
+   ELSE
+      CALL vftr_MPI_Barrier_f082c(comm%MPI_VAL, tmperror)
+   END IF
    IF (PRESENT(error)) error = tmperror
 
 END SUBROUTINE MPI_Barrier_f08
