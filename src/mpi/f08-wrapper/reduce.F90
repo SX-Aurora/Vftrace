e467b45f01f331602e341cd1cff3bc71af80573d
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-03 06:45:17 +0200

    added vftrace-mpi-logging status switch to reduce F08-wrapper.

--- src/mpi/f08-wrapper/reduce.F90
+++ src/mpi/f08-wrapper/reduce.F90
@@ -20,9 +20,13 @@ SUBROUTINE MPI_Reduce_f08(sendbuf, recvbuf, count, datatype, &
                       op, root, comm, error)
    USE vftr_mpi_reduce_f082c_f08interface, &
       ONLY : vftr_MPI_Reduce_f082c
-   USE mpi_f08, ONLY : MPI_Datatype, &
-                       MPI_Comm, &
-                       MPI_Op
+   USE vftr_mpi_logging_f08, &
+      ONLY : vftr_no_mpi_logging_f08
+   USE mpi_f08, &
+      ONLY : PMPI_Reduce, &
+             MPI_Datatype, &
+             MPI_Comm, &
+             MPI_Op
    IMPLICIT NONE
    INTEGER, INTENT(IN) :: sendbuf
    INTEGER :: recvbuf
@@ -34,8 +38,13 @@ SUBROUTINE MPI_Reduce_f08(sendbuf, recvbuf, count, datatype, &
    INTEGER, OPTIONAL, INTENT(OUT) :: error
    INTEGER :: tmperror
 
-   CALL vftr_MPI_Reduce_f082c(sendbuf, recvbuf, count, datatype%MPI_VAL, &
-                              op%MPI_VAL, root, comm%MPI_VAL, tmperror)
+   IF (vftr_no_mpi_logging_f08()) THEN
+      CALL PMPI_Reduce_f08(sendbuf, recvbuf, count, datatype, &
+                           op, root, comm, tmperror)
+   ELSE
+      CALL vftr_MPI_Reduce_f082c(sendbuf, recvbuf, count, datatype%MPI_VAL, &
+                                 op%MPI_VAL, root, comm%MPI_VAL, tmperror)
+   END IF
    IF (PRESENT(error)) error = tmperror
 
 END SUBROUTINE MPI_Reduce_f08
