c55ec606758d81268a934c24290c3734043c6b40
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 11:12:42 +0200

    split special cases for reduce_scatter operation

--- src/mpi/vftr_mpi/reduce_scatter.h
+++ src/mpi/vftr_mpi/reduce_scatter.h
@@ -22,8 +22,17 @@
 #ifdef _MPI
 #include <mpi.h>
 
-int vftr_MPI_Reduce_scatter(const void *sendbuf, void *recvbuf, const int *recvcounts,
-                            MPI_Datatype datatype, MPI_Op op, MPI_Comm comm);
+int vftr_MPI_Reduce_scatter(const void *sendbuf, void *recvbuf,
+                            const int *recvcounts, MPI_Datatype datatype,
+                            MPI_Op op, MPI_Comm comm);
+
+int vftr_MPI_Reduce_scatter_inplace(const void *sendbuf, void *recvbuf,
+                                    const int *recvcounts, MPI_Datatype datatype,
+                                    MPI_Op op, MPI_Comm comm);
+
+int vftr_MPI_Reduce_scatter_intercom(const void *sendbuf, void *recvbuf,
+                                     const int *recvcounts, MPI_Datatype datatype,
+                                     MPI_Op op, MPI_Comm comm);
 
 #endif
 #endif
