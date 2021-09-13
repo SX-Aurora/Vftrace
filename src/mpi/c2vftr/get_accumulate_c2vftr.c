c11cc76ea212986fe602aab6aa4082b5c64d05dd
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 08:05:15 +0200

    added files for get_accumulate c2vftr layer

--- src/mpi/c2vftr/get_accumulate_c2vftr.c
+++ src/mpi/c2vftr/get_accumulate_c2vftr.c
@@ -22,21 +22,22 @@
 #include "vftr_mpi_utils.h"
 #include "get_accumulate.h"
 
-int vftr_MPI_Get_accumulate(const void *origin_addr, int origin_count,
-                       MPI_Datatype origin_datatype, void *result_addr,
-                       int result_count, MPI_Datatype result_datatype,
-                       int target_rank, MPI_Aint target_disp, int target_count,
-                       MPI_Datatype target_datatype, MPI_Op op, MPI_Win win) {
+int vftr_MPI_Get_accumulate_c2vftr(const void *origin_addr, int origin_count,
+                                   MPI_Datatype origin_datatype, void *result_addr,
+                                   int result_count, MPI_Datatype result_datatype,
+                                   int target_rank, MPI_Aint target_disp,
+                                   int target_count, MPI_Datatype target_datatype,
+                                   MPI_Op op, MPI_Win win) {
    if (vftr_no_mpi_logging()) {
       return PMPI_Get_accumulate(origin_addr, origin_count, origin_datatype,
                                  result_addr, result_count, result_datatype,
-                                 target_rank, target_disp, target_count, target_datatype,
-                                 op, win);
+                                 target_rank, target_disp, target_count,
+                                 target_datatype, op, win);
    } else {
       return vftr_MPI_Get_accumulate(origin_addr, origin_count, origin_datatype,
                                      result_addr, result_count, result_datatype,
-                                     target_rank, target_disp, target_count, target_datatype,
-                                     op, win);
+                                     target_rank, target_disp, target_count,
+                                     target_datatype, op, win);
    }
 }
 
