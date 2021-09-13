dac37f4f46ed426eed05c7484a4e512ffc17cb95
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 07:45:37 +0200

    added files for rput c2vftr layer

--- src/mpi/c2vftr/rput_c2vftr.h
+++ src/mpi/c2vftr/rput_c2vftr.h
@@ -16,26 +16,17 @@
    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#ifndef RPUT_C2VFTR_H
+#define RPUT_C2VFTR_H
+
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
-#include "rput.h"
+int vftr_MPI_Rput_c2vftr(const void *origin_addr, int origin_count,
+                         MPI_Datatype origin_datatype, int target_rank,
+                         MPI_Aint target_disp, int target_count,
+                         MPI_Datatype target_datatype, MPI_Win win,
+                         MPI_Request *request);
 
-int vftr_MPI_Rput(const void *origin_addr, int origin_count,
-             MPI_Datatype origin_datatype, int target_rank,
-             MPI_Aint target_disp, int target_count,
-             MPI_Datatype target_datatype, MPI_Win win,
-             MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Rput(origin_addr, origin_count, origin_datatype,
-                       target_rank, target_disp, target_count,
-                       target_datatype, win, request);
-   } else {
-      return vftr_MPI_Rput(origin_addr, origin_count, origin_datatype,
-                           target_rank, target_disp, target_count,
-                           target_datatype, win, request);
-   }
-}
-
+#endif
 #endif
