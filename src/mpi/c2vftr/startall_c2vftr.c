465aa5b7bb4c7bc1f88f617d83b7b7ac70567b1e
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 07:17:02 +0200

    added files for startall c2vftr layer

---
+++ src/mpi/c2vftr/startall_c2vftr.c
@@ -0,0 +1,33 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifdef _MPI
+#include <mpi.h>
+
+#include "vftr_mpi_utils.h"
+#include "startall.h"
+
+int vftr_MPI_Startall_c2vftr(int count, MPI_Request *array_of_requests) {
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Startall(count, array_of_requests);
+   } else {
+      return vftr_MPI_Startall(count, array_of_requests);
+   }
+}
+
+#endif
