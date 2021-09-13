ed5406043f546a9e4ee67219134ef5b07b71440d
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 09:17:43 +0200

    added files for reduce_scatter c2vftr layer

--- src/mpi/c2vftr/reduce_scatter_c2vftr.h
+++ src/mpi/c2vftr/reduce_scatter_c2vftr.h
@@ -16,19 +16,15 @@
    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#ifndef REDUCE_SCATTER_C2VFTR_H
+#define REDUCE_SCATTER_C2VFTR_H
+
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
-#include "reduce_scatter.h"
+int vftr_MPI_Reduce_scatter_c2vftr(const void *sendbuf, void *recvbuf,
+                                   const int *recvcounts, MPI_Datatype datatype,
+                                   MPI_Op op, MPI_Comm comm);
 
-int vftr_MPI_Reduce_scatter(const void *sendbuf, void *recvbuf, const int *recvcounts,
-                       MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Reduce_scatter(sendbuf, recvbuf, recvcounts, datatype, op, comm);
-   } else {
-      return vftr_MPI_Reduce_scatter(sendbuf, recvbuf, recvcounts, datatype, op, comm);
-   }
-}
-
+#endif
 #endif
