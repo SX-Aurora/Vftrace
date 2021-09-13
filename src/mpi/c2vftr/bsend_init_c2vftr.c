ee84c965d74ff5039f7571e47c141d94cc134dee
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 07:22:18 +0200

    added files for bsend_init c2vftr layer

--- src/mpi/c2vftr/bsend_init_c2vftr.c
+++ src/mpi/c2vftr/bsend_init_c2vftr.c
@@ -22,9 +22,9 @@
 #include "vftr_mpi_utils.h"
 #include "bsend_init.h"
 
-int vftr_MPI_Bsend_init(const void *buf, int count, MPI_Datatype datatype,
-                   int dest, int tag, MPI_Comm comm,
-                   MPI_Request *request) {
+int vftr_MPI_Bsend_init_c2vftr(const void *buf, int count, MPI_Datatype datatype,
+                               int dest, int tag, MPI_Comm comm,
+                               MPI_Request *request) {
    if (vftr_no_mpi_logging()) {
       return PMPI_Bsend_init(buf, count, datatype, dest, tag, comm, request);
    } else {
