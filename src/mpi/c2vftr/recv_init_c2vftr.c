ad4bfbedb040ded886fb8f5b6075b858dccdf07d
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 07:27:42 +0200

    added files for recv_init c2vftr layer

--- src/mpi/c2vftr/recv_init_c2vftr.c
+++ src/mpi/c2vftr/recv_init_c2vftr.c
@@ -22,8 +22,9 @@
 #include "vftr_mpi_utils.h"
 #include "recv_init.h"
 
-int vftr_MPI_Recv_init(void *buf, int count, MPI_Datatype datatype,
-                  int source, int tag, MPI_Comm comm, MPI_Request *request) {
+int vftr_MPI_Recv_init_c2vftr(void *buf, int count, MPI_Datatype datatype,
+                              int source, int tag, MPI_Comm comm,
+                              MPI_Request *request) {
    if (vftr_no_mpi_logging()) {
       return PMPI_Recv_init(buf, count, datatype, source, tag, comm, request);
    } else {
