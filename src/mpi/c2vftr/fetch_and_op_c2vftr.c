34cd39754c33bfbc2ea1da4c74f62a87b96c38df
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 08:09:13 +0200

    added files for fetch_and_op c2vftr layer

--- src/mpi/c2vftr/fetch_and_op_c2vftr.c
+++ src/mpi/c2vftr/fetch_and_op_c2vftr.c
@@ -22,9 +22,9 @@
 #include "vftr_mpi_utils.h"
 #include "fetch_and_op.h"
 
-int vftr_MPI_Fetch_and_op(const void *origin_addr, void *result_addr,
-                     MPI_Datatype datatype, int target_rank,
-                     MPI_Aint target_disp, MPI_Op op, MPI_Win win) {
+int vftr_MPI_Fetch_and_op_c2vftr(const void *origin_addr, void *result_addr,
+                                 MPI_Datatype datatype, int target_rank,
+                                 MPI_Aint target_disp, MPI_Op op, MPI_Win win) {
    if (vftr_no_mpi_logging()) {
       return PMPI_Fetch_and_op(origin_addr, result_addr, datatype,
                                target_rank, target_disp, op, win);
---
+++ src/mpi/c2vftr/fetch_and_op_c2vftr.h
@@ -0,0 +1,30 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef FETCH_AND_OP_C2VFTR_H
+#define FETCH_AND_OP_C2VFTR_H
+
+#ifdef _MPI
+#include <mpi.h>
+
+int vftr_MPI_Fetch_and_op_c2vftr(const void *origin_addr, void *result_addr,
+                                 MPI_Datatype datatype, int target_rank,
+                                 MPI_Aint target_disp, MPI_Op op, MPI_Win win);
+
+#endif
+#endif
