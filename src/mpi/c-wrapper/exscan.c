7ffd11a95c0af90e119e07a8f6c45b5d40e2e664
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 10:04:14 +0200

    corrected typo

--- src/mpi/c-wrapper/exscan.c
+++ src/mpi/c-wrapper/exscan.c
@@ -23,7 +23,7 @@
 
 int MPI_Exscan(const void *sendbuf, void *recvbuf, int count,
                MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
-   return vftr_MPI_Exscan(sendbuf, recvbuf, count, datatype, op, comm);
+   return vftr_MPI_Exscan_c2vftr(sendbuf, recvbuf, count, datatype, op, comm);
 }
 
 #endif
