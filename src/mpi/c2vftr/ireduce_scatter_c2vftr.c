d09cea4b3efc5b7357040b1a2c01d330a67cf559
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 12:33:26 +0200

    split special cases for ireduce_scatter operation

--- src/mpi/c2vftr/ireduce_scatter_c2vftr.c
+++ src/mpi/c2vftr/ireduce_scatter_c2vftr.c
@@ -23,6 +23,7 @@
 #include <mpi.h>
 
 #include "vftr_mpi_utils.h"
+#include "vftr_buf_addr_const.h"
 #include "ireduce_scatter.h"
 
 int vftr_MPI_Ireduce_scatter_c2vftr(const void *sendbuf, void *recvbuf,
@@ -32,16 +33,30 @@ int vftr_MPI_Ireduce_scatter_c2vftr(const void *sendbuf, void *recvbuf,
       return PMPI_Ireduce_scatter(sendbuf, recvbuf, recvcounts,
                                   datatype, op, comm, request);
    } else {
+      // create a copy of recvcount.
+      // It will be deallocated upon completion of the request
       int size;
       PMPI_Comm_size(comm, &size);
-  
       int *tmp_recvcounts = (int*) malloc(size*sizeof(int));
       for (int i=0; i<size; i++) {
          tmp_recvcounts[i] = recvcounts[i];
       }
+      // determine if inter or intra communicator
+      int isintercom;
+      PMPI_Comm_test_inter(comm, &isintercom);
+      if (isintercom) {
+         return vftr_MPI_Ireduce_scatter_intercom(sendbuf, recvbuf, tmp_recvcounts,
+                                                  datatype, op, comm, request);
+      } else {
+         if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
+            return vftr_MPI_Ireduce_scatter_inplace(sendbuf, recvbuf, tmp_recvcounts,
+                                                    datatype, op, comm, request);
+         } else {
+            return vftr_MPI_Ireduce_scatter(sendbuf, recvbuf, tmp_recvcounts,
+                                            datatype, op, comm, request);
+         }
+      }
 
-      return vftr_MPI_Ireduce_scatter(sendbuf, recvbuf, tmp_recvcounts,
-                                      datatype, op, comm, request);
    }
 }
 
