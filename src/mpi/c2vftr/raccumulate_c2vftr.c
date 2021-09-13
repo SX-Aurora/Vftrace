5566f26aad8aeef47e7a06ec6a0c9714d24f4b1a
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 08:02:57 +0200

    added files for raccumulate c2vftr layer

--- src/mpi/c2vftr/raccumulate_c2vftr.c
+++ src/mpi/c2vftr/raccumulate_c2vftr.c
@@ -22,11 +22,11 @@
 #include "vftr_mpi_utils.h"
 #include "raccumulate.h"
 
-int vftr_MPI_Raccumulate(const void *origin_addr, int origin_count,
-                    MPI_Datatype origin_datatype, int target_rank,
-                    MPI_Aint target_disp, int target_count,
-                    MPI_Datatype target_datatype, MPI_Op op,
-                    MPI_Win win, MPI_Request *request) {
+int vftr_MPI_Raccumulate_c2vftr(const void *origin_addr, int origin_count,
+                                MPI_Datatype origin_datatype, int target_rank,
+                                MPI_Aint target_disp, int target_count,
+                                MPI_Datatype target_datatype, MPI_Op op,
+                                MPI_Win win, MPI_Request *request) {
    if (vftr_no_mpi_logging()) {
       return PMPI_Raccumulate(origin_addr, origin_count, origin_datatype,
                               target_rank, target_disp, target_count,
