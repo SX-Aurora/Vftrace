32f28c29355c102d2995636b4bcf625b8c2d4aa7
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 10:04:22 +0200

    corrected typo

--- src/mpi/c-wrapper/iexscan.c
+++ src/mpi/c-wrapper/iexscan.c
@@ -19,7 +19,7 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_iexscan.h"
+#include "iexscan_c2vftr.h"
 
 int MPI_Iexscan(const void *sendbuf, void *recvbuf, int count,
                 MPI_Datatype datatype, MPI_Op op, MPI_Comm comm,
