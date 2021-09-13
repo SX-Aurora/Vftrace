546c3614dfd91fe789ddbede8bb5d48cd19cec0e
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 08:07:32 +0200

    added files for rget_accumulate c2vftr layer

--- src/mpi/c2vftr/rget_accumulate_c2vftr.c
+++ src/mpi/c2vftr/rget_accumulate_c2vftr.c
@@ -22,22 +22,22 @@
 #include "vftr_mpi_utils.h"
 #include "rget_accumulate.h"
 
-int vftr_MPI_Rget_accumulate(const void *origin_addr, int origin_count,
-                        MPI_Datatype origin_datatype, void *result_addr,
-                        int result_count, MPI_Datatype result_datatype,
-                        int target_rank, MPI_Aint target_disp, int target_count,
-                        MPI_Datatype target_datatype, MPI_Op op, MPI_Win win,
-                        MPI_Request *request) {
+int vftr_MPI_Rget_accumulate_c2vftr(const void *origin_addr, int origin_count,
+                                    MPI_Datatype origin_datatype, void *result_addr,
+                                    int result_count, MPI_Datatype result_datatype,
+                                    int target_rank, MPI_Aint target_disp,
+                                    int target_count, MPI_Datatype target_datatype,
+                                    MPI_Op op, MPI_Win win, MPI_Request *request) {
    if (vftr_no_mpi_logging()) {
       return PMPI_Rget_accumulate(origin_addr, origin_count, origin_datatype,
                                   result_addr, result_count, result_datatype,
-                                  target_rank, target_disp, target_count, target_datatype,
-                                  op, win, request);
+                                  target_rank, target_disp, target_count,
+                                  target_datatype, op, win, request);
    } else {
       return vftr_MPI_Rget_accumulate(origin_addr, origin_count, origin_datatype,
                                       result_addr, result_count, result_datatype,
-                                      target_rank, target_disp, target_count, target_datatype,
-                                      op, win, request);
+                                      target_rank, target_disp, target_count,
+                                      target_datatype, op, win, request);
    }
 }
 
---
+++ src/mpi/c2vftr/rget_accumulate_c2vftr.h
@@ -0,0 +1,33 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef RGET_ACCUMULATE_C2VFTR_H
+#define RGET_ACCUMULATE_C2VFTR_H
+
+#ifdef _MPI
+#include <mpi.h>
+
+int vftr_MPI_Rget_accumulate_c2vftr(const void *origin_addr, int origin_count,
+                                    MPI_Datatype origin_datatype, void *result_addr,
+                                    int result_count, MPI_Datatype result_datatype,
+                                    int target_rank, MPI_Aint target_disp,
+                                    int target_count, MPI_Datatype target_datatype,
+                                    MPI_Op op, MPI_Win win, MPI_Request *request);
+
+#endif
+#endif
