08c0aa731a90a109ccb1609ddc3b8001bc98c02e
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-13 07:59:53 +0200

    added files for accumulate c2vftr layer

--- src/mpi/c2vftr/accumulate_c2vftr.c
+++ src/mpi/c2vftr/accumulate_c2vftr.c
@@ -22,11 +22,11 @@
 #include "vftr_mpi_utils.h"
 #include "accumulate.h"
 
-int vftr_MPI_Accumulate(const void *origin_addr, int origin_count,
-                   MPI_Datatype origin_datatype, int target_rank,
-                   MPI_Aint target_disp, int target_count,
-                   MPI_Datatype target_datatype, MPI_Op op,
-                   MPI_Win win) {
+int vftr_MPI_Accumulate_c2vftr(const void *origin_addr, int origin_count,
+                               MPI_Datatype origin_datatype, int target_rank,
+                               MPI_Aint target_disp, int target_count,
+                               MPI_Datatype target_datatype, MPI_Op op,
+                               MPI_Win win) {
    if (vftr_no_mpi_logging()) {
       return PMPI_Accumulate(origin_addr, origin_count, origin_datatype,
                              target_rank, target_disp, target_count,
