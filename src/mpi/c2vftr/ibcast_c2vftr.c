456cd30c97c5ca0ac1386f9f1f212372c29ace51
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-14 07:34:03 +0200

    added and split special cases for ibcast operation

--- src/mpi/c2vftr/ibcast_c2vftr.c
+++ src/mpi/c2vftr/ibcast_c2vftr.c
@@ -22,12 +22,19 @@
 #include "vftr_mpi_utils.h"
 #include "ibcast.h"
 
-int vftr_MPI_Ibcast(void *buffer, int count, MPI_Datatype datatype,
-               int root, MPI_Comm comm, MPI_Request *request) {
+int vftr_MPI_Ibcast_c2vftr(void *buffer, int count, MPI_Datatype datatype,
+                           int root, MPI_Comm comm, MPI_Request *request) {
    if (vftr_no_mpi_logging()) {
       return PMPI_Ibcast(buffer, count, datatype, root, comm, request);
    } else {
-      return vftr_MPI_Ibcast(buffer, count, datatype, root, comm, request);
+      // determine if inter or intra communicator
+      int isintercom;
+      PMPI_Comm_test_inter(comm, &isintercom);
+      if (isintercom) {
+         return vftr_MPI_Ibcast_intercom(buffer, count, datatype, root, comm, request);
+      } else {
+         return vftr_MPI_Ibcast(buffer, count, datatype, root, comm, request);
+      }
    }
 }
 
--- src/mpi/c2vftr/ibcast_c2vftr.h
+++ src/mpi/c2vftr/ibcast_c2vftr.h
@@ -16,17 +16,14 @@
    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#ifndef IBCAST_H
-#define IBCAST_H
+#ifndef IBCAST_C2VFTR_H
+#define IBCAST_C2VFTR_H
 
 #ifdef _MPI
 #include <mpi.h>
 
-int vftr_MPI_Ibcast(void *buffer, int count, MPI_Datatype datatype,
-                    int root, MPI_Comm comm, MPI_Request *request);
-
-int vftr_MPI_Ibcast_intercom(void *buffer, int count, MPI_Datatype datatype,
-                             int root, MPI_Comm comm, MPI_Request *request);
+int vftr_MPI_Ibcast_c2vftr(void *buffer, int count, MPI_Datatype datatype,
+                           int root, MPI_Comm comm, MPI_Request *request);
 
 #endif
 #endif
