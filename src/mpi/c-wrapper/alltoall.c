164c1ec563b2a8a48d2f5a1d96a3bc3a1a8555b6
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in alltoall

--- src/mpi/c-wrapper/alltoall.c
+++ src/mpi/c-wrapper/alltoall.c
@@ -19,14 +19,29 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
+#include "vftr_regions.h"
+#include "vftr_environment.h"
 #include "alltoall_c2vftr.h"
 
 int MPI_Alltoall(const void *sendbuf, int sendcount,
-                 MPI_Datatype sendtype, void *recvbuf, int recvcount,
-                 MPI_Datatype recvtype, MPI_Comm comm) {
-   return vftr_MPI_Alltoall_c2vftr(sendbuf, sendcount, sendtype,
-                                   recvbuf, recvcount, recvtype,
-                                   comm);
+                 MPI_Datatype sendtype, void *recvbuf,
+                 int recvcount, MPI_Datatype recvtype,
+                 MPI_Comm comm) {
+   // Estimate synchronization time
+   if (vftr_environment.mpi_show_sync_time->value) {
+      vftr_internal_region_begin("MPI_Alltoall_sync");
+      PMPI_Barrier(comm);
+      vftr_internal_region_end("MPI_Alltoall_sync");
+   }
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Alltoall(sendbuf, sendcount, sendtype, recvbuf,
+                           recvcount, recvtype, comm);
+   } else {
+      return vftr_MPI_Alltoall_c2vftr(sendbuf, sendcount, sendtype,
+                                      recvbuf, recvcount, recvtype,
+                                      comm);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/alltoall_c2vftr.c
+++ src/mpi/c2vftr/alltoall_c2vftr.c
@@ -19,8 +19,6 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
-#include "vftr_regions.h"
 #include "vftr_environment.h"
 #include "vftr_buf_addr_const.h"
 #include "alltoall.h"
@@ -29,35 +27,24 @@ int vftr_MPI_Alltoall_c2vftr(const void *sendbuf, int sendcount,
                              MPI_Datatype sendtype, void *recvbuf,
                              int recvcount, MPI_Datatype recvtype,
                              MPI_Comm comm) {
-   // Estimate synchronization time
-   if (vftr_environment.mpi_show_sync_time->value) {
-      vftr_internal_region_begin("MPI_Alltoall_sync");
-      PMPI_Barrier(comm);
-      vftr_internal_region_end("MPI_Alltoall_sync");
-   }
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Alltoall(sendbuf, sendcount, sendtype, recvbuf,
-                           recvcount, recvtype, comm);
+   // determine if inter or intra communicator
+   int isintercom;
+   PMPI_Comm_test_inter(comm, &isintercom);
+   if (isintercom) {
+      return vftr_MPI_Alltoall_intercom(sendbuf, sendcount, sendtype,
+                                        recvbuf, recvcount, recvtype,
+                                        comm);
    } else {
-      // determine if inter or intra communicator
-      int isintercom;
-      PMPI_Comm_test_inter(comm, &isintercom);
-      if (isintercom) {
-         return vftr_MPI_Alltoall_intercom(sendbuf, sendcount, sendtype,
-                                           recvbuf, recvcount, recvtype,
-                                           comm);
+      if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
+         return vftr_MPI_Alltoall_inplace(sendbuf, sendcount, sendtype,
+                                          recvbuf, recvcount, recvtype,
+                                          comm);
       } else {
-         if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
-            return vftr_MPI_Alltoall_inplace(sendbuf, sendcount, sendtype,
-                                             recvbuf, recvcount, recvtype,
-                                             comm);
-         } else {
-            return vftr_MPI_Alltoall(sendbuf, sendcount, sendtype,
-                                     recvbuf, recvcount, recvtype,
-                                     comm);
-         }
-
+         return vftr_MPI_Alltoall(sendbuf, sendcount, sendtype,
+                                  recvbuf, recvcount, recvtype,
+                                  comm);
       }
+
    }
 }
 
