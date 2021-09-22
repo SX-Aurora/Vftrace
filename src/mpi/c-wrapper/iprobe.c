5887390702f0c4d4ac4fcbddc10a4d5411a929a0
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in iprobe

--- src/mpi/c-wrapper/iprobe.c
+++ src/mpi/c-wrapper/iprobe.c
@@ -19,10 +19,16 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "iprobe_c2vftr.h"
 
-int MPI_Iprobe(int source, int tag, MPI_Comm comm, int *flag, MPI_Status *status) {
-   return vftr_MPI_Iprobe_c2vftr(source, tag, comm, flag, status);
+int MPI_Iprobe(int source, int tag, MPI_Comm comm,
+               int *flag, MPI_Status *status) {
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Iprobe(source, tag, comm, flag, status);
+   } else {
+      return vftr_MPI_Iprobe_c2vftr(source, tag, comm, flag, status);
+   }
 }
 
 #endif
