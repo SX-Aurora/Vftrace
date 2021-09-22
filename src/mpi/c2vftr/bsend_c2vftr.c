7da45830b0abe381729c9f92fab824ea00d30b03
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in bsend

--- src/mpi/c2vftr/bsend_c2vftr.c
+++ src/mpi/c2vftr/bsend_c2vftr.c
@@ -19,16 +19,11 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "bsend.h"
 
 int vftr_MPI_Bsend_c2vftr(const void *buf, int count, MPI_Datatype datatype,
                           int dest, int tag, MPI_Comm comm) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Bsend(buf, count, datatype, dest, tag, comm);
-   } else {
-      return vftr_MPI_Bsend(buf, count, datatype, dest, tag, comm);
-   }
+   return vftr_MPI_Bsend(buf, count, datatype, dest, tag, comm);
 }
 
 #endif
