9ee6033b18442e77c568021ec3d62852e51a9bae
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in gather

--- src/mpi/c-wrapper/gather.c
+++ src/mpi/c-wrapper/gather.c
@@ -19,14 +19,31 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_regions.h"
+#include "vftr_environment.h"
+#include "vftr_mpi_utils.h"
 #include "gather_c2vftr.h"
 
 int MPI_Gather(const void *sendbuf, int sendcount,
-               MPI_Datatype sendtype, void *recvbuf, int recvcount,
-               MPI_Datatype recvtype, int root, MPI_Comm comm) {
-   return vftr_MPI_Gather_c2vftr(sendbuf, sendcount, sendtype,
-                                 recvbuf, recvcount, recvtype,
-                                 root, comm);
+               MPI_Datatype sendtype, void *recvbuf,
+               int recvcount, MPI_Datatype recvtype,
+               int root, MPI_Comm comm) {
+   // Estimate synchronization time
+   if (vftr_environment.mpi_show_sync_time->value) {
+      vftr_internal_region_begin("MPI_Gather_sync");
+      PMPI_Barrier(comm);
+      vftr_internal_region_end("MPI_Gather_sync");
+   }
+
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Gather(sendbuf, sendcount, sendtype,
+                         recvbuf, recvcount, recvtype,
+                         root, comm);
+   } else {
+      return vftr_MPI_Gather_c2vftr(sendbuf, sendcount, sendtype,
+                                    recvbuf, recvcount, recvtype,
+                                    root, comm);
+   }
 }
 
 #endif
