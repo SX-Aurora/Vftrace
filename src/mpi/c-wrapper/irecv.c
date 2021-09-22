e34b7bd45de13fbae52552e55ba8fbc8e845181a
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in irecv

--- src/mpi/c-wrapper/irecv.c
+++ src/mpi/c-wrapper/irecv.c
@@ -19,12 +19,16 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "irecv_c2vftr.h"
 
 int MPI_Irecv(void *buf, int count, MPI_Datatype datatype,
               int source, int tag, MPI_Comm comm, MPI_Request *request) {
-   return vftr_MPI_Irecv_c2vftr(buf, count, datatype, source,
-                                tag, comm, request);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Irecv(buf, count, datatype, source, tag, comm, request);
+   } else {
+      return vftr_MPI_Irecv_c2vftr(buf, count, datatype, source, tag, comm, request);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/irecv_c2vftr.c
+++ src/mpi/c2vftr/irecv_c2vftr.c
@@ -19,16 +19,11 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "irecv.h"
 
 int vftr_MPI_Irecv_c2vftr(void *buf, int count, MPI_Datatype datatype,
                           int source, int tag, MPI_Comm comm, MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Irecv(buf, count, datatype, source, tag, comm, request);
-   } else {
-      return vftr_MPI_Irecv(buf, count, datatype, source, tag, comm, request);
-   }
+   return vftr_MPI_Irecv(buf, count, datatype, source, tag, comm, request);
 }
 
 #endif
