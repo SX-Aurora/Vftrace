d5ed93aea73f7c603ef31bdedcd3b91d9bf80c48
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in ibcast

--- src/mpi/c-wrapper/ibcast.c
+++ src/mpi/c-wrapper/ibcast.c
@@ -19,11 +19,16 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "ibcast_c2vftr.h"
 
 int MPI_Ibcast(void *buffer, int count, MPI_Datatype datatype,
                int root, MPI_Comm comm, MPI_Request *request) {
-   return vftr_MPI_Ibcast_c2vftr(buffer, count, datatype, root, comm, request);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Ibcast(buffer, count, datatype, root, comm, request);
+   } else {
+      return vftr_MPI_Ibcast_c2vftr(buffer, count, datatype, root, comm, request);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/ibcast_c2vftr.c
+++ src/mpi/c2vftr/ibcast_c2vftr.c
@@ -19,22 +19,17 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "ibcast.h"
 
 int vftr_MPI_Ibcast_c2vftr(void *buffer, int count, MPI_Datatype datatype,
                            int root, MPI_Comm comm, MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Ibcast(buffer, count, datatype, root, comm, request);
+   // determine if inter or intra communicator
+   int isintercom;
+   PMPI_Comm_test_inter(comm, &isintercom);
+   if (isintercom) {
+      return vftr_MPI_Ibcast_intercom(buffer, count, datatype, root, comm, request);
    } else {
-      // determine if inter or intra communicator
-      int isintercom;
-      PMPI_Comm_test_inter(comm, &isintercom);
-      if (isintercom) {
-         return vftr_MPI_Ibcast_intercom(buffer, count, datatype, root, comm, request);
-      } else {
-         return vftr_MPI_Ibcast(buffer, count, datatype, root, comm, request);
-      }
+      return vftr_MPI_Ibcast(buffer, count, datatype, root, comm, request);
    }
 }
 
