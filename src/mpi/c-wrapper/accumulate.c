ee34dc286fc9f52a2ccfa7615cd3aab08352f391
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 08:12:18 +0200

    corrected function name

--- src/mpi/c-wrapper/accumulate.c
+++ src/mpi/c-wrapper/accumulate.c
@@ -28,9 +28,9 @@ int MPI_Accumulate(const void *origin_addr, int origin_count,
                    MPI_Datatype target_datatype, MPI_Op op,
                    MPI_Win win) {
    if (vftr_no_mpi_logging()) {
-      return PMPI_Accumulate_c2vftr(origin_addr, origin_count, origin_datatype,
-                                    target_rank, target_disp, target_count,
-                                    target_datatype, op, win);
+      return PMPI_Accumulate(origin_addr, origin_count, origin_datatype,
+                             target_rank, target_disp, target_count,
+                             target_datatype, op, win);
    } else {
       return vftr_MPI_Accumulate_c2vftr(origin_addr, origin_count, origin_datatype,
                                         target_rank, target_disp, target_count,
