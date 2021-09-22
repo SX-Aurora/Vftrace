8cf9f9b02d3c1d27085db3c4a22f9efd5145abbe
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in get

--- src/mpi/c2vftr/get_c2vftr.c
+++ src/mpi/c2vftr/get_c2vftr.c
@@ -19,23 +19,15 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "get.h"
 
 int vftr_MPI_Get_c2vftr(void *origin_addr, int origin_count,
                         MPI_Datatype origin_datatype, int target_rank,
                         MPI_Aint target_disp, int target_count,
                         MPI_Datatype target_datatype, MPI_Win win) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Get(origin_addr, origin_count, origin_datatype,
-                      target_rank, target_disp, target_count,
-                      target_datatype, win);
-   } else {
-      return vftr_MPI_Get(origin_addr, origin_count, origin_datatype,
-                          target_rank, target_disp, target_count,
-                          target_datatype, win);
-
-   }
+   return vftr_MPI_Get(origin_addr, origin_count, origin_datatype,
+                       target_rank, target_disp, target_count,
+                       target_datatype, win);
 }
 
 #endif
