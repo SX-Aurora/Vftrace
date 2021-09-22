4b59f0114911e333159566d814e72772bb092e1d
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in ialltoallv

--- src/mpi/c-wrapper/ialltoallv.c
+++ src/mpi/c-wrapper/ialltoallv.c
@@ -17,20 +17,31 @@
 */
 
 #ifdef _MPI
+#include <stdlib.h>
+
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "ialltoallv_c2vftr.h"
 
 int MPI_Ialltoallv(const void *sendbuf, const int *sendcounts,
-                   const int *sdispls, MPI_Datatype sendtype, void *recvbuf,
-                   const int *recvcounts, const int *rdispls,
-                   MPI_Datatype recvtype, MPI_Comm comm,
-                   MPI_Request *request) {
-   return vftr_MPI_Ialltoallv_c2vftr(sendbuf, sendcounts,
-                                     sdispls, sendtype,
-                                     recvbuf, recvcounts,
-                                     rdispls, recvtype,
-                                     comm, request);
+                   const int *sdispls, MPI_Datatype sendtype,
+                   void *recvbuf, const int *recvcounts,
+                   const int *rdispls, MPI_Datatype recvtype,
+                   MPI_Comm comm, MPI_Request *request) {
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Ialltoallv(sendbuf, sendcounts,
+                             sdispls, sendtype,
+                             recvbuf, recvcounts,
+                             rdispls, recvtype,
+                             comm, request);
+   } else {
+      return vftr_MPI_Ialltoallv_c2vftr(sendbuf, sendcounts,
+                                        sdispls, sendtype,
+                                        recvbuf, recvcounts,
+                                        rdispls, recvtype,
+                                        comm, request);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/ialltoallv_c2vftr.c
+++ src/mpi/c2vftr/ialltoallv_c2vftr.c
@@ -21,7 +21,6 @@
 
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "vftr_buf_addr_const.h"
 #include "ialltoallv.h"
 
@@ -30,65 +29,57 @@ int vftr_MPI_Ialltoallv_c2vftr(const void *sendbuf, const int *sendcounts,
                                void *recvbuf, const int *recvcounts,
                                const int *rdispls, MPI_Datatype recvtype,
                                MPI_Comm comm, MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Ialltoallv(sendbuf, sendcounts,
-                             sdispls, sendtype,
-                             recvbuf, recvcounts,
-                             rdispls, recvtype,
-                             comm, request);
+   int size;
+   int isintercom;
+   PMPI_Comm_test_inter(comm, &isintercom);
+   if (isintercom) {
+      PMPI_Comm_remote_size(comm, &size);
    } else {
-      int size;
-      int isintercom;
-      PMPI_Comm_test_inter(comm, &isintercom);
-      if (isintercom) {
-         PMPI_Comm_remote_size(comm, &size);
-      } else {
-         PMPI_Comm_size(comm, &size);
-      }
-   
-      int *tmp_sendcounts = NULL;
-      int *tmp_sdispls = NULL;
-      // sendcounts and senddisplacements are ignored 
-      // if sendbuffer is MPI_IN_PLACE
-      if (!vftr_is_C_MPI_IN_PLACE(sendbuf)) {
-         tmp_sendcounts = (int*) malloc(size*sizeof(int));
-         for (int i=0; i<size; i++) {
-            tmp_sendcounts[i] = sendcounts[i];
-         }
-         tmp_sdispls = (int*) malloc(size*sizeof(int));
-         for (int i=0; i<size; i++) {
-            tmp_sdispls[i] = sdispls[i];
-         }
-      }
-      int *tmp_recvcounts = (int*) malloc(size*sizeof(int));
-      for (int i=0; i<size; i++) {
-         tmp_recvcounts[i] = recvcounts[i];
-      }
-      int *tmp_rdispls = (int*) malloc(size*sizeof(int));
-      for (int i=0; i<size; i++) {
-         tmp_rdispls[i] = rdispls[i];
-      }
+      PMPI_Comm_size(comm, &size);
+   }
 
-      if (isintercom) {
-         return vftr_MPI_Ialltoallv_intercom(sendbuf, tmp_sendcounts,
-                                             tmp_sdispls, sendtype,
-                                             recvbuf, tmp_recvcounts,
-                                             tmp_rdispls, recvtype,
-                                             comm, request);
+   int *tmp_sendcounts = NULL;
+   int *tmp_sdispls = NULL;
+   // sendcounts and senddisplacements are ignored 
+   // if sendbuffer is MPI_IN_PLACE
+   if (!vftr_is_C_MPI_IN_PLACE(sendbuf)) {
+      tmp_sendcounts = (int*) malloc(size*sizeof(int));
+      for (int i=0; i<size; i++) {
+         tmp_sendcounts[i] = sendcounts[i];
+      }
+      tmp_sdispls = (int*) malloc(size*sizeof(int));
+      for (int i=0; i<size; i++) {
+         tmp_sdispls[i] = sdispls[i];
+      }
+   }
+   int *tmp_recvcounts = (int*) malloc(size*sizeof(int));
+   for (int i=0; i<size; i++) {
+      tmp_recvcounts[i] = recvcounts[i];
+   }
+   int *tmp_rdispls = (int*) malloc(size*sizeof(int));
+   for (int i=0; i<size; i++) {
+      tmp_rdispls[i] = rdispls[i];
+   }
+
+   if (isintercom) {
+      return vftr_MPI_Ialltoallv_intercom(sendbuf, tmp_sendcounts,
+                                          tmp_sdispls, sendtype,
+                                          recvbuf, tmp_recvcounts,
+                                          tmp_rdispls, recvtype,
+                                          comm, request);
+   } else {
+      if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
+         return vftr_MPI_Ialltoallv_inplace(sendbuf, tmp_sendcounts,
+                                            tmp_sdispls, sendtype,
+                                            recvbuf, tmp_recvcounts,
+                                            tmp_rdispls, recvtype,
+                                            comm, request);
       } else {
-         if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
-            return vftr_MPI_Ialltoallv_inplace(sendbuf, tmp_sendcounts,
-                                               tmp_sdispls, sendtype,
-                                               recvbuf, tmp_recvcounts,
-                                               tmp_rdispls, recvtype,
-                                               comm, request);
-         } else {
-            return vftr_MPI_Ialltoallv(sendbuf, tmp_sendcounts,
-                                       tmp_sdispls, sendtype,
-                                       recvbuf, tmp_recvcounts,
-                                       tmp_rdispls, recvtype,
-                                       comm, request);
-         }
+         return vftr_MPI_Ialltoallv(sendbuf, tmp_sendcounts,
+                                    tmp_sdispls, sendtype,
+                                    recvbuf, tmp_recvcounts,
+                                    tmp_rdispls, recvtype,
+                                    comm, request);
       }
    }
 }
