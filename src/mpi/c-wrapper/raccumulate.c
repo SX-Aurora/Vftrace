277fef691c709a3ce96a80893a9c69f1c357c74b
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:23 +0200

    moved profiling switch to wrapper in raccumulate

--- src/mpi/c-wrapper/raccumulate.c
+++ src/mpi/c-wrapper/raccumulate.c
@@ -19,6 +19,7 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "raccumulate_c2vftr.h"
 
 int MPI_Raccumulate(const void *origin_addr, int origin_count,
@@ -26,9 +27,15 @@ int MPI_Raccumulate(const void *origin_addr, int origin_count,
                     MPI_Aint target_disp, int target_count,
                     MPI_Datatype target_datatype, MPI_Op op,
                     MPI_Win win, MPI_Request *request) {
-   return vftr_MPI_Raccumulate_c2vftr(origin_addr, origin_count, origin_datatype,
-                                      target_rank, target_disp, target_count,
-                                      target_datatype, op, win, request);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Raccumulate(origin_addr, origin_count, origin_datatype,
+                              target_rank, target_disp, target_count,
+                              target_datatype, op, win, request);
+   } else {
+      return vftr_MPI_Raccumulate_c2vftr(origin_addr, origin_count, origin_datatype,
+                                         target_rank, target_disp, target_count,
+                                         target_datatype, op, win, request);
+   }
 }
 
 #endif
