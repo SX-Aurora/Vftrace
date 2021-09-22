dd932c73ec31981575056b9bd41b004c9d9854db
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 08:19:22 +0200

    moved profiling switch to wrapper in allgatherv

--- src/mpi/c-wrapper/allgatherv.c
+++ src/mpi/c-wrapper/allgatherv.c
@@ -19,15 +19,30 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_regions.h"
+#include "vftr_environment.h"
+#include "vftr_mpi_utils.h"
 #include "allgatherv_c2vftr.h"
 
 int MPI_Allgatherv(const void *sendbuf, int sendcount,
                    MPI_Datatype sendtype, void *recvbuf,
                    const int *recvcounts, const int *displs,
                    MPI_Datatype recvtype, MPI_Comm comm) {
-   return vftr_MPI_Allgatherv_c2vftr(sendbuf, sendcount, sendtype,
-                                     recvbuf, recvcounts, displs,
-                                     recvtype, comm);
+   // Estimate synchronization time
+   if (vftr_environment.mpi_show_sync_time->value) {
+      vftr_internal_region_begin("MPI_Allatherv_sync");
+      PMPI_Barrier(comm);
+      vftr_internal_region_end("MPI_Allatherv_sync");
+   }
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Allgatherv(sendbuf, sendcount, sendtype,
+                             recvbuf, recvcounts, displs,
+                             recvtype, comm);
+   } else {
+      return vftr_MPI_Allgatherv_c2vftr(sendbuf, sendcount, sendtype,
+                                        recvbuf, recvcounts, displs,
+                                        recvtype, comm);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/allgatherv_c2vftr.c
+++ src/mpi/c2vftr/allgatherv_c2vftr.c
@@ -19,9 +19,6 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_regions.h"
-#include "vftr_environment.h"
-#include "vftr_mpi_utils.h"
 #include "vftr_buf_addr_const.h"
 #include "allgatherv.h"
 
@@ -29,34 +26,22 @@ int vftr_MPI_Allgatherv_c2vftr(const void *sendbuf, int sendcount,
                                MPI_Datatype sendtype, void *recvbuf,
                                const int *recvcounts, const int *displs,
                                MPI_Datatype recvtype, MPI_Comm comm) {
-   // Estimate synchronization time
-   if (vftr_environment.mpi_show_sync_time->value) {
-      vftr_internal_region_begin("MPI_Allatherv_sync");
-      PMPI_Barrier(comm);
-      vftr_internal_region_end("MPI_Allatherv_sync");
-   }
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Allgatherv(sendbuf, sendcount, sendtype,
-                             recvbuf, recvcounts, displs,
-                             recvtype, comm);
+   // determine if inter or intra communicator
+   int isintercom;
+   PMPI_Comm_test_inter(comm, &isintercom);
+   if (isintercom) {
+      return vftr_MPI_Allgatherv_intercom(sendbuf, sendcount, sendtype,
+                                          recvbuf, recvcounts, displs,
+                                          recvtype, comm);
    } else {
-      // determine if inter or intra communicator
-      int isintercom;
-      PMPI_Comm_test_inter(comm, &isintercom);
-      if (isintercom) {
-         return vftr_MPI_Allgatherv_intercom(sendbuf, sendcount, sendtype,
-                                             recvbuf, recvcounts, displs,
-                                             recvtype, comm);
+      if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
+         return vftr_MPI_Allgatherv_inplace(sendbuf, sendcount, sendtype,
+                                            recvbuf, recvcounts, displs,
+                                            recvtype, comm);
       } else {
-         if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
-            return vftr_MPI_Allgatherv_inplace(sendbuf, sendcount, sendtype,
-                                               recvbuf, recvcounts, displs,
-                                               recvtype, comm);
-         } else {
-            return vftr_MPI_Allgatherv(sendbuf, sendcount, sendtype,
-                                       recvbuf, recvcounts, displs,
-                                       recvtype, comm);
-         }
+         return vftr_MPI_Allgatherv(sendbuf, sendcount, sendtype,
+                                    recvbuf, recvcounts, displs,
+                                    recvtype, comm);
       }
    }
 }
