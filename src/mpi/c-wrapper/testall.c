58e668887a164363f019e1f237cd4b6c7e586ab3
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:24 +0200

    moved profiling switch to wrapper in testall

--- src/mpi/c-wrapper/testall.c
+++ src/mpi/c-wrapper/testall.c
@@ -19,11 +19,16 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "testall_c2vftr.h"
 
 int MPI_Testall(int count, MPI_Request array_of_requests[],
                 int *flag, MPI_Status array_of_statuses[]) {
-   return vftr_MPI_Testall_c2vftr(count, array_of_requests, flag, array_of_statuses);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Testall(count, array_of_requests, flag, array_of_statuses);
+   } else {
+      return vftr_MPI_Testall_c2vftr(count, array_of_requests, flag, array_of_statuses);
+   }
 }
 
 #endif
