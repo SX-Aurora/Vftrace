4fb7b84b07ccede8a9d77b7d53380ff4716d40dc
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:23 +0200

    moved profiling switch to wrapper in sendrecv_replace

--- src/mpi/c-wrapper/sendrecv_replace.c
+++ src/mpi/c-wrapper/sendrecv_replace.c
@@ -19,13 +19,19 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "sendrecv_replace_c2vftr.h"
 
 int MPI_Sendrecv_replace(void *buf, int count, MPI_Datatype datatype,
                          int dest, int sendtag, int source, int recvtag,
                          MPI_Comm comm, MPI_Status *status) {
-   return vftr_MPI_Sendrecv_replace_c2vftr(buf, count, datatype, dest, sendtag,
-                                           source, recvtag, comm, status);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Sendrecv_replace(buf, count, datatype, dest, sendtag,
+                                   source, recvtag, comm, status);
+   } else {
+      return vftr_MPI_Sendrecv_replace_c2vftr(buf, count, datatype, dest, sendtag,
+                                              source, recvtag, comm, status);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/sendrecv_replace_c2vftr.c
+++ src/mpi/c2vftr/sendrecv_replace_c2vftr.c
@@ -19,19 +19,13 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "sendrecv_replace.h"
 
 int vftr_MPI_Sendrecv_replace_c2vftr(void *buf, int count, MPI_Datatype datatype,
                                      int dest, int sendtag, int source, int recvtag,
                                      MPI_Comm comm, MPI_Status *status) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Sendrecv_replace(buf, count, datatype, dest, sendtag,
-                                   source, recvtag, comm, status);
-   } else {
-      return vftr_MPI_Sendrecv_replace(buf, count, datatype, dest, sendtag,
-                                       source, recvtag, comm, status);
-   }
+   return vftr_MPI_Sendrecv_replace(buf, count, datatype, dest, sendtag,
+                                    source, recvtag, comm, status);
 }
 
 #endif
