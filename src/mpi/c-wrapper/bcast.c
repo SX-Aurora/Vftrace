49a3c8f45f582571f64b6189b9afea37d8903e36
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in bcast

--- src/mpi/c-wrapper/bcast.c
+++ src/mpi/c-wrapper/bcast.c
@@ -19,11 +19,25 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_regions.h"
+#include "vftr_environment.h"
+#include "vftr_mpi_utils.h"
 #include "bcast_c2vftr.h"
 
 int MPI_Bcast(void *buffer, int count, MPI_Datatype datatype,
               int root, MPI_Comm comm) {
-   return vftr_MPI_Bcast_c2vftr(buffer, count, datatype, root, comm);
+   // Estimate synchronization time
+   if (vftr_environment.mpi_show_sync_time->value) {
+      vftr_internal_region_begin("MPI_Bcast_sync");
+      PMPI_Barrier(comm);
+      vftr_internal_region_end("MPI_Bcast_sync");
+   }
+
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Bcast(buffer, count, datatype, root, comm);
+   } else {
+      return vftr_MPI_Bcast_c2vftr(buffer, count, datatype, root, comm);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/bcast_c2vftr.c
+++ src/mpi/c2vftr/bcast_c2vftr.c
@@ -19,31 +19,17 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_regions.h"
-#include "vftr_environment.h"
-#include "vftr_mpi_utils.h"
 #include "bcast.h"
 
 int vftr_MPI_Bcast_c2vftr(void *buffer, int count, MPI_Datatype datatype,
                           int root, MPI_Comm comm) {
-   // Estimate synchronization time
-   if (vftr_environment.mpi_show_sync_time->value) {
-      vftr_internal_region_begin("MPI_Bcast_sync");
-      PMPI_Barrier(comm);
-      vftr_internal_region_end("MPI_Bcast_sync");
-   }
-
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Bcast(buffer, count, datatype, root, comm);
+   // determine if inter or intra communicator
+   int isintercom;
+   PMPI_Comm_test_inter(comm, &isintercom);
+   if (isintercom) {
+      return vftr_MPI_Bcast_intercom(buffer, count, datatype, root, comm);
    } else {
-      // determine if inter or intra communicator
-      int isintercom;
-      PMPI_Comm_test_inter(comm, &isintercom);
-      if (isintercom) {
-         return vftr_MPI_Bcast_intercom(buffer, count, datatype, root, comm);
-      } else {
-         return vftr_MPI_Bcast(buffer, count, datatype, root, comm);
-      }
+      return vftr_MPI_Bcast(buffer, count, datatype, root, comm);
    }
 }
 
