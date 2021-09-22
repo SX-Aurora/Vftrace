5b9675fa24047a36e341979db6c157028fa6bd5b
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:24 +0200

    moved profiling switch to wrapper in waitsome

--- src/mpi/c2vftr/waitsome_c2vftr.c
+++ src/mpi/c2vftr/waitsome_c2vftr.c
@@ -19,19 +19,13 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "waitsome.h"
 
 int vftr_MPI_Waitsome_c2vftr(int incount, MPI_Request array_of_requests[],
                              int *outcount, int array_of_indices[],
                              MPI_Status array_of_statuses[]) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Waitsome(incount, array_of_requests, outcount,
-                           array_of_indices, array_of_statuses);
-   } else {
-      return vftr_MPI_Waitsome(incount, array_of_requests, outcount,
-                               array_of_indices, array_of_statuses);
-   }
+   return vftr_MPI_Waitsome(incount, array_of_requests, outcount,
+                            array_of_indices, array_of_statuses);
 }
 
 #endif
