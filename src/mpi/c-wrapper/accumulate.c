897d547373918f588fed52c79f5a4e251ffffd06
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 07:57:45 +0200

    moved profiling switch to wrapper in accumulate

--- src/mpi/c-wrapper/accumulate.c
+++ src/mpi/c-wrapper/accumulate.c
@@ -19,6 +19,7 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "accumulate_c2vftr.h"
 
 int MPI_Accumulate(const void *origin_addr, int origin_count,
@@ -26,9 +27,15 @@ int MPI_Accumulate(const void *origin_addr, int origin_count,
                    MPI_Aint target_disp, int target_count,
                    MPI_Datatype target_datatype, MPI_Op op,
                    MPI_Win win) {
-   return vftr_MPI_Accumulate_c2vftr(origin_addr, origin_count, origin_datatype,
-                                     target_rank, target_disp, target_count,
-                                     target_datatype, op, win);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Accumulate_c2vftr(origin_addr, origin_count, origin_datatype,
+                                    target_rank, target_disp, target_count,
+                                    target_datatype, op, win);
+   } else {
+      return vftr_MPI_Accumulate_c2vftr(origin_addr, origin_count, origin_datatype,
+                                        target_rank, target_disp, target_count,
+                                        target_datatype, op, win);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/accumulate_c2vftr.c
+++ src/mpi/c2vftr/accumulate_c2vftr.c
@@ -27,16 +27,9 @@ int vftr_MPI_Accumulate_c2vftr(const void *origin_addr, int origin_count,
                                MPI_Aint target_disp, int target_count,
                                MPI_Datatype target_datatype, MPI_Op op,
                                MPI_Win win) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Accumulate(origin_addr, origin_count, origin_datatype,
-                             target_rank, target_disp, target_count,
-                             target_datatype, op, win);
-
-   } else {
-      return vftr_MPI_Accumulate(origin_addr, origin_count, origin_datatype,
-                                 target_rank, target_disp, target_count,
-                                 target_datatype, op, win);
-   }
+   return vftr_MPI_Accumulate(origin_addr, origin_count, origin_datatype,
+                              target_rank, target_disp, target_count,
+                              target_datatype, op, win);
 }
 
 #endif
