9bd3db1fafefc6f027ac3be974aa8c80c40e9f12
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:23 +0200

    moved profiling switch to wrapper in recv_init

--- src/mpi/c-wrapper/recv_init.c
+++ src/mpi/c-wrapper/recv_init.c
@@ -19,12 +19,17 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "recv_init_c2vftr.h"
 
 int MPI_Recv_init(void *buf, int count, MPI_Datatype datatype,
-                  int source, int tag, MPI_Comm comm, MPI_Request *request) {
-   return vftr_MPI_Recv_init_c2vftr(buf, count, datatype, source,
-                                    tag, comm, request);
+                  int source, int tag, MPI_Comm comm,
+                  MPI_Request *request) {
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Recv_init(buf, count, datatype, source, tag, comm, request);
+   } else {
+      return vftr_MPI_Recv_init_c2vftr(buf, count, datatype, source, tag, comm, request);
+   }
 }
 
 #endif
