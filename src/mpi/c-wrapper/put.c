96d20cfda84bc3327a200e38f3d2203c5db51bd5
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:23 +0200

    moved profiling switch to wrapper in put

--- src/mpi/c-wrapper/put.c
+++ src/mpi/c-wrapper/put.c
@@ -19,15 +19,22 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "put_c2vftr.h"
 
 int MPI_Put(const void *origin_addr, int origin_count,
             MPI_Datatype origin_datatype, int target_rank,
             MPI_Aint target_disp, int target_count,
             MPI_Datatype target_datatype, MPI_Win win) {
-   return vftr_MPI_Put_c2vftr(origin_addr, origin_count, origin_datatype,
-                              target_rank, target_disp, target_count,
-                              target_datatype, win);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Put(origin_addr, origin_count, origin_datatype,
+                      target_rank, target_disp, target_count,
+                      target_datatype, win);
+   } else {
+      return vftr_MPI_Put_c2vftr(origin_addr, origin_count, origin_datatype,
+                                 target_rank, target_disp, target_count,
+                                 target_datatype, win);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/put_c2vftr.c
+++ src/mpi/c2vftr/put_c2vftr.c
@@ -19,22 +19,15 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "put.h"
 
 int vftr_MPI_Put_c2vftr(const void *origin_addr, int origin_count,
                         MPI_Datatype origin_datatype, int target_rank,
                         MPI_Aint target_disp, int target_count,
                         MPI_Datatype target_datatype, MPI_Win win) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Put(origin_addr, origin_count, origin_datatype,
-                      target_rank, target_disp, target_count,
-                      target_datatype, win);
-   } else {
-      return vftr_MPI_Put(origin_addr, origin_count, origin_datatype,
-                          target_rank, target_disp, target_count,
-                          target_datatype, win);
-   }
+   return vftr_MPI_Put(origin_addr, origin_count, origin_datatype,
+                       target_rank, target_disp, target_count,
+                       target_datatype, win);
 }
 
 #endif
