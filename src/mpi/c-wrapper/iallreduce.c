375f3cc2775b862f73d418f36ce8058b40dee32a
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in iallreduce

--- src/mpi/c-wrapper/iallreduce.c
+++ src/mpi/c-wrapper/iallreduce.c
@@ -19,13 +19,18 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "iallreduce_c2vftr.h"
 
 int MPI_Iallreduce(const void *sendbuf, void *recvbuf, int count,
                    MPI_Datatype datatype, MPI_Op op, MPI_Comm comm,
                    MPI_Request *request) {
-   return vftr_MPI_Iallreduce_c2vftr(sendbuf, recvbuf, count,
-                                     datatype, op, comm, request);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Iallreduce(sendbuf, recvbuf, count, datatype, op, comm, request);
+   } else {
+      return vftr_MPI_Iallreduce_c2vftr(sendbuf, recvbuf, count,
+                                        datatype, op, comm, request);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/iallreduce_c2vftr.c
+++ src/mpi/c2vftr/iallreduce_c2vftr.c
@@ -19,17 +19,12 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "iallreduce.h"
 
 int vftr_MPI_Iallreduce_c2vftr(const void *sendbuf, void *recvbuf, int count,
                                MPI_Datatype datatype, MPI_Op op, MPI_Comm comm,
                                MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Iallreduce(sendbuf, recvbuf, count, datatype, op, comm, request);
-   } else {
-      return vftr_MPI_Iallreduce(sendbuf, recvbuf, count, datatype, op, comm, request);
-   }
+   return vftr_MPI_Iallreduce(sendbuf, recvbuf, count, datatype, op, comm, request);
 }
 
 #endif
