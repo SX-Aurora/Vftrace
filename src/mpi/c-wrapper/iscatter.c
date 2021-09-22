76cbd58f46a53fab18e675f61f3c7ce440fe2713
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:23 +0200

    moved profiling switch to wrapper in iscatter

--- src/mpi/c-wrapper/iscatter.c
+++ src/mpi/c-wrapper/iscatter.c
@@ -19,14 +19,23 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "iscatter_c2vftr.h"
 
-int MPI_Iscatter(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
-                 void *recvbuf, int recvcount, MPI_Datatype recvtype,
-                 int root, MPI_Comm comm, MPI_Request *request) {
-   return vftr_MPI_Iscatter_c2vftr(sendbuf, sendcount, sendtype,
-                                   recvbuf, recvcount, recvtype,
-                                   root, comm, request);
+int MPI_Iscatter(const void *sendbuf, int sendcount,
+                 MPI_Datatype sendtype, void *recvbuf,
+                 int recvcount, MPI_Datatype recvtype,
+                 int root, MPI_Comm comm,
+                 MPI_Request *request) {
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Iscatter(sendbuf, sendcount, sendtype,
+                           recvbuf, recvcount, recvtype,
+                           root, comm, request);
+   } else {
+      return vftr_MPI_Iscatter_c2vftr(sendbuf, sendcount, sendtype,
+                                      recvbuf, recvcount, recvtype,
+                                      root, comm, request);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/iscatter_c2vftr.c
+++ src/mpi/c2vftr/iscatter_c2vftr.c
@@ -19,7 +19,6 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "vftr_buf_addr_const.h"
 #include "iscatter.h"
 
@@ -28,28 +27,22 @@ int vftr_MPI_Iscatter_c2vftr(const void *sendbuf, int sendcount,
                              int recvcount, MPI_Datatype recvtype,
                              int root, MPI_Comm comm,
                              MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Iscatter(sendbuf, sendcount, sendtype,
-                           recvbuf, recvcount, recvtype,
-                           root, comm, request);
+   // determine if inter or intra communicator
+   int isintercom;
+   PMPI_Comm_test_inter(comm, &isintercom);
+   if (isintercom) {
+      return vftr_MPI_Iscatter_intercom(sendbuf, sendcount, sendtype,
+                                        recvbuf, recvcount, recvtype,
+                                        root, comm, request);
    } else {
-      // determine if inter or intra communicator
-      int isintercom;
-      PMPI_Comm_test_inter(comm, &isintercom);
-      if (isintercom) {
-         return vftr_MPI_Iscatter_intercom(sendbuf, sendcount, sendtype,
-                                           recvbuf, recvcount, recvtype,
-                                           root, comm, request);
+      if (vftr_is_C_MPI_IN_PLACE(recvbuf)) {
+         return vftr_MPI_Iscatter_inplace(sendbuf, sendcount, sendtype,
+                                          recvbuf, recvcount, recvtype,
+                                          root, comm, request);
       } else {
-         if (vftr_is_C_MPI_IN_PLACE(recvbuf)) {
-            return vftr_MPI_Iscatter_inplace(sendbuf, sendcount, sendtype,
-                                             recvbuf, recvcount, recvtype,
-                                             root, comm, request);
-         } else {
-            return vftr_MPI_Iscatter(sendbuf, sendcount, sendtype,
-                                     recvbuf, recvcount, recvtype,
-                                     root, comm, request);
-         }
+         return vftr_MPI_Iscatter(sendbuf, sendcount, sendtype,
+                                  recvbuf, recvcount, recvtype,
+                                  root, comm, request);
       }
    }
 }
