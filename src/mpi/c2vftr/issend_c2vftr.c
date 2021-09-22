58821a98cdb748563b342fb2fc10217c3d6227ba
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:23 +0200

    moved profiling switch to wrapper in issend

--- src/mpi/c2vftr/issend_c2vftr.c
+++ src/mpi/c2vftr/issend_c2vftr.c
@@ -19,17 +19,12 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "issend.h"
 
 int vftr_MPI_Issend_c2vftr(const void *buf, int count, MPI_Datatype datatype,
                            int dest, int tag, MPI_Comm comm,
                            MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Issend(buf, count, datatype, dest, tag, comm, request);
-   } else {
-      return vftr_MPI_Issend(buf, count, datatype, dest, tag, comm, request);
-   }
+   return vftr_MPI_Issend(buf, count, datatype, dest, tag, comm, request);
 }
 
 #endif
