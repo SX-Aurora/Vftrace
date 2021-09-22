b0056b63950abba7843245416464b532489530d7
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in iallgather

--- src/mpi/c-wrapper/iallgather.c
+++ src/mpi/c-wrapper/iallgather.c
@@ -19,15 +19,21 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "iallgather_c2vftr.h"
 
 int MPI_Iallgather(const void *sendbuf, int sendcount,
-                   MPI_Datatype sendtype, void *recvbuf, int recvcount,
-                   MPI_Datatype recvtype, MPI_Comm comm,
-                   MPI_Request *request) {
-   return vftr_MPI_Iallgather_c2vftr(sendbuf, sendcount, sendtype,
-                                     recvbuf, recvcount, recvtype,
-                                     comm, request);
+                   MPI_Datatype sendtype, void *recvbuf,
+                   int recvcount, MPI_Datatype recvtype,
+                   MPI_Comm comm, MPI_Request *request) {
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Iallgather(sendbuf, sendcount, sendtype, recvbuf,
+                             recvcount, recvtype, comm, request);
+   } else {
+      return vftr_MPI_Iallgather_c2vftr(sendbuf, sendcount, sendtype,
+                                        recvbuf, recvcount, recvtype,
+                                        comm, request);
+   }
 }
 
 #endif
