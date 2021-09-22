0973ca8668c90799013d8f99cee91e4cc2c5a903
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:23 +0200

    moved profiling switch to wrapper in iscan

--- src/mpi/c2vftr/iscan_c2vftr.c
+++ src/mpi/c2vftr/iscan_c2vftr.c
@@ -19,17 +19,12 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "iscan.h"
 
 int vftr_MPI_Iscan_c2vftr(const void *sendbuf, void *recvbuf, int count,
                           MPI_Datatype datatype, MPI_Op op, MPI_Comm comm,
                           MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Iscan(sendbuf, recvbuf, count, datatype, op, comm, request);
-   } else {
-      return vftr_MPI_Iscan(sendbuf, recvbuf, count, datatype, op, comm, request);
-   }
+   return vftr_MPI_Iscan(sendbuf, recvbuf, count, datatype, op, comm, request);
 }
 
 #endif
