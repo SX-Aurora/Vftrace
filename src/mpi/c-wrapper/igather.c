6f16cf796fbc9e3353fd2561b6ac2f98c268794b
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in igather

--- src/mpi/c-wrapper/igather.c
+++ src/mpi/c-wrapper/igather.c
@@ -19,15 +19,22 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "igather_c2vftr.h"
 
 int MPI_Igather(const void *sendbuf, int sendcount,
-                MPI_Datatype sendtype, void *recvbuf, int recvcount,
-                MPI_Datatype recvtype, int root, MPI_Comm comm,
+                MPI_Datatype sendtype, void *recvbuf,
+                int recvcount, MPI_Datatype recvtype,
+                int root, MPI_Comm comm,
                 MPI_Request *request) {
-   return vftr_MPI_Igather_c2vftr(sendbuf, sendcount, sendtype,
-                                  recvbuf, recvcount, recvtype,
-                                  root, comm, request);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Igather(sendbuf, sendcount, sendtype, recvbuf, recvcount,
+                          recvtype, root, comm, request);
+   } else {
+      return vftr_MPI_Igather_c2vftr(sendbuf, sendcount, sendtype,
+                                     recvbuf, recvcount, recvtype,
+                                     root, comm, request);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/igather_c2vftr.c
+++ src/mpi/c2vftr/igather_c2vftr.c
@@ -19,7 +19,6 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "vftr_buf_addr_const.h"
 #include "igather.h"
 
@@ -28,27 +27,22 @@ int vftr_MPI_Igather_c2vftr(const void *sendbuf, int sendcount,
                             int recvcount, MPI_Datatype recvtype,
                             int root, MPI_Comm comm,
                             MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Igather(sendbuf, sendcount, sendtype, recvbuf, recvcount,
-                          recvtype, root, comm, request);
+   // determine if inter or intra communicator
+   int isintercom;
+   PMPI_Comm_test_inter(comm, &isintercom);
+   if (isintercom) {
+      return vftr_MPI_Igather_intercom(sendbuf, sendcount, sendtype,
+                                       recvbuf, recvcount, recvtype,
+                                       root, comm, request);
    } else {
-      // determine if inter or intra communicator
-      int isintercom;
-      PMPI_Comm_test_inter(comm, &isintercom);
-      if (isintercom) {
-         return vftr_MPI_Igather_intercom(sendbuf, sendcount, sendtype,
-                                          recvbuf, recvcount, recvtype,
-                                          root, comm, request);
+      if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
+         return vftr_MPI_Igather_inplace(sendbuf, sendcount, sendtype,
+                                         recvbuf, recvcount, recvtype,
+                                         root, comm, request);
       } else {
-         if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
-            return vftr_MPI_Igather_inplace(sendbuf, sendcount, sendtype,
-                                            recvbuf, recvcount, recvtype,
-                                            root, comm, request);
-         } else {
-            return vftr_MPI_Igather(sendbuf, sendcount, sendtype,
-                                    recvbuf, recvcount, recvtype,
-                                    root, comm, request);
-         }
+         return vftr_MPI_Igather(sendbuf, sendcount, sendtype,
+                                 recvbuf, recvcount, recvtype,
+                                 root, comm, request);
       }
    }
 }
