ceb670b3222c9ca09c57572c6c753f15032abf3f
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in iexscan

--- src/mpi/c-wrapper/iexscan.c
+++ src/mpi/c-wrapper/iexscan.c
@@ -19,13 +19,17 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "iexscan_c2vftr.h"
 
 int MPI_Iexscan(const void *sendbuf, void *recvbuf, int count,
                 MPI_Datatype datatype, MPI_Op op, MPI_Comm comm,
                 MPI_Request *request) {
-   return vftr_MPI_Iexscan_c2vftr(sendbuf, recvbuf, count,
-                                  datatype, op, comm, request);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Iexscan(sendbuf, recvbuf, count, datatype, op, comm, request);
+   } else {
+      return vftr_MPI_Iexscan_c2vftr(sendbuf, recvbuf, count, datatype, op, comm, request);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/iexscan_c2vftr.c
+++ src/mpi/c2vftr/iexscan_c2vftr.c
@@ -19,17 +19,12 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "iexscan.h"
 
 int vftr_MPI_Iexscan_c2vftr(const void *sendbuf, void *recvbuf, int count,
                             MPI_Datatype datatype, MPI_Op op, MPI_Comm comm,
                             MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Iexscan(sendbuf, recvbuf, count, datatype, op, comm, request);
-   } else {
-      return vftr_MPI_Iexscan(sendbuf, recvbuf, count, datatype, op, comm, request);
-   }
+   return vftr_MPI_Iexscan(sendbuf, recvbuf, count, datatype, op, comm, request);
 }
 
 #endif
