4d31f244d1fa95494430fdb4d7330d141a35d1ec
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:23 +0200

    moved profiling switch to wrapper in rsend_init

--- src/mpi/c-wrapper/rsend_init.c
+++ src/mpi/c-wrapper/rsend_init.c
@@ -19,13 +19,17 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "rsend_init_c2vftr.h"
 
 int MPI_Rsend_init(const void *buf, int count, MPI_Datatype datatype,
                    int dest, int tag, MPI_Comm comm,
                    MPI_Request *request) {
-   return vftr_MPI_Rsend_init_c2vftr(buf, count, datatype, dest,
-                                     tag, comm, request);
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Rsend_init(buf, count, datatype, dest, tag, comm, request);
+   } else {
+      return vftr_MPI_Rsend_init_c2vftr(buf, count, datatype, dest, tag, comm, request);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/rsend_init_c2vftr.c
+++ src/mpi/c2vftr/rsend_init_c2vftr.c
@@ -19,17 +19,12 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "rsend_init.h"
 
 int vftr_MPI_Rsend_init_c2vftr(const void *buf, int count, MPI_Datatype datatype,
                                int dest, int tag, MPI_Comm comm,
                                MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Rsend_init(buf, count, datatype, dest, tag, comm, request);
-   } else {
-      return vftr_MPI_Rsend_init(buf, count, datatype, dest, tag, comm, request);
-   }
+   return vftr_MPI_Rsend_init(buf, count, datatype, dest, tag, comm, request);
 }
 
 #endif
