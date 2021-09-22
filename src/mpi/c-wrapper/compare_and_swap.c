5eae3e2c69b9f56d8d1d43d9307e2e7efc45c113
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:22 +0200

    moved profiling switch to wrapper in compare_and_swap

--- src/mpi/c-wrapper/compare_and_swap.c
+++ src/mpi/c-wrapper/compare_and_swap.c
@@ -19,13 +19,20 @@
 #ifdef _MPI
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "compare_and_swap_c2vftr.h"
 
 int MPI_Compare_and_swap(const void *origin_addr, const void *compare_addr,
                          void *result_addr, MPI_Datatype datatype,
-                         int target_rank, MPI_Aint target_disp, MPI_Win win) {
-   return vftr_MPI_Compare_and_swap_c2vftr(origin_addr, compare_addr, result_addr,
-                                           datatype, target_rank, target_disp, win);
+                         int target_rank, MPI_Aint target_disp,
+                         MPI_Win win) {
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Compare_and_swap(origin_addr, compare_addr, result_addr,
+                                   datatype, target_rank, target_disp, win);
+   } else {
+      return vftr_MPI_Compare_and_swap_c2vftr(origin_addr, compare_addr, result_addr,
+                                              datatype, target_rank, target_disp, win);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/compare_and_swap_c2vftr.c
+++ src/mpi/c2vftr/compare_and_swap_c2vftr.c
@@ -19,20 +19,14 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "compare_and_swap.h"
 
 int vftr_MPI_Compare_and_swap_c2vftr(const void *origin_addr, const void *compare_addr,
                                      void *result_addr, MPI_Datatype datatype,
                                      int target_rank, MPI_Aint target_disp,
                                      MPI_Win win) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Compare_and_swap(origin_addr, compare_addr, result_addr,
-                                   datatype, target_rank, target_disp, win);
-   } else {
-      return vftr_MPI_Compare_and_swap(origin_addr, compare_addr, result_addr,
-                                       datatype, target_rank, target_disp, win);
-   }
+   return vftr_MPI_Compare_and_swap(origin_addr, compare_addr, result_addr,
+                                    datatype, target_rank, target_disp, win);
 }
 
 #endif
