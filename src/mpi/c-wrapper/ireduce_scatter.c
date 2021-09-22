e19ccbdf8f750b2c35c619ffa0044fb17d58b11e
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:23 +0200

    moved profiling switch to wrapper in ireduce_scatter

--- src/mpi/c-wrapper/ireduce_scatter.c
+++ src/mpi/c-wrapper/ireduce_scatter.c
@@ -17,15 +17,24 @@
 */
 
 #ifdef _MPI
+
+#include <stdlib.h>
+
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "ireduce_scatter_c2vftr.h"
 
-int MPI_Ireduce_scatter(const void *sendbuf, void *recvbuf, const int *recvcounts,
-                        MPI_Datatype datatype, MPI_Op op, MPI_Comm comm,
-                        MPI_Request *request) {
-   return vftr_MPI_Ireduce_scatter_c2vftr(sendbuf, recvbuf, recvcounts,
-                                          datatype, op, comm, request);
+int MPI_Ireduce_scatter(const void *sendbuf, void *recvbuf,
+                        const int *recvcounts, MPI_Datatype datatype,
+                        MPI_Op op, MPI_Comm comm, MPI_Request *request) {
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Ireduce_scatter(sendbuf, recvbuf, recvcounts,
+                                  datatype, op, comm, request);
+   } else {
+      return vftr_MPI_Ireduce_scatter_c2vftr(sendbuf, recvbuf, recvcounts,
+                                             datatype, op, comm, request);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/ireduce_scatter_c2vftr.c
+++ src/mpi/c2vftr/ireduce_scatter_c2vftr.c
@@ -17,46 +17,38 @@
 */
 
 #ifdef _MPI
-
 #include <stdlib.h>
 
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "vftr_buf_addr_const.h"
 #include "ireduce_scatter.h"
 
 int vftr_MPI_Ireduce_scatter_c2vftr(const void *sendbuf, void *recvbuf,
                                     const int *recvcounts, MPI_Datatype datatype,
                                     MPI_Op op, MPI_Comm comm, MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Ireduce_scatter(sendbuf, recvbuf, recvcounts,
-                                  datatype, op, comm, request);
+   // create a copy of recvcount.
+   // It will be deallocated upon completion of the request
+   int size;
+   PMPI_Comm_size(comm, &size);
+   int *tmp_recvcounts = (int*) malloc(size*sizeof(int));
+   for (int i=0; i<size; i++) {
+      tmp_recvcounts[i] = recvcounts[i];
+   }
+   // determine if inter or intra communicator
+   int isintercom;
+   PMPI_Comm_test_inter(comm, &isintercom);
+   if (isintercom) {
+      return vftr_MPI_Ireduce_scatter_intercom(sendbuf, recvbuf, tmp_recvcounts,
+                                               datatype, op, comm, request);
    } else {
-      // create a copy of recvcount.
-      // It will be deallocated upon completion of the request
-      int size;
-      PMPI_Comm_size(comm, &size);
-      int *tmp_recvcounts = (int*) malloc(size*sizeof(int));
-      for (int i=0; i<size; i++) {
-         tmp_recvcounts[i] = recvcounts[i];
-      }
-      // determine if inter or intra communicator
-      int isintercom;
-      PMPI_Comm_test_inter(comm, &isintercom);
-      if (isintercom) {
-         return vftr_MPI_Ireduce_scatter_intercom(sendbuf, recvbuf, tmp_recvcounts,
-                                                  datatype, op, comm, request);
+      if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
+         return vftr_MPI_Ireduce_scatter_inplace(sendbuf, recvbuf, tmp_recvcounts,
+                                                 datatype, op, comm, request);
       } else {
-         if (vftr_is_C_MPI_IN_PLACE(sendbuf)) {
-            return vftr_MPI_Ireduce_scatter_inplace(sendbuf, recvbuf, tmp_recvcounts,
-                                                    datatype, op, comm, request);
-         } else {
-            return vftr_MPI_Ireduce_scatter(sendbuf, recvbuf, tmp_recvcounts,
-                                            datatype, op, comm, request);
-         }
+         return vftr_MPI_Ireduce_scatter(sendbuf, recvbuf, tmp_recvcounts,
+                                         datatype, op, comm, request);
       }
-
    }
 }
 
