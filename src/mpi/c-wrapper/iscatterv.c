c32dbd4b92e2f0f7316a963dba5af8d22c893088
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-22 10:37:23 +0200

    moved profiling switch to wrapper in iscatterv

--- src/mpi/c-wrapper/iscatterv.c
+++ src/mpi/c-wrapper/iscatterv.c
@@ -17,17 +17,27 @@
 */
 
 #ifdef _MPI
+#include <stdlib.h>
+
 #include <mpi.h>
 
+#include "vftr_mpi_utils.h"
 #include "iscatterv_c2vftr.h"
 
 int MPI_Iscatterv(const void *sendbuf, const int *sendcounts,
                   const int *displs, MPI_Datatype sendtype,
-                  void *recvbuf, int recvcount, MPI_Datatype recvtype,
-                  int root, MPI_Comm comm, MPI_Request *request) {
-   return vftr_MPI_Iscatterv_c2vftr(sendbuf, sendcounts, displs,
-                                    sendtype, recvbuf, recvcount,
-                                    recvtype, root, comm, request);
+                  void *recvbuf, int recvcount,
+                  MPI_Datatype recvtype, int root,
+                  MPI_Comm comm, MPI_Request *request) {
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Iscatterv(sendbuf, sendcounts, displs, sendtype,
+                            recvbuf, recvcount, recvtype, root, comm,
+                            request);
+   } else {
+      return vftr_MPI_Iscatterv_c2vftr(sendbuf, sendcounts, displs,
+                                       sendtype, recvbuf, recvcount,
+                                       recvtype, root, comm, request);
+   }
 }
 
 #endif
--- src/mpi/c2vftr/iscatterv_c2vftr.c
+++ src/mpi/c2vftr/iscatterv_c2vftr.c
@@ -21,7 +21,6 @@
 
 #include <mpi.h>
 
-#include "vftr_mpi_utils.h"
 #include "vftr_buf_addr_const.h"
 #include "iscatterv.h"
 
@@ -30,55 +29,49 @@ int vftr_MPI_Iscatterv_c2vftr(const void *sendbuf, const int *sendcounts,
                               void *recvbuf, int recvcount,
                               MPI_Datatype recvtype, int root,
                               MPI_Comm comm, MPI_Request *request) {
-   if (vftr_no_mpi_logging()) {
-      return PMPI_Iscatterv(sendbuf, sendcounts, displs, sendtype,
-                            recvbuf, recvcount, recvtype, root, comm,
-                            request);
+   int isroot;
+   int size;
+   int isintercom;
+   PMPI_Comm_test_inter(comm, &isintercom);
+   if (isintercom) {
+      isroot = MPI_ROOT == root;
    } else {
-      int isroot;
-      int size;
-      int isintercom;
-      PMPI_Comm_test_inter(comm, &isintercom);
-      if (isintercom) {
-         isroot = MPI_ROOT == root;
-      } else {
-         int myrank;
-         PMPI_Comm_rank(comm, &myrank);
-         isroot = myrank == root;
-      }
-  
-      int *tmp_sendcounts = NULL;
-      int *tmp_displs = NULL;
-      if (isroot) {
-         if (isintercom) {
-            PMPI_Comm_remote_size(comm, &size);
-         } else {
-            PMPI_Comm_size(comm, &size);
-         }
-         tmp_sendcounts = (int*) malloc(size*sizeof(int));
-         for (int i=0; i<size; i++) {
-            tmp_sendcounts[i] = sendcounts[i];
-         }
-         tmp_displs = (int*) malloc(size*sizeof(int));
-         for (int i=0; i<size; i++) {
-            tmp_displs[i] = displs[i];
-         }
-      }
+      int myrank;
+      PMPI_Comm_rank(comm, &myrank);
+      isroot = myrank == root;
+   }
 
+   int *tmp_sendcounts = NULL;
+   int *tmp_displs = NULL;
+   if (isroot) {
       if (isintercom) {
-         return vftr_MPI_Iscatterv_intercom(sendbuf, tmp_sendcounts, tmp_displs,
-                                            sendtype, recvbuf, recvcount,
-                                            recvtype, root, comm, request);
+         PMPI_Comm_remote_size(comm, &size);
       } else {
-         if (vftr_is_C_MPI_IN_PLACE(recvbuf)) {
-            return vftr_MPI_Iscatterv_inplace(sendbuf, tmp_sendcounts, tmp_displs,
-                                              sendtype, recvbuf, recvcount,
-                                              recvtype, root, comm, request);
-         } else {
-            return vftr_MPI_Iscatterv(sendbuf, tmp_sendcounts, tmp_displs,
-                                      sendtype, recvbuf, recvcount,
-                                      recvtype, root, comm, request);
-         }
+         PMPI_Comm_size(comm, &size);
+      }
+      tmp_sendcounts = (int*) malloc(size*sizeof(int));
+      for (int i=0; i<size; i++) {
+         tmp_sendcounts[i] = sendcounts[i];
+      }
+      tmp_displs = (int*) malloc(size*sizeof(int));
+      for (int i=0; i<size; i++) {
+         tmp_displs[i] = displs[i];
+      }
+   }
+
+   if (isintercom) {
+      return vftr_MPI_Iscatterv_intercom(sendbuf, tmp_sendcounts, tmp_displs,
+                                         sendtype, recvbuf, recvcount,
+                                         recvtype, root, comm, request);
+   } else {
+      if (vftr_is_C_MPI_IN_PLACE(recvbuf)) {
+         return vftr_MPI_Iscatterv_inplace(sendbuf, tmp_sendcounts, tmp_displs,
+                                           sendtype, recvbuf, recvcount,
+                                           recvtype, root, comm, request);
+      } else {
+         return vftr_MPI_Iscatterv(sendbuf, tmp_sendcounts, tmp_displs,
+                                   sendtype, recvbuf, recvcount,
+                                   recvtype, root, comm, request);
       }
    }
 }
