4a7ef74477a8aa0154e151435590a2ce65c2d807
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-23 14:03:37 +0200

    ported allgatherv to f08vftr interface

--- src/mpi/f082vftr/allgatherv_f082vftr_f08i.F90
+++ src/mpi/f082vftr/allgatherv_f082vftr_f08i.F90
@@ -14,7 +14,7 @@
 ! with this program; if not, write to the Free Software Foundation, Inc.,
 ! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-MODULE vftr_mpi_allgatherv_f082c_f08interface
+MODULE vftr_mpi_allgatherv_f082vftr_f08i
 #ifdef _MPI
 
    USE, INTRINSIC :: ISO_FORTRAN_ENV
@@ -24,14 +24,14 @@ MODULE vftr_mpi_allgatherv_f082c_f08interface
 
    PRIVATE
 
-   PUBLIC :: vftr_MPI_Allgatherv_f082c
+   PUBLIC :: vftr_MPI_Allgatherv_f082vftr
 
    INTERFACE 
 
-      SUBROUTINE vftr_MPI_Allgatherv_f082c(sendbuf, sendcount, f_sendtype, &
-                                           recvbuf, f_recvcounts, f_displs, &
-                                           f_recvtype, f_comm, f_error) &
-         BIND(C, name="vftr_MPI_Allgatherv_f082c")
+      SUBROUTINE vftr_MPI_Allgatherv_f082vftr(sendbuf, sendcount, f_sendtype, &
+                                              recvbuf, f_recvcounts, f_displs, &
+                                              f_recvtype, f_comm, f_error) &
+         BIND(C, name="vftr_MPI_Allgatherv_f082vftr")
          IMPLICIT NONE
          INTEGER, INTENT(IN) :: sendbuf
          INTEGER, INTENT(IN) :: sendcount
@@ -42,7 +42,7 @@ MODULE vftr_mpi_allgatherv_f082c_f08interface
          INTEGER, INTENT(IN) :: f_recvtype
          INTEGER, INTENT(IN) :: f_comm
          INTEGER, INTENT(OUT) :: f_error
-      END SUBROUTINE vftr_MPI_Allgatherv_f082c
+      END SUBROUTINE vftr_MPI_Allgatherv_f082vftr
 
    END INTERFACE
 
@@ -50,4 +50,4 @@ MODULE vftr_mpi_allgatherv_f082c_f08interface
 
 CONTAINS
 
-END MODULE vftr_mpi_allgatherv_f082c_f08interface
+END MODULE vftr_mpi_allgatherv_f082vftr_f08i
