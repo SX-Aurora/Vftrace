dcb5fa5d1e640cb0b477be2ad83dbe8ffd07926a
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-23 06:48:42 +0200

    corrected ibarrier test

--- test/mpi/f-mpi/ibarrier.F90
+++ test/mpi/f-mpi/ibarrier.F90
@@ -3,9 +3,10 @@ PROGRAM ibarrier
    IMPLICIT NONE
    INTEGER :: ierr
    INTEGER :: request
+   INTEGER :: status(MPI_STATUS_SIZE)
 
    CALL MPI_Init(ierr)
    CALL MPI_Ibarrier(MPI_COMM_WORLD, request, ierr)
-   CALL MPI_Wait(request, ierr)
+   CALL MPI_Wait(request, status, ierr)
    CALL MPI_Finalize(ierr)
 END PROGRAM ibarrier
