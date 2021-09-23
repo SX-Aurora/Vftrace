476ddcf8b29c11a4c45df3ad4727a886e9cc27d7
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-23 13:19:53 +0200

    ported bcast to f08vftr interface

--- src/mpi/f082vftr/bcast_f082vftr.c
+++ src/mpi/f082vftr/bcast_f082vftr.c
@@ -19,20 +19,31 @@
 #ifdef _MPI
 #include <mpi.h>
 
-#include <vftr_mpi_buf_addr_const.h>
-#include <vftr_mpi_bcast.h>
+#include "vftr_mpi_buf_addr_const.h"
+#include "bcast.h"
 
-void vftr_MPI_Bcast_f082c(void *buffer, MPI_Fint *count, MPI_Fint *f_datatype, 
-                          MPI_Fint *root, MPI_Fint *f_comm, MPI_Fint *f_error) {
+void vftr_MPI_Bcast_f082vftr(void *buffer, MPI_Fint *count, MPI_Fint *f_datatype, 
+                             MPI_Fint *root, MPI_Fint *f_comm, MPI_Fint *f_error) {
 
    MPI_Datatype c_datatype = PMPI_Type_f2c(*f_datatype);
    MPI_Comm c_comm = PMPI_Comm_f2c(*f_comm);
 
-   int c_error = vftr_MPI_Bcast(buffer,
-                                (int)(*count),
-                                c_datatype,
-                                (int)(*root),
-                                c_comm);
+   int c_error;
+   int isintercom;
+   PMPI_Comm_test_inter(c_comm, &isintercom);
+   if (isintercom) {
+      c_error = vftr_MPI_Bcast_intercom(buffer,
+                                        (int)(*count),
+                                        c_datatype,
+                                        (int)(*root),
+                                        c_comm);
+   } else {
+      c_error = vftr_MPI_Bcast(buffer,
+                               (int)(*count),
+                               c_datatype,
+                               (int)(*root),
+                               c_comm);
+   }
 
    *f_error = (MPI_Fint) (c_error);
 }
