c4c3e8965c55f8a7e61786498b7ac78fd2f00e1a
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-27 10:50:35 +0200

    type fix

--- test/units/no_instrument_attribute.c
+++ test/units/no_instrument_attribute.c
@@ -8,7 +8,7 @@ int not_instrumented_function () {
 int instrumented_function () {
    return 0;
 }
-int main(int argc, int **argv) {
+int main(int argc, char **argv) {
 #ifdef _MPI
    MPI_Init(&argc, &argv);
 #endif
