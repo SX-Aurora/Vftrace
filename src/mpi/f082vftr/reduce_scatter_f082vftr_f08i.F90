5da96422ee10110e71e6abc89a079cb35f3063f5
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-27 07:33:44 +0200

    ported reduce_scatter to f08vftr interface

--- src/mpi/f082vftr/reduce_scatter_f082vftr_f08i.F90
+++ src/mpi/f082vftr/reduce_scatter_f082vftr_f08i.F90
@@ -14,7 +14,7 @@
 ! with this program; if not, write to the Free Software Foundation, Inc.,
 ! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-MODULE vftr_mpi_reduce_scatter_f082c_f08interface
+MODULE vftr_mpi_reduce_scatter_f082vftr_f08i
 #ifdef _MPI
 
    USE, INTRINSIC :: ISO_FORTRAN_ENV
@@ -24,14 +24,14 @@ MODULE vftr_mpi_reduce_scatter_f082c_f08interface
 
    PRIVATE
 
-   PUBLIC :: vftr_MPI_Reduce_scatter_f082c
+   PUBLIC :: vftr_MPI_Reduce_scatter_f082vftr
 
    INTERFACE 
 
-      SUBROUTINE vftr_MPI_Reduce_scatter_f082c(sendbuf, recvbuf, f_recvcounts, &
-                                               f_datatype, f_op, f_comm, &
-                                               f_error) &
-         BIND(C, name="vftr_MPI_Reduce_scatter_f082c")
+      SUBROUTINE vftr_MPI_Reduce_scatter_f082vftr(sendbuf, recvbuf, f_recvcounts, &
+                                                  f_datatype, f_op, f_comm, &
+                                                  f_error) &
+         BIND(C, name="vftr_MPI_Reduce_scatter_f082vftr")
          IMPLICIT NONE
          INTEGER, INTENT(IN) :: sendbuf
          INTEGER, INTENT(IN) :: recvbuf
@@ -40,7 +40,7 @@ MODULE vftr_mpi_reduce_scatter_f082c_f08interface
          INTEGER, INTENT(IN) :: f_op
          INTEGER, INTENT(IN) :: f_comm
          INTEGER, INTENT(OUT) :: f_error
-      END SUBROUTINE vftr_MPI_Reduce_scatter_f082c
+      END SUBROUTINE vftr_MPI_Reduce_scatter_f082vftr
 
    END INTERFACE
 
@@ -48,4 +48,4 @@ MODULE vftr_mpi_reduce_scatter_f082c_f08interface
 
 CONTAINS
 
-END MODULE vftr_mpi_reduce_scatter_f082c_f08interface
+END MODULE vftr_mpi_reduce_scatter_f082vftr_f08i
