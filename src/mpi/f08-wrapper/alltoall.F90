c5cf7481bf56ffab88e7cc94a44a35435f91e0ca
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-09-27 10:19:52 +0200

    corrected imported mpi-module procedure in alltoall F08 wrapper

--- src/mpi/f08-wrapper/alltoall.F90
+++ src/mpi/f08-wrapper/alltoall.F90
@@ -24,7 +24,7 @@ SUBROUTINE MPI_Alltoall_f08(sendbuf, sendcount, sendtype, &
    USE vftr_mpi_logging_f08, &
       ONLY : vftr_no_mpi_logging_f08
    USE mpi_f08, &
-      ONLY : PMPI_Alltoall_f08, &
+      ONLY : PMPI_Alltoall, &
              MPI_Datatype, &
              MPI_Comm
    IMPLICIT NONE
@@ -39,9 +39,9 @@ SUBROUTINE MPI_Alltoall_f08(sendbuf, sendcount, sendtype, &
    INTEGER :: tmperror
 
    IF (vftr_no_mpi_logging_f08()) THEN
-      CALL PMPI_Alltoall_f08(sendbuf, sendcount, sendtype, &
-                             recvbuf, recvcount, recvtype, &
-                             comm, tmperror)
+      CALL PMPI_Alltoall(sendbuf, sendcount, sendtype, &
+                         recvbuf, recvcount, recvtype, &
+                         comm, tmperror)
    ELSE
       CALL vftr_MPI_Alltoall_f082vftr(sendbuf, sendcount, sendtype%MPI_VAL, &
                                    recvbuf, recvcount, recvtype%MPI_VAL, &
