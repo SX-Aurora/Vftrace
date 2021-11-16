20c2bed6ceda2ecba4a3a452bf3f33823a2fb823
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-11-16 12:49:17 +0100

    bugfix

--- src/mpi/f08-wrapper/wait.F90
+++ src/mpi/f08-wrapper/wait.F90
@@ -32,7 +32,7 @@ SUBROUTINE MPI_Wait_f08(request, status, error)
    INTEGER :: tmperror
 
    IF (vftr_no_mpi_logging_f08()) THEN
-      CALL PMPI_Wait(request, status, error)
+      CALL PMPI_Wait(request, status, tmperror)
    ELSE
       CALL vftr_MPI_Wait_f082vftr(request%MPI_VAL, status, tmperror)
    END IF
