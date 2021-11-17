7bd73b0850eb580879cf3c2cc8734442883d0095
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-11-17 12:49:43 +0100

    added sendrecv wrapper and interface for Fortran08

---
+++ src/mpi/f08-wrapper/sendrecv.F90
@@ -0,0 +1,60 @@
+! This file is part of Vftrace.
+!
+! Vftrace is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! Vftrace is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License along
+! with this program; if not, write to the Free Software Foundation, Inc.,
+! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#ifdef _MPI
+
+SUBROUTINE MPI_Sendrecv_f08(sendbuf, sendcount, sendtype, dest, sendtag, &
+                            recvbuf, recvcount, recvtype, source, recvtag, &
+                            comm, status, error)
+   USE vftr_mpi_sendrecv_f082vftr_f08i, &
+      ONLY : vftr_MPI_Sendrecv_f082vftr
+   USE vftr_mpi_logging_f08, &
+      ONLY : vftr_no_mpi_logging_f08
+   USE mpi_f08, &
+      ONLY : PMPI_Sendrecv, &
+             MPI_Datatype, &
+             MPI_Comm, &
+             MPI_Status
+   IMPLICIT NONE
+   INTEGER :: sendbuf
+   INTEGER, INTENT(IN) ::  sendcount
+   TYPE(MPI_Datatype), INTENT(IN) :: sendtype
+   INTEGER, INTENT(IN) ::  dest
+   INTEGER, INTENT(IN) ::  sendtag
+   INTEGER :: recvbuf
+   INTEGER, INTENT(IN) ::  recvcount
+   TYPE(MPI_Datatype), INTENT(IN) :: recvtype
+   INTEGER, INTENT(IN) ::  source
+   INTEGER, INTENT(IN) ::  recvtag
+   TYPE(MPI_Comm), INTENT(IN) :: comm
+   TYPE(MPI_Status) :: status
+   INTEGER, OPTIONAL, INTENT(OUT) :: error
+   INTEGER :: tmperror
+
+   IF (vftr_no_mpi_logging_f08()) THEN
+      CALL PMPI_Sendrecv(sendbuf, sendcount, sendtype, dest, sendtag, &
+                         recvbuf, recvcount, recvtype, source, recvtag, &
+                         comm, status, tmperror)
+   ELSE
+      CALL vftr_MPI_Sendrecv_f082vftr(sendbuf, sendcount, sendtype%MPI_VAL, dest, sendtag, &
+                                      recvbuf, recvcount, recvtype%MPI_VAL, source, recvtag, &
+                                      comm%MPI_VAL, status, tmperror)
+   END IF
+   IF (PRESENT(error)) error = tmperror
+
+END SUBROUTINE MPI_Sendrecv_f08
+
+#endif
---
+++ src/mpi/f082vftr/sendrecv_f082vftr.c
@@ -0,0 +1,54 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifdef _MPI
+#include <mpi.h>
+
+#include "sendrecv.h"
+
+void vftr_MPI_Sendrecv_f082vftr(void *sendbuf, MPI_Fint *sendcount,
+                                MPI_Fint *f_sendtype, MPI_Fint *dest, MPI_Fint *sendtag,
+                                void *recvbuf, MPI_Fint *recvcount, MPI_Fint *f_recvtype,
+                                MPI_Fint *source, MPI_Fint *recvtag, MPI_Fint *f_comm,
+                                MPI_F08_status *f_status, MPI_Fint *f_error) {
+
+   MPI_Comm c_comm = PMPI_Comm_f2c(*f_comm);
+   MPI_Datatype c_sendtype = PMPI_Type_f2c(*f_sendtype);
+   MPI_Datatype c_recvtype = PMPI_Type_f2c(*f_recvtype);
+   MPI_Status c_status;
+
+   int c_error = vftr_MPI_Sendrecv(sendbuf,
+                                   (int)(*sendcount),
+                                   c_sendtype,
+                                   (int)(*dest),
+                                   (int)(*sendtag),
+                                   recvbuf,
+                                   (int)(*recvcount),
+                                   c_recvtype,
+                                   (int)(*source),
+                                   (int)(*recvtag),
+                                   c_comm,
+                                   &c_status);
+
+   if (f_status != MPI_F08_STATUS_IGNORE) {
+      PMPI_Status_c2f08(&c_status, f_status);
+   }
+   *f_error = (MPI_Fint) (c_error);
+}
+
+#endif
