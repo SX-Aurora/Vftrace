085b6a961d7506a7c2d4fafe3655472f86fe5910
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-11-17 08:21:03 +0100

    added waitany wrapper and interface for Fortran08

---
+++ src/mpi/f08-wrapper/waitany.F90
@@ -0,0 +1,54 @@
+! This file is part of Vftrace.
+!
+! Vftrace is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! Vftrace is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License along
+! with this program; if not, write to the Free Software Foundation, Inc.,
+! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#ifdef _MPI
+
+SUBROUTINE MPI_Waitany_f08(count, array_of_requests, index, status, error)
+   USE vftr_mpi_waitany_f082vftr_f08i, &
+      ONLY : vftr_MPI_Waitany_f082vftr
+   USE vftr_mpi_logging_f08, &
+      ONLY : vftr_no_mpi_logging_f08
+   USE mpi_f08, &
+      ONLY: PMPI_Waitany, &
+            MPI_Request, &
+            MPI_Status
+   IMPLICIT NONE
+   INTEGER, INTENT(IN) :: count
+   TYPE(MPI_Request), INTENT(INOUT) :: array_of_requests(count)
+   INTEGER, INTENT(OUT) :: index
+   TYPE(MPI_Status) :: status
+   INTEGER, OPTIONAL, INTENT(OUT) :: error
+   INTEGER :: tmperror
+   INTEGER, DIMENSION(count) :: tmparray_of_requests
+   INTEGER :: i
+
+   IF (vftr_no_mpi_logging_f08()) THEN
+      CALL PMPI_Waitany(count, array_of_requests, index, status, tmperror)
+   ELSE
+      DO i = 1, count
+         tmparray_of_requests(i) = array_of_requests(i)%MPI_VAL
+      END DO
+      CALL vftr_MPI_Waitany_f082vftr(count, tmparray_of_requests, index, status, tmperror)
+      DO i = 1, count
+         array_of_requests(i)%MPI_VAL = tmparray_of_requests(i)
+      END DO
+   END IF
+   IF (PRESENT(error)) error = tmperror
+
+END SUBROUTINE MPI_Waitany_f08
+
+#endif 
+
---
+++ src/mpi/f082vftr/waitany_f082vftr_f08i.F90
@@ -0,0 +1,50 @@
+! This file is part of Vftrace.
+!
+! Vftrace is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! Vftrace is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License along
+! with this program; if not, write to the Free Software Foundation, Inc.,
+! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+MODULE vftr_mpi_waitany_f082vftr_f08i
+#ifdef _MPI
+
+   USE, INTRINSIC :: ISO_FORTRAN_ENV
+   USE, INTRINSIC :: ISO_C_BINDING
+
+   IMPLICIT NONE
+
+   PRIVATE
+
+   PUBLIC :: vftr_MPI_Waitany_f082vftr
+
+   INTERFACE
+
+      SUBROUTINE vftr_MPI_Waitany_f082vftr(f_count, f_array_of_requests, &
+                                           f_index, f_status, f_error) &
+         BIND(C, name="vftr_MPI_Waitany_f082vftr")
+         USE mpi_f08, &
+            ONLY: MPI_Status
+         IMPLICIT NONE
+         INTEGER :: f_count
+         INTEGER :: f_array_of_requests(*)
+         INTEGER :: f_index
+         TYPE(MPI_Status) :: f_status
+         INTEGER :: f_error
+      END SUBROUTINE vftr_MPI_Waitany_f082vftr
+
+   END INTERFACE
+
+#endif 
+
+CONTAINS
+
+END MODULE vftr_mpi_waitany_f082vftr_f08i
