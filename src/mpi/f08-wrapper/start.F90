5ce2a3fe270a90e39d08b48b7bf21ed4df883aaf
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-11-18 07:11:53 +0100

    corrected start wrapper and interface for Fortran08

--- src/mpi/f08-wrapper/start.F90
+++ src/mpi/f08-wrapper/start.F90
@@ -21,17 +21,18 @@ SUBROUTINE MPI_Start_f08(request, error)
       ONLY : vftr_MPI_Start_f082vftr
    USE vftr_mpi_logging_f08, &
       ONLY : vftr_no_mpi_logging_f08
-   USE mpi, &
+   USE mpi_f08, &
       ONLY : PMPI_Start, &
              MPI_Request
    IMPLICIT NONE
    TYPE(MPI_Request), INTENT(INOUT) :: request
    INTEGER, OPTIONAL, INTENT(OUT) :: error
+   INTEGER :: tmperror
 
    IF (vftr_no_mpi_logging_f08()) THEN
       CALL PMPI_Start(request, tmperror)
    ELSE
-      CALL vftr_MPI_Start_f082vftr(request, tmperror)
+      CALL vftr_MPI_Start_f082vftr(request%MPI_VAL, tmperror)
    END IF
    IF (PRESENT(error)) error = tmperror
 
