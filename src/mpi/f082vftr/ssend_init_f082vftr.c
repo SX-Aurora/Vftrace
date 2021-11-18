1e6572c4ed975643681e184effb01d7994da9c88
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-11-18 08:38:15 +0100

    prettify

--- src/mpi/f082vftr/ssend_init_f082vftr.c
+++ src/mpi/f082vftr/ssend_init_f082vftr.c
@@ -22,8 +22,8 @@
 #include "ssend_init.h"
 
 void vftr_MPI_Ssend_init_f082vftr(void *buf, MPI_Fint *count, MPI_Fint *f_datatype,
-                                 MPI_Fint *dest, MPI_Fint *tag, MPI_Fint *f_comm,
-                                 MPI_Fint *f_request, MPI_Fint *f_error) {
+                                  MPI_Fint *dest, MPI_Fint *tag, MPI_Fint *f_comm,
+                                  MPI_Fint *f_request, MPI_Fint *f_error) {
 
    MPI_Comm c_comm = PMPI_Comm_f2c(*f_comm);
    MPI_Datatype c_datatype = PMPI_Type_f2c(*f_datatype);
