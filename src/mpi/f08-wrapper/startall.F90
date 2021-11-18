ec8f205ff9013862d6071e12b8eceac4ef78e490
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-11-18 07:38:20 +0100

    corrected startall wrapper and interface for Fortran08

--- src/mpi/f08-wrapper/startall.F90
+++ src/mpi/f08-wrapper/startall.F90
@@ -29,11 +29,19 @@ SUBROUTINE MPI_Startall_f08(count, array_of_requests, error)
    TYPE(MPI_Request), INTENT(INOUT) :: array_of_requests(count)
    INTEGER, OPTIONAL, INTENT(OUT) :: error
    INTEGER :: tmperror
+   INTEGER, DIMENSION(count) :: tmparray_of_requests
+   INTEGER :: i
 
    IF (vftr_no_mpi_logging_f08()) THEN
       CALL PMPI_Startall(count, array_of_requests, tmperror)
    ELSE
-      CALL vftr_MPI_Startall_f2vftr(count, array_of_requests, tmperror)
+      DO i = 1, count
+         tmparray_of_requests(i) = array_of_requests(i)%MPI_VAL
+      END DO
+      CALL vftr_MPI_Startall_f082vftr(count, tmparray_of_requests, tmperror)
+      DO i = 1, count
+         array_of_requests(i)%MPI_VAL = tmparray_of_requests(i)
+      END DO
    END IF
    IF (PRESENT(error)) error = tmperror
 
