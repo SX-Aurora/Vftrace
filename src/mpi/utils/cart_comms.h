ff89d5b9eab574acd608c990b0841a83fe17f1e1
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-11-25 13:23:08 +0100

    adjusted header file

--- src/mpi/utils/cart_comms.h
+++ src/mpi/utils/cart_comms.h
@@ -24,6 +24,6 @@
 // get the list of neighbor ranks in a cartesian communicator
 void vftr_mpi_cart_neighbor_ranks(MPI_Comm cart_comm,
                                   int *nneighbors_ptr,
-                                  int **neighbors_ptr) {
+                                  int **neighbors_ptr);
 
 #endif
