ac498479f64ad85e5b41d2b936d7555a3ea7f828
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-11-26 10:50:49 +0100

    improved testscripts for cartesian neighbor_allgather routines

--- test/mpi/testscripts/neighbor_allgather_cart.sh
+++ test/mpi/testscripts/neighbor_allgather_cart.sh
@@ -15,99 +15,104 @@ do
    ${MPI_EXEC} ${MPI_OPTS} ${NP} ${nprocs} ./${vftr_binary} ${nb} || exit 1
 
    # A neighbor list needs to be created for checking the communication
-   neighborlist[0]="-1  2 -1  1 -1 -1"
-   neighborlist[1]="-1  3  0 -1 -1 -1"
-   neighborlist[2]=" 0 -1 -1  3 -1 -1"
-   neighborlist[3]=" 1 -1  2 -1 -1 -1"
+   neighborlist[0]="1 2"
+   neighborlist[1]="0 3"
+   neighborlist[2]="0 3"
+   neighborlist[3]="1 2"
+   msgnum[0]="1 1"
+   msgnum[1]="1 1"
+   msgnum[2]="1 1"
+   msgnum[3]="1 1"
    # check each rank for the correct message communication
    # patterns in the vfd file
    for irank in $(seq 0 1 $(bc <<< "${nprocs}-1"));
    do
       ../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd
 
-      for jrank in ${neighborlist[${irank}]};
+      npeers=$(echo "${neighborlist[${irank}]}" | wc -w)
+      for peer_idx in $(seq 1 1 ${npeers});
       do
-         if [[ "${jrank}" -gt "0" ]] ; then
-            ipeer=$(bc <<< "${jrank} + 1")
-            # Validate sending
-            # check if every message is unique
-            uniq=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd | \
-                   awk '$2=="send" && $3!="end"{getline;print;}' | \
-                   grep "peer=${jrank}" | \
-                   wc -l)
-            if [[ "${uniq}" -ne "1" ]] ; then
-               echo "Message send from rank ${irank} to ${jrank} appeared ${uniq} times!"
-               echo "Was expecting message to only appear once!"
-               exit 1;
-            fi
-            # Get actually used message size
-            count=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd | \
+         jrank=$(echo "${neighborlist[${irank}]}" | awk -v i=${peer_idx} '{print $i}')
+         msgnum_val=$(echo "${msgnum[${irank}]}" | awk -v i=${peer_idx} '{print $i}')
+         ipeer=$(bc <<< "${jrank} + 1")
+         # Validate sending
+         # check if every message appears the correct amount of times
+         msgcount=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd | \
                     awk '$2=="send" && $3!="end"{getline;print;}' | \
                     grep "peer=${jrank}" | \
-                    sed 's/=/ /g' | \
-                    awk '{print $2}' | \
-                    head -n ${ipeer} | tail -n 1)
-            # get peer process
-            peer=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd | \
-                   awk '$2=="send" && $3!="end"{getline;print;}' | \
-                   grep "peer=${jrank}" | \
-                   sed 's/=/ /g' | \
-                   awk '{print $9}' | \
-                   head -n ${ipeer} | tail -n 1)
-            # Check if actually used message size is consistent
-            # with expected message size
-            if [[ "${count}" -ne "${nb}" ]] ; then
-               echo "Message send size from rank ${irank} to ${jrank} is ${count}!"
-               echo "Was expecting message size of ${nb}!"
-               exit 1;
-            fi
-            # Check if actually used peer process is consistent
-            # with expected peer process
-            if [[ "${peer}" -ne "${jrank}" ]] ; then
-               echo "Message send from rank ${irank} to ${peer}!"
-               echo "Was expecting sending to rank ${jrank}!"
-               exit 1;
-            fi
-            
-            # validate receiving
-            # check if every message is unique
-            uniq=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd | \
-                   awk '$2=="recv" && $3!="end"{getline;print;}' | \
-                   grep "peer=${jrank}" | \
-                   wc -l)
-            if [[ "${uniq}" -ne "1" ]] ; then
-               echo "Message received by rank ${irank} from ${jrank} appeared ${uniq} times!"
-               echo "Was expecting message to only appear once!"
-               exit 1;
-            fi
-            # Get actually used message size
-            count=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd | \
+                    wc -l)
+         if [[ "${msgcount}" -ne "${msgnum_val}" ]] ; then
+            echo "Message send from rank ${irank} to ${jrank} appeared ${msgcount} times!"
+            echo "Was expecting message to appear ${msgnum_val} times!"
+            exit 1;
+         fi
+         # Get actually used message size
+         count=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd | \
+                 awk '$2=="send" && $3!="end"{getline;print;}' | \
+                 grep "peer=${jrank}" | \
+                 sed 's/=/ /g' | \
+                 awk '{print $2}' | \
+                 head -n ${ipeer} | tail -n 1)
+         # get peer process
+         peer=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd | \
+                awk '$2=="send" && $3!="end"{getline;print;}' | \
+                grep "peer=${jrank}" | \
+                sed 's/=/ /g' | \
+                awk '{print $9}' | \
+                head -n ${ipeer} | tail -n 1)
+         # Check if actually used message size is consistent
+         # with expected message size
+         if [[ "${count}" -ne "${nb}" ]] ; then
+            echo "Message send size from rank ${irank} to ${jrank} is ${count}!"
+            echo "Was expecting message size of ${nb}!"
+            exit 1;
+         fi
+         # Check if actually used peer process is consistent
+         # with expected peer process
+         if [[ "${peer}" -ne "${jrank}" ]] ; then
+            echo "Message send from rank ${irank} to ${peer}!"
+            echo "Was expecting sending to rank ${jrank}!"
+            exit 1;
+         fi
+         
+         # validate receiving
+         # check if every message appears the correct amount of times
+         msgcount=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd | \
                     awk '$2=="recv" && $3!="end"{getline;print;}' | \
                     grep "peer=${jrank}" | \
-                    sed 's/=/ /g' | \
-                    awk '{print $2}' | \
-                    head -n ${ipeer} | tail -n 1)
-            # get peer process
-            peer=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd | \
-                   awk '$2=="recv" && $3!="end"{getline;print;}' | \
-                   grep "peer=${jrank}" | \
-                   sed 's/=/ /g' | \
-                   awk '{print $9}' | \
-                   head -n ${ipeer} | tail -n 1)
-            # Check if actually used message size is consistent
-            # with expected message size
-            if [[ "${count}" -ne "${nb}" ]] ; then
-               echo "Message recv size from rank ${jrank} to ${irank} is ${count}!"
-               echo "Was expecting message size of ${nb}!"
-               exit 1;
-            fi
-            # Check if actually used peer process is consistent
-            # with expected peer process
-            if [[ "${peer}" -ne "${jrank}" ]] ; then
-               echo "Message received from rank ${peer} by ${irank}!"
-               echo "Was expecting receiving from rank ${jrank}!"
-               exit 1;
-            fi
+                    wc -l)
+         if [[ "${msgcount}" -ne "${msgnum_val}" ]] ; then
+            echo "Message received by rank ${irank} from ${jrank} appeared ${msgcount} times!"
+            echo "Was expecting message to appear ${msgnum_val} times!"
+            exit 1;
+         fi
+         # Get actually used message size
+         count=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd | \
+                 awk '$2=="recv" && $3!="end"{getline;print;}' | \
+                 grep "peer=${jrank}" | \
+                 sed 's/=/ /g' | \
+                 awk '{print $2}' | \
+                 head -n ${ipeer} | tail -n 1)
+         # get peer process
+         peer=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${irank}.vfd | \
+                awk '$2=="recv" && $3!="end"{getline;print;}' | \
+                grep "peer=${jrank}" | \
+                sed 's/=/ /g' | \
+                awk '{print $9}' | \
+                head -n ${ipeer} | tail -n 1)
+         # Check if actually used message size is consistent
+         # with expected message size
+         if [[ "${count}" -ne "${nb}" ]] ; then
+            echo "Message recv size from rank ${jrank} to ${irank} is ${count}!"
+            echo "Was expecting message size of ${nb}!"
+            exit 1;
+         fi
+         # Check if actually used peer process is consistent
+         # with expected peer process
+         if [[ "${peer}" -ne "${jrank}" ]] ; then
+            echo "Message received from rank ${peer} by ${irank}!"
+            echo "Was expecting receiving from rank ${jrank}!"
+            exit 1;
          fi
       done
    done
