fa533e82af37c07553913de2b6379dc8d4e59640
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-11-30 13:07:58 +0100

    bugfix

--- test/mpi/c-mpi/neighbor_allgatherv_cart_periodic.c
+++ test/mpi/c-mpi/neighbor_allgatherv_cart_periodic.c
@@ -76,9 +76,9 @@ int main(int argc, char** argv) {
    int *displs = (int*) malloc(nneighbors*sizeof(int));
    int ntot = 0;
    for (int ineighbor=0; ineighbor<nneighbors; ineighbor++) {
-         recvcounts[ineighbor] = nints - my_rank + neighbors[ineighbor];
-         displs[ineighbor] = ntot;
-         ntot += recvcounts[ineighbor];
+      recvcounts[ineighbor] = nints - my_rank + neighbors[ineighbor];
+      displs[ineighbor] = ntot;
+      ntot += recvcounts[ineighbor];
    }
    int *rbuffer = (int*) malloc(ntot*sizeof(int));
    for (int i=0; i<ntot; i++) {rbuffer[i]=-1;}
