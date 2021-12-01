0d80e9efd9d5bc75bb559fe9c437f97e4ac3b07f
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-12-01 14:39:56 +0100

    added c-wrapper for ineighbor_alltoallv

---
+++ src/mpi/c-wrapper/ineighbor_alltoallv.c
@@ -0,0 +1,47 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifdef _MPI
+#include <stdlib.h>
+
+#include <mpi.h>
+
+#include "mpi_logging.h"
+#include "ineighbor_alltoallv_c2vftr.h"
+
+int MPI_Ineighbor_alltoallv(const void *sendbuf, const int *sendcounts,
+                            const int *sdispls, MPI_Datatype sendtype,
+                            void *recvbuf, const int *recvcounts,
+                            const int *rdispls, MPI_Datatype recvtype,
+                            MPI_Comm comm, MPI_Request *request) {
+   if (vftr_no_mpi_logging()) {
+      return PMPI_Ineighbor_alltoallv(sendbuf, sendcounts,
+                                      sdispls, sendtype,
+                                      recvbuf, recvcounts,
+                                      rdispls, recvtype,
+                                      comm, request);
+   } else {
+      return vftr_MPI_Ineighbor_alltoallv_c2vftr(sendbuf, sendcounts,
+                                                 sdispls, sendtype,
+                                                 recvbuf, recvcounts,
+                                                 rdispls, recvtype,
+                                                 comm, request);
+   }
+}
+
+#endif
