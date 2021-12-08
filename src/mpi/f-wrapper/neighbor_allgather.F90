86f277a5edfe9b33c2342f66063c656d0892cfaa
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-12-08 08:45:42 +0100

    added neighbor_allgather fortran wrapper

---
+++ src/mpi/f-wrapper/neighbor_allgather.F90
@@ -0,0 +1,54 @@
+! This file is part of Vftrace.
+!
+! Vftrace is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! Vftrace is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License along
+! with this program; if not, write to the Free Software Foundation, Inc.,
+! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#ifdef _MPI
+
+SUBROUTINE MPI_NEIGHBOR_ALLGATHER(SENDBUF, SENDCOUNT, SENDTYPE, &
+                                  RECVBUF, RECVCOUNT, RECVTYPE, &
+                                  COMM, ERROR)
+   USE vftr_mpi_neighbor_allgather_f2vftr_fi, &
+      ONLY : vftr_MPI_Neighbor_allgather_f2vftr
+   USE vftr_mpi_logging_F, &
+      ONLY : vftr_no_mpi_logging_F
+   USE vftr_sync_time_F, &
+      ONLY : vftr_estimate_sync_time
+   USE mpi, &
+      ONLY : PMPI_NEIGHBOR_ALLGATHER
+   IMPLICIT NONE
+   INTEGER SENDBUF
+   INTEGER SENDCOUNT
+   INTEGER SENDTYPE
+   INTEGER RECVBUF
+   INTEGER RECVCOUNT
+   INTEGER RECVTYPE
+   INTEGER COMM
+   INTEGER ERROR
+
+   CALL vftr_estimate_sync_time("MPI_Neighbor_allgather_sync", COMM)
+
+   IF (vftr_no_mpi_logging_F()) THEN
+      CALL PMPI_NEIGHBOR_ALLGATHER(SENDBUF, SENDCOUNT, SENDTYPE, &
+                                   RECVBUF, RECVCOUNT, RECVTYPE, &
+                                   COMM, ERROR)
+   ELSE
+      CALL vftr_MPI_Neighbor_allgather_f2vftr(SENDBUF, SENDCOUNT, SENDTYPE, &
+                                              RECVBUF, RECVCOUNT, RECVTYPE, &
+                                              COMM, ERROR)
+   END IF
+
+END SUBROUTINE MPI_NEIGHBOR_ALLGATHER
+
+#endif
