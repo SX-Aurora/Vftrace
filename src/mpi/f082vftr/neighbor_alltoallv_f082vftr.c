74cac59f413e9242518eb1648f786b54a85d0d9a
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-12-09 09:30:33 +0100

    added translation routine f082vftr neighbor_alltoallv_f082vftr.c

---
+++ src/mpi/f082vftr/neighbor_alltoallv_f082vftr.c
@@ -0,0 +1,125 @@
+/*
+   This file is part of Vftrace.
+
+   Vftrace is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Vftrace is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifdef _MPI
+#include <mpi.h>
+
+#include <stdlib.h>
+
+#include "neighbor_alltoallv.h"
+
+void vftr_MPI_Neighbor_alltoallv_f082vftr(void *sendbuf, MPI_Fint *f_sendcounts,
+                                          MPI_Fint *f_sdispls, MPI_Fint *f_sendtype,
+                                          void *recvbuf, MPI_Fint *f_recvcounts,
+                                          MPI_Fint *f_rdispls, MPI_Fint *f_recvtype,
+                                          MPI_Fint *f_comm, MPI_Fint *f_error) {
+
+   MPI_Comm c_comm = PMPI_Comm_f2c(*f_comm);
+
+   int sizein;
+   int sizeout;
+   // determine the topology of the communicator
+   int topology;
+   PMPI_Topo_test(c_comm, &topology);
+   switch(topology) {
+      case MPI_GRAPH:;
+         int rank;
+         PMPI_Comm_rank(c_comm, &rank);
+         MPI_Graph_neighbors_count(c_comm, rank, &sizein);
+         sizeout = sizein;
+         break;
+      case MPI_CART:
+         MPI_Cartdim_get(c_comm, &sizein);
+         // Number of neighbors for cartesian communicators is always 2*ndims
+         sizein *= 2;
+         sizeout = sizein;
+         break;
+      case MPI_DIST_GRAPH:;
+         int weighted;
+         PMPI_Dist_graph_neighbors_count(c_comm, &sizein,
+                                         &sizeout, &weighted);
+         break;
+   }
+
+   int *c_sendcounts = (int*) malloc(sizeout*sizeof(int));
+   for (int i=0; i<sizeout; i++) {
+      c_sendcounts[i] = (int) f_sendcounts[i];
+   }
+   int *c_sdispls = (int*) malloc(sizeout*sizeof(int));
+   for (int i=0; i<sizeout; i++) {
+      c_sdispls[i] = (int) f_sdispls[i];
+   }
+
+   int *c_recvcounts = (int*) malloc(sizein*sizeof(int));
+   for (int i=0; i<sizein; i++) {
+      c_recvcounts[i] = (int) f_recvcounts[i];
+   }
+   int *c_rdispls = (int*) malloc(sizein*sizeof(int));
+   for (int i=0; i<sizein; i++) {
+      c_rdispls[i] = (int) f_rdispls[i];
+   }
+
+   MPI_Datatype c_sendtype = PMPI_Type_f2c(*f_sendtype);
+   MPI_Datatype c_recvtype = PMPI_Type_f2c(*f_recvtype);
+
+   int c_error;
+   switch(topology) {
+      case MPI_GRAPH:
+         c_error = vftr_MPI_Neighbor_alltoallv_graph(sendbuf,
+                                                     c_sendcounts,
+                                                     c_sdispls,
+                                                     c_sendtype,
+                                                     recvbuf,
+                                                     c_recvcounts,
+                                                     c_rdispls,
+                                                     c_recvtype,
+                                                     c_comm);
+         break;
+      case MPI_CART:
+         c_error = vftr_MPI_Neighbor_alltoallv_cart(sendbuf,
+                                                    c_sendcounts,
+                                                    c_sdispls,
+                                                    c_sendtype,
+                                                    recvbuf,
+                                                    c_recvcounts,
+                                                    c_rdispls,
+                                                    c_recvtype,
+                                                    c_comm);
+         break;
+      case MPI_DIST_GRAPH:
+         c_error = vftr_MPI_Neighbor_alltoallv_dist_graph(sendbuf,
+                                                          c_sendcounts,
+                                                          c_sdispls,
+                                                          c_sendtype,
+                                                          recvbuf,
+                                                          c_recvcounts,
+                                                          c_rdispls,
+                                                          c_recvtype,
+                                                          c_comm);
+         break;
+   }
+
+   free(c_sendcounts);
+   free(c_sdispls);
+   free(c_recvcounts);
+   free(c_rdispls);
+
+   *f_error = (MPI_Fint) (c_error);
+}
+
+#endif
