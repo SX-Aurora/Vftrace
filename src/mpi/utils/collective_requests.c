c18ab4c671874a0bc2ca26568b327978a14b257e
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-02-11 06:47:48 +0100

    adjusted collective_request registration to new data-structure

--- src/mpi/utils/collective_requests.c
+++ src/mpi/utils/collective_requests.c
@@ -26,15 +26,13 @@
 #include "vftr_timer.h"
 #include "vftr_filewrite.h"
 
-vftr_request_t *vftr_open_collective_request_list = NULL;
-
 void vftr_register_collective_request(vftr_direction dir, int nmsg, int *count,
                                       MPI_Datatype *type, int *peer_rank,
                                       MPI_Comm comm, MPI_Request request,
                                       int n_tmp_ptr, void **tmp_ptrs,
                                       long long tstart) {
 
-   vftr_request_t *new_request = vftr_new_request(dir, nmsg, count, type, -1, comm, request, n_tmp_ptr, tmp_ptrs, tstart);
+   vftr_request_t *new_request = vftr_register_request(dir, nmsg, count, type, -1, comm, request, n_tmp_ptr, tmp_ptrs, tstart);
    int isintercom;
    PMPI_Comm_test_inter(comm, &isintercom);
    // translate the ranks to MPI_COMM_WORLD ranks
@@ -53,76 +51,52 @@ void vftr_register_collective_request(vftr_direction dir, int nmsg, int *count,
          }
       }
    }
-
-   vftr_request_prepend(&vftr_open_collective_request_list, new_request);
+   new_request->request_kind = collective;
+   new_request->persistent = false;
 }
 
-void vftr_clear_completed_collective_requests() {
-   // go through the complete list and check the request
-   vftr_request_t *current_request = vftr_open_collective_request_list;
-   while (current_request != NULL) {
-      // Test if the current request is still ongoing or completed
-      // Note: MPI_Test is a destructive test. It will destroy the request
-      //       Thus, running with and without vftr can lead to different program executions
-      //       MPI_Request_get_status is a non destructive status check
-      int flag;
-      PMPI_Request_get_status(current_request->request,
-                              &flag,
-                              MPI_STATUS_IGNORE);
+void vftr_clear_completed_collective_request(vftr_request_t *request) {
+   // Test if the current request is still ongoing or completed
+   // Note: MPI_Test is a destructive test. It will destroy the request
+   //       Thus, running with and without vftr can lead to different program executions
+   //       MPI_Request_get_status is a non destructive status check
+   int flag;
+   PMPI_Request_get_status(request->request,
+                           &flag,
+                           MPI_STATUS_IGNORE);
 
-      // if the requested communication is finished write the communication out
-      // and remove the request from the request list
-      if (flag) {
-         // record the time when the communication is finished
-         // (might be to late, but thats as accurate as we can make it
-         //  without violating the MPI-Standard)
-         // Therefore: measures asynchronous communication with vftrace always
-         //            yields to small bandwidth.
-         long long tend = vftr_get_runtime_usec ();
+   // if the requested communication is finished write the communication out
+   // and remove the request from the request list
+   if (flag) {
+      // record the time when the communication is finished
+      // (might be to late, but thats as accurate as we can make it
+      //  without violating the MPI-Standard)
+      // Therefore: measures asynchronous communication with vftrace always
+      //            yields to small bandwidth.
+      long long tend = vftr_get_runtime_usec ();
 
-         // Every rank should already be translated to the global rank
-         // by the register routine
-         if (vftr_environment.do_sampling->value) {
-            for (int i=0; i<current_request->nmsg; i++) {
-               // if a rank is -1 skip the registering, as
-               // it is an invalid rank due to non periodic
-               // cartesian communicators.
-               if (current_request->rank[i] != -1) {
-                  vftr_store_message_info(current_request->dir,
-                                          current_request->count[i],
-                                          current_request->type_idx[i],
-                                          current_request->type_size[i],
-                                          current_request->rank[i],
-                                          current_request->tag,
-                                          current_request->tstart,
-                                          tend,
-                                          current_request->callingstackID);
-               }
+      // Every rank should already be translated to the global rank
+      // by the register routine
+      if (vftr_environment.do_sampling->value) {
+         for (int i=0; i<request->nmsg; i++) {
+            // if a rank is -1 skip the registering, as
+            // it is an invalid rank due to non periodic
+            // cartesian communicators.
+            if (request->rank[i] != -1) {
+               vftr_store_message_info(request->dir,
+                                       request->count[i],
+                                       request->type_idx[i],
+                                       request->type_size[i],
+                                       request->rank[i],
+                                       request->tag,
+                                       request->tstart,
+                                       tend,
+                                       request->callingstackID);
             }
          }
-
-         // Take the request out of the list and close the gap
-         vftr_remove_request(&vftr_open_collective_request_list, current_request);
-
-         // create a temporary pointer to the current element to be used for deallocation
-         vftr_request_t * tmp_current_request = current_request;
-         // advance in list
-         current_request = current_request->next;
-         vftr_free_request(&tmp_current_request);
-      } else {
-         // advance in list
-         current_request = current_request->next;
       }
-   } // end of while loop
-}
 
-int vftr_number_of_open_collective_requests() {
-   int nrequests = 0;
-   // go through the complete list and check the request
-   vftr_request_t *current_request = vftr_open_collective_request_list;
-   while (current_request != NULL) {
-      nrequests++;
-      current_request = current_request->next;
+      // Take the request out of the list
+      vftr_remove_request(request);
    }
-   return nrequests;
 }
