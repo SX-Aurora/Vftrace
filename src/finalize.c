7e6c9eb8ebd7cf26ef76d3c3c965a70ecfe75be7
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-02-23 11:36:35 +0100

    added finalize routine

---
+++ src/finalize.c
@@ -0,0 +1,26 @@
+#ifdef _DEBUG
+#include <stdio.h>
+#endif
+
+#include "off_hooks.h"
+#include "cyghooks.h"
+#include "vftr_hooks.h"
+#include "vftrace_state.h"
+
+void vftr_finalize(void *func, void *caller) {
+
+   // update the vftrace state
+   vftrace.state = finalized;
+
+
+
+
+
+
+
+
+   // redirect the function entry and exit hooks to deactivate them
+   // use a dummy function that does nothing
+   vftr_set_enter_func_hook(vftr_function_hook_off);
+   vftr_set_exit_func_hook(vftr_function_hook_off);
+}
---
+++ src/finalize.h
@@ -0,0 +1,6 @@
+#ifndef FINALIZE_H
+#define FINALIZE_H
+
+void vftr_finalize(void *func, void *caller);
+
+#endif
