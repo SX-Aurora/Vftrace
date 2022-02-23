f2d68021f35337ee6160cd176f5cb2d8063360b9
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-02-23 10:31:47 +0100

    removed all sources

--- src/vftr_dlopen.h
+++
@@ -1,9 +0,0 @@
-#ifndef VFTR_DLOPEN_H
-#define VFTR_DLOPEN_H
-
-int lib_opened;
-char *dlopened_lib;
-
-void vftr_copy_symtab(int rank, char *foo);
-
-#endif
--- src/vftr_signals.h
+++
@@ -1,7 +0,0 @@
-#ifndef VFTR_SIGNALS_H
-#define VFTR_SIGNALS_H
-
-void vftr_abort();
-void vftr_setup_signals();
-
-#endif
