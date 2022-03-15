0f927282cc54eb48af55e60368c6769ca0e1d6cb
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-03-15 14:11:16 +0100

    started printing the profile table

--- src/log_profile.c
+++ src/log_profile.c
@@ -1,4 +1,7 @@
+#include <stdlib.h>
+
 #include "stack_types.h"
+#include "table_types.h"
 
 long long vftr_total_overhead_usec(stacktree_t stacktree) {
    long long overhead = 0;
@@ -6,4 +9,74 @@ long long vftr_total_overhead_usec(stacktree_t stacktree) {
       overhead += stacktree.stacks[istack].profiling.callProf.overhead_time_usec;
    }
    return overhead;
+}
+
+int *vftr_stack_calls_list(int nstacks, stack_t *stacks) {
+   int *calls_list = (int*) malloc(nstacks*sizeof(int));
+
+   for (int istack=0; istack<nstacks; istack++) {
+      calls_list[istack] = stacks[istack].profiling.callProf.calls;
+   }
+   return calls_list;
+}
+
+double *vftr_stack_inclusive_time_list(int nstacks, stack_t *stacks) {
+   double *inclusive_time_list = (double*) malloc(nstacks*sizeof(double));
+ 
+   for (int istack=0; istack<nstacks; istack++) {
+      inclusive_time_list[istack] = stacks[istack].profiling.callProf.time_usec;
+      inclusive_time_list[istack] *= 1.0e-6;
+   }
+   // the init function has as inclusive time the inclusive time of main
+   inclusive_time_list[0] = stacks[1].profiling.callProf.time_usec * 1.0e-6;
+   return inclusive_time_list;
+}
+
+void vftr_stack_compute_exclusive_time(stack_t *stacks, int myID,
+                                       double *exclusive_time_list) {
+   stack_t *mystack = stacks+myID;
+   exclusive_time_list[myID] = mystack->profiling.callProf.time_usec; 
+   for (int icallee=0; icallee<mystack->ncallees; icallee++) {
+      int calleeID = mystack->callees[icallee];
+      exclusive_time_list[myID] -= stacks[calleeID].profiling.callProf.time_usec;
+      vftr_stack_compute_exclusive_time(stacks, calleeID, exclusive_time_list);
+   }
+   exclusive_time_list[myID] *= 1.0e-6;
+}
+
+double *vftr_stack_exclusive_time_list(int nstacks, stack_t *stacks) {
+   double *exclusive_time_list = (double*) malloc(nstacks*sizeof(double));
+   vftr_stack_compute_exclusive_time(stacks, 0, exclusive_time_list);
+   // the init function has no exclusive time.
+   exclusive_time_list[0] = 0.0;
+   return exclusive_time_list;
+}
+
+double *vftr_stack_overhead_time_list(int nstacks, stack_t *stacks) {
+   double *overhead_time_list = (double*) malloc(nstacks*sizeof(double));
+ 
+   for (int istack=0; istack<nstacks; istack++) {
+      overhead_time_list[istack] = stacks[istack].profiling.callProf.overhead_time_usec;
+      overhead_time_list[istack] *= 1.0e-6;
+   }
+   return overhead_time_list;
+}
+
+char **vftr_stack_function_name_list(int nstacks, stack_t *stacks) {
+   char **name_list = (char**) malloc(nstacks*sizeof(char*));
+   for (int istack=0; istack<nstacks; istack++) {
+      name_list[istack] = stacks[istack].name;
+   }
+   return name_list;
+}
+
+char **vftr_stack_caller_name_list(int nstacks, stack_t *stacks) {
+   char **name_list = (char**) malloc(nstacks*sizeof(char*));
+   // the init function is never called
+   name_list[0] = "----";
+   for (int istack=1; istack<nstacks; istack++) {
+      int callerID = stacks[istack].caller;
+      name_list[istack] = stacks[callerID].name;
+   }
+   return name_list;
 }
--- src/logfile.c
+++ src/logfile.c
@@ -4,15 +4,17 @@
 #include <string.h>
 #include <libgen.h>
 
+#include "timer_types.h"
+#include "table_types.h"
 #include "environment_types.h"
 #include "vftrace_state.h"
 
 #include "exe_info.h"
 #include "misc_utils.h"
-#include "timer_types.h"
 #include "license.h"
 #include "config.h"
 #include "log_profile.h"
+#include "tables.h"
 
 char *vftr_get_logfile_name(environment_t environment, int rankID, int nranks) {
    if (environment.logfile_basename.set) {
@@ -82,7 +84,43 @@ void vftr_write_logfile_summary(FILE *fp, vftrace_t vftrace, long long runtime) 
    // TODO: Add MPI overhead
    // TODO: distinguish between overhead from threads and regular overhead
    // TODO: Add Performance counters
-   fprintf(fp, "\n------------------------------------------------------------\n");
+   fprintf(fp, "------------------------------------------------------------\n");
+}
+
+void vftr_write_logfile_profile_table(FILE *fp, stacktree_t stacktree,
+                                      environment_t environment,
+                                      long long runtime) {
+   fprintf(fp, "\nRuntime profile\n");
+
+   table_t table = vftr_new_table();
+   vftr_table_set_nrows(&table, stacktree.nstacks);
+
+   int *calls = vftr_stack_calls_list(stacktree.nstacks, stacktree.stacks);
+   vftr_table_add_column(&table, col_int, "Calls", "%d", 'c', 'r', (void*) calls);
+
+   double *excl_time = vftr_stack_exclusive_time_list(stacktree.nstacks, stacktree.stacks);
+   vftr_table_add_column(&table, col_double, "t_excl / s", "%.3f", 'c', 'r', (void*) excl_time);
+
+   double *incl_time = vftr_stack_inclusive_time_list(stacktree.nstacks, stacktree.stacks);
+   vftr_table_add_column(&table, col_double, "t_incl / s", "%.3f", 'c', 'r', (void*) incl_time);
+   
+  // 
+  // double *vftr_stack_overhead_time_list(int nstacks, stack_t *stacks);
+
+   char **function_names = vftr_stack_function_name_list(stacktree.nstacks, stacktree.stacks);
+   vftr_table_add_column(&table, col_string, "Function", "%s", 'c', 'r', (void*) function_names);
+
+   char **caller_names = vftr_stack_caller_name_list(stacktree.nstacks, stacktree.stacks);
+   vftr_table_add_column(&table, col_string, "Caller", "%s", 'c', 'r', (void*) caller_names);
+
+   vftr_print_table(fp, table);
+
+   vftr_table_free(&table);
+   free(calls);
+   free(excl_time);
+   free(incl_time);
+   free(function_names);
+   free(caller_names);
 }
 
 void vftr_write_logfile(vftrace_t vftrace, long long runtime) {
@@ -100,6 +138,8 @@ void vftr_write_logfile(vftrace_t vftrace, long long runtime) {
 
    vftr_write_logfile_summary(fp, vftrace, runtime);
 
+   vftr_write_logfile_profile_table(fp, vftrace.process.stacktree,
+                                    vftrace.environment, runtime);
 
 
 
