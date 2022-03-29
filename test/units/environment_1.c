9b97ed54babc5cd6754a1178f0f8d2a0917b4884
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-03-29 09:16:11 +0200

    check environment printing

--- test/units/environment_1.c
+++ test/units/environment_1.c
@@ -1,41 +1,43 @@
-#include "vftr_environment.h"
-#include "vftr_setup.h"
+#include <stdlib.h>
+
+#include "environment_types.h"
+#include "environment.h"
+
 #ifdef _MPI
 #include <mpi.h>
 #endif
 
 
-int main (int argc, char **argv) {
-
+int main(int argc, char **argv) {
 #if defined(_MPI)
   PMPI_Init(&argc, &argv);
-  vftr_get_mpi_info (&vftr_mpirank, &vftr_mpisize);
+  //vftr_get_mpi_info(&vftr_mpirank, &vftr_mpisize);
 #else 
-  vftr_mpirank = 0;
-  vftr_mpisize = 1;
+  //vftr_mpirank = 0;
+  //vftr_mpisize = 1;
 #endif
-
   // The basic usage of the environment module:
-  putenv ("VFTR_OFF=yes");
-  vftr_read_environment();
-  vftr_assert_environment();
-  vftr_print_environment(stdout);
-  vftr_free_environment();
+  putenv("VFTR_OFF=yes");
+  environment_t environment;
+  environment = vftr_read_environment();
+  // TODO: vftr_assert_environment();
+  vftr_print_env(stdout, environment);
+  vftr_environment_free(&environment);
 
-  // Check each of the different possible data types (except regular expression).
-  fprintf (stdout, "***************************\n");
-  putenv ("VFTR_OFF=no");
-  putenv ("VFTR_SAMPLING=YES");
-  putenv ("VFTR_REGIONS_PRECISE=0");
-  putenv ("VFTR_MPI_LOG=on");
-  putenv ("VFTR_OUT_DIRECTORY=\"foo/bar\"");
-  putenv ("VFTR_BUFSIZE=1234");
-  putenv ("VFTR_SAMPLETIME=12.34");
+  // Check each of the different possible data types(except regular expression).
+  fprintf(stdout, "***************************\n");
+  putenv("VFTR_OFF=no");
+  putenv("VFTR_SAMPLING=YES");
+  putenv("VFTR_REGIONS_PRECISE=0");
+  putenv("VFTR_MPI_LOG=on");
+  putenv("VFTR_OUT_DIRECTORY=\"foo/bar\"");
+  putenv("VFTR_BUFSIZE=1234");
+  putenv("VFTR_SAMPLETIME=12.34");
   
-  vftr_read_environment ();
-  vftr_assert_environment ();
-  vftr_print_environment (stdout);
-  vftr_free_environment ();
+  environment = vftr_read_environment();
+  // TODO: vftr_assert_environment();
+  vftr_print_env(stdout, environment);
+  vftr_environment_free(&environment);
 
 #ifdef _MPI
   PMPI_Finalize();
--- test/units/environment_2.c
+++ test/units/environment_2.c
@@ -1,5 +1,8 @@
-#include "vftr_environment.h"
-#include "vftr_setup.h"
+#include <stdlib.h>
+
+#include "environment_types.h"
+#include "environment.h"
+
 #ifdef _MPI
 #include <mpi.h>
 #endif
@@ -9,20 +12,21 @@ int main (int argc, char **argv) {
 
 #if defined(_MPI)
   PMPI_Init(&argc, &argv);
-  vftr_get_mpi_info (&vftr_mpirank, &vftr_mpisize);
+//  vftr_get_mpi_info (&vftr_mpirank, &vftr_mpisize);
 #else 
-  vftr_mpirank = 0;
-  vftr_mpisize = 1;
+ // vftr_mpirank = 0;
+  //vftr_mpisize = 1;
 #endif
 
   // Check if the environment advisor works
   putenv ("VFTR_OF=yes"); // Should be VFTR_OFF
   putenv ("VFTR_TRUNCATE=yes"); // Should be VFTR_PROF_TRUNCATE
 
-  vftr_read_environment ();
-  vftr_assert_environment ();
-  vftr_check_env_names (stdout);
-  vftr_free_environment ();
+  environment_t environment;
+  environment = vftr_read_environment();
+  // TODO: vftr_assert_environment ();
+  // TODO: vftr_check_env_names(stdout);
+  vftr_environment_free(&environment);
 
 #ifdef _MPI
   PMPI_Finalize();
