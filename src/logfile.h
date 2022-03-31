3966c85547be8e0cc746df891fdf3aa683ee88b4
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-03-31 12:32:53 +0200

    added missing headers

--- src/logfile.h
+++ src/logfile.h
@@ -1,6 +1,9 @@
 #ifndef LOGFILE_H
 #define LOGFILE_H
 
+#include "environment_types.h"
+#include "vftrace_state.h"
+
 char *vftr_get_logfile_name(environment_t environment, int rankID, int nranks);
 
 void vftr_write_logfile(vftrace_t vftrace, long long runtime);
