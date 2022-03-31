c1eb05838de839dc9be03ef62edd603249665dc0
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-03-31 12:31:33 +0200

    added logfile name function to header

--- src/logfile.h
+++ src/logfile.h
@@ -1,6 +1,8 @@
 #ifndef LOGFILE_H
 #define LOGFILE_H
 
+char *vftr_get_logfile_name(environment_t environment, int rankID, int nranks);
+
 void vftr_write_logfile(vftrace_t vftrace, long long runtime);
 
 #endif
