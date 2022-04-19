782f969d98ddb4c0c6aa6d4adb24d94e7b115aec
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-04-19 07:44:17 +0200

    prettify

--- test/units/collatehashes_parallel.sh
+++ test/units/collatehashes_parallel.sh
@@ -7,20 +7,9 @@ ref_file=${srcdir}/ref_output/${test_name}.out
 rm -f ${output_file}
 rm ${test_name}_p*.tmpout
 
-if [ "x$HAS_MPI" == "xYES" ]; then
-   ${MPI_EXEC} ${MPI_OPTS} ${NP} 2 ./${test_name} || exit 1
-else
-   ./${test_name} || exit 1
-fi
-
-last_success=$?
+${MPI_EXEC} ${MPI_OPTS} ${NP} 2 ./${test_name} || exit 1
 
 cat ${test_name}_p0.tmpout \
     ${test_name}_p1.tmpout > ${output_file}
 
-if [ $last_success == 0 ]; then
-  diff ${output_file} ${ref_file}
-else
-  exit  ${last_success}
-fi
-
+diff ${output_file} ${ref_file}
