6730a516d4962df0f79a3cd7748f04f835579766
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-04-28 12:55:31 +0200

    prettify hooks

--- src/hook_types.h
+++ src/hook_types.h
@@ -1,7 +1,7 @@
 #ifndef HOOK_TYPES_H
 #define HOOK_TYPES_H
 
-// function pointers which will be called by the hooks
+// function pointers which will be called by the cyg-hooks
 // On the first function entry vftrace will be initialized.
 // After that the function pointers will be redirected to the
 // actual hook functionality or a dummy function if vftrace is off.
--- src/off_hooks.c
+++ src/off_hooks.c
@@ -1,4 +1,4 @@
 void vftr_function_hook_off(void *func, void *call_site) {
-   (void)func;
-   (void)call_site;
+   (void) func;
+   (void) call_site;
 }
