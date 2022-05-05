43ea69d79b81968920c923675f6b661a2f51e9dd
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-05 14:20:37 +0200

    externalized sync-time estimation in scan.c

--- src/mpi/c-wrapper/scan.c
+++ src/mpi/c-wrapper/scan.c
@@ -20,19 +20,12 @@
 #include <mpi.h>
 
 #include "mpi_logging.h"
-#include "vftr_regions.h"
-#include "vftr_environment.h"
 #include "scan_c2vftr.h"
 
 int MPI_Scan(const void *sendbuf, void *recvbuf, int count,
              MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
 
-   // Estimate synchronization time
-   if (vftr_environment.mpi_show_sync_time->value) {
-      vftr_internal_region_begin("MPI_Scan_sync");
-      PMPI_Barrier(comm);
-      vftr_internal_region_end("MPI_Scan_sync");
-   }
+   vftr_estimate_sync_time("MPI_Scan_sync", comm);
 
    if (vftr_no_mpi_logging()) {
       return PMPI_Scan(sendbuf, recvbuf, count, datatype, op, comm);
