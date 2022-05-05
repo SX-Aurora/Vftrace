028bb9756b99a920c8de3d11faae6297979848a1
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-05 14:23:13 +0200

    adjusted header and functions interfaces to new structure in mpi/utils/p2p_requests.h

--- src/mpi/utils/p2p_requests.h
+++ src/mpi/utils/p2p_requests.h
@@ -21,12 +21,12 @@
 
 #include "requests.h"
 
-void vftr_register_p2p_request(vftr_direction dir, int count,
+void vftr_register_p2p_request(message_direction dir, int count,
                                MPI_Datatype type, int peer_rank, int tag,
                                MPI_Comm comm, MPI_Request request,
                                long long tstart);
 
-void vftr_register_pers_p2p_request(vftr_direction dir, int count,
+void vftr_register_pers_p2p_request(message_direction dir, int count,
                                     MPI_Datatype type, int peer_rank, int tag,
                                     MPI_Comm comm, MPI_Request request);
 
