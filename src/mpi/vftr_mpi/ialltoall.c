9cb9b12b1874f09097dd91a9aba2d44c9fc64702
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-05 14:23:14 +0200

    adjusted header and functions interfaces to new structure in mpi/vftr_mpi/ialltoall.c

--- src/mpi/vftr_mpi/ialltoall.c
+++ src/mpi/vftr_mpi/ialltoall.c
@@ -21,7 +21,7 @@
 #include <stdlib.h>
 
 #include "rank_translate.h"
-#include "vftr_timer.h"
+#include "timer.h"
 #include "collective_requests.h"
 
 int vftr_MPI_Ialltoall(const void *sendbuf, int sendcount,
@@ -65,7 +65,7 @@ int vftr_MPI_Ialltoall(const void *sendbuf, int sendcount,
    peer_ranks = NULL;
    long long t2end = vftr_get_runtime_usec();
 
-   vftr_mpi_overhead_usec += t2end - t2start;
+   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
 
    return retVal;
 }
@@ -117,7 +117,7 @@ int vftr_MPI_Ialltoall_inplace(const void *sendbuf, int sendcount,
    }
    long long t2end = vftr_get_runtime_usec();
 
-   vftr_mpi_overhead_usec += t2end - t2start;
+   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
 
    return retVal;
 }
@@ -172,7 +172,7 @@ int vftr_MPI_Ialltoall_intercom(const void *sendbuf, int sendcount,
    peer_ranks = NULL;
    long long t2end = vftr_get_runtime_usec();
 
-   vftr_mpi_overhead_usec += t2end - t2start;
+   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
 
    return retVal;
 }
