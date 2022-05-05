129cac302b200823bb6f73864a846f0f0afbfa14
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-05 14:23:13 +0200

    adjusted header and functions interfaces to new structure in mpi/utils/sync_messages.h

--- src/mpi/utils/sync_messages.h
+++ src/mpi/utils/sync_messages.h
@@ -21,7 +21,7 @@
 
 #include "mpi_util_types.h"
 
-void vftr_store_sync_message_info(vftr_direction dir, int count, MPI_Datatype type,
+void vftr_store_sync_message_info(message_direction dir, int count, MPI_Datatype type,
                                   int peer_rank, int tag, MPI_Comm comm, 
                                   long long tstart, long long tend);
 
