fc40dc72b2978f70e31620f00f410d8825ac78a9
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-05 14:23:13 +0200

    adjusted header and functions interfaces to new structure in mpi/utils/mpi_logging.c

--- src/mpi/utils/mpi_logging.c
+++ src/mpi/utils/mpi_logging.c
@@ -18,20 +18,42 @@
 
 #include <stdbool.h>
 
-#include "vftr_environment.h"
-#include "vftr_pause.h"
-#include "pcontrol.h"
+#include "vftrace_state.h"
+#include "sampling_types.h"
+#include "mpi_util_types.h"
 
 // determine based on several criteria if
 // the communication should just be executed or also logged
 bool vftr_no_mpi_logging() {
-   return vftrace_Pcontrol_level == 0 || 
-          vftr_off() ||
-          !vftr_environment.mpi_log->value ||
-          vftr_paused;
+   return vftrace.mpi_state.pcontrol_level == 0 || 
+          vftrace.state == off ||
+          !vftrace.environment.mpi_log.value.bool_val ||
+          vftrace.state == paused;
 }
 
 // int version of above function for well defined fortran-interoperability
 int vftr_no_mpi_logging_int() {
    return vftr_no_mpi_logging() ? 1 : 0;
+}
+
+// Store the message information in a vfd file
+void vftr_store_message_info(message_direction dir, int count, int type_idx,
+                             int type_size, int rank, int tag,
+                             long long tstart, long long tend,
+                             int stackID) {
+
+   FILE *fp = vftrace.sampling.vfdfilefp;
+   sample_kind kind = samp_message;
+   fwrite(&kind, sizeof(sample_kind), 1, fp);
+   fwrite(&dir, sizeof(message_direction), 1, fp);
+   fwrite(&rank, sizeof(int), 1, fp);
+   fwrite(&type_idx, sizeof(int), 1, fp);
+   fwrite(&count, sizeof(int), 1, fp);
+   fwrite(&type_size, sizeof(int), 1, fp);
+   fwrite(&tag, sizeof(int), 1, fp);
+   fwrite(&tstart, sizeof(long long), 1, fp);
+   fwrite(&tend, sizeof(long long), 1, fp);
+   fwrite(&stackID, sizeof(int), 1, fp);
+
+   //vftr_message_samplecount++;
 }
