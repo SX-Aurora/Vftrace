2bf0507c2977cd6c4c1ec7632234d511fac8f4c7
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-05 14:20:37 +0200

    externalized sync-time estimation in neighbor_allgather.c

--- src/mpi/c-wrapper/neighbor_allgather.c
+++ src/mpi/c-wrapper/neighbor_allgather.c
@@ -20,19 +20,14 @@
 #include <mpi.h>
 
 #include "mpi_logging.h"
-#include "vftr_regions.h"
-#include "vftr_environment.h"
 #include "neighbor_allgather_c2vftr.h"
 
 int MPI_Neighbor_allgather(const void *sendbuf, int sendcount,
                            MPI_Datatype sendtype, void *recvbuf, int recvcount,
                            MPI_Datatype recvtype, MPI_Comm comm) {
-   // Estimate synchronization time
-   if (vftr_environment.mpi_show_sync_time->value) {
-      vftr_internal_region_begin("MPI_Neighbor_allgather_sync");
-      PMPI_Barrier(comm);
-      vftr_internal_region_end("MPI_Neighbor_allgather_sync");
-   }
+
+   vftr_estimate_sync_time("MPI_Neighbor_allgather_sync", comm);
+
    if (vftr_no_mpi_logging()) {
       return PMPI_Neighbor_allgather(sendbuf, sendcount, sendtype,
                                      recvbuf, recvcount, recvtype,
