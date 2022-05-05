cefa7e014a5b6bceba75e404e84d8813cdd70ffd
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-05 14:23:13 +0200

    adjusted header and functions interfaces to new structure in mpi/utils/mpi_logging.h

--- src/mpi/utils/mpi_logging.h
+++ src/mpi/utils/mpi_logging.h
@@ -21,11 +21,19 @@
 
 #include <stdbool.h>
 
+#include "mpi_util_types.h"
+
 // determine based on several criteria if
 // the communication should just be executed or also logged
 bool vftr_no_mpi_logging();
 
 // int version of above function for well defined fortran-interoperability
 int vftr_no_mpi_logging_int();
+
+// Store the message information in a vfd file
+void vftr_store_message_info(message_direction dir, int count, int type_idx,
+                             int type_size, int rank, int tag,
+                             long long tstart, long long tend,
+                             int stackID);
 
 #endif
