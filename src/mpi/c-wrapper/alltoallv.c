441ea30ba2acdd8fcedfa594d349a922752beb98
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-05 14:20:37 +0200

    externalized sync-time estimation in alltoallv.c

--- src/mpi/c-wrapper/alltoallv.c
+++ src/mpi/c-wrapper/alltoallv.c
@@ -20,8 +20,6 @@
 #include <mpi.h>
 
 #include "mpi_logging.h"
-#include "vftr_regions.h"
-#include "vftr_environment.h"
 #include "alltoallv_c2vftr.h"
 
 int MPI_Alltoallv(const void *sendbuf, const int *sendcounts,
@@ -29,12 +27,9 @@ int MPI_Alltoallv(const void *sendbuf, const int *sendcounts,
                   void *recvbuf, const int *recvcounts,
                   const int *rdispls, MPI_Datatype recvtype,
                   MPI_Comm comm) {
-   // Estimate synchronization time
-   if (vftr_environment.mpi_show_sync_time->value) {
-      vftr_internal_region_begin("MPI_Alltoallv_sync");
-      PMPI_Barrier(comm);
-      vftr_internal_region_end("MPI_Alltoallv_sync");
-   }
+
+   vftr_estimate_sync_time("MPI_Alltoallv_sync", comm);
+
    if (vftr_no_mpi_logging()) {
       return PMPI_Alltoallv(sendbuf, sendcounts,
                             sdispls, sendtype,
