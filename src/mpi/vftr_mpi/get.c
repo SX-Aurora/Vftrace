6dc5ca25750c07c11dc8d95e73979348c5dbf89c
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-05 14:23:14 +0200

    adjusted header and functions interfaces to new structure in mpi/vftr_mpi/get.c

--- src/mpi/vftr_mpi/get.c
+++ src/mpi/vftr_mpi/get.c
@@ -18,7 +18,7 @@
 
 #include <mpi.h>
 
-#include "vftr_timer.h"
+#include "timer.h"
 #include "sync_messages.h"
 
 int vftr_MPI_Get(void *origin_addr, int origin_count,
@@ -50,7 +50,7 @@ int vftr_MPI_Get(void *origin_addr, int origin_count,
                                 global_rank, -1, MPI_COMM_WORLD, tstart, tend);
    long long t2end = vftr_get_runtime_usec();
 
-   vftr_mpi_overhead_usec += t2end - t2start;
+   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
 
    return retVal;
 }
