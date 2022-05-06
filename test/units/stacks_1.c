d272cc331684c5dff160f9186d2a33a4417fa1df
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:54 +0200

    removed trailing whitespaces in test/units/stacks_1.c

--- test/units/stacks_1.c
+++ test/units/stacks_1.c
@@ -11,7 +11,7 @@ int main (int argc, char **argv) {
 #if defined(_MPI)
   PMPI_Init(&argc, &argv);
   vftr_get_mpi_info (&vftr_mpirank, &vftr_mpisize);
-#else 
+#else
   vftr_mpirank = 0;
   vftr_mpisize = 1;
 #endif
@@ -22,12 +22,12 @@ int main (int argc, char **argv) {
   fprintf (stdout, "Initial vftr_stackscount: %d\n", vftr_stackscount);
   function_t *func1 = vftr_new_function (NULL, "init", NULL, false);
   function_t *func2 = vftr_new_function ((void*)addrs, "func2", func1, false);
-  function_t *func3 = vftr_new_function ((void*)(addrs + 1), "func3", func1, false);	
+  function_t *func3 = vftr_new_function ((void*)(addrs + 1), "func3", func1, false);
   function_t *func4 = vftr_new_function ((void*)(addrs + 2), "func4", func3, false);
   function_t *func5 = vftr_new_function ((void*)(addrs + 3), "func5", func2, false);
   function_t *func6 = vftr_new_function ((void*)(addrs + 4), "func6", func2, false);
   function_t *func7 = vftr_new_function ((void*)(addrs + 5), "func4", func6, false);
-  vftr_normalize_stacks();			
+  vftr_normalize_stacks();
   fprintf (stdout, "%s: %d %d\n", func1->name, func1->id, func1->gid);
   fprintf (stdout, "%s: %d %d\n", func2->name, func2->id, func2->gid);
   fprintf (stdout, "%s: %d %d\n", func3->name, func3->id, func3->gid);
