41d1e68224724ae6fa21d9a95bcd60959732e4dc
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:43 +0200

    removed trailing whitespaces in src/mpi/f082vftr/waitsome_f082vftr.c

--- src/mpi/f082vftr/waitsome_f082vftr.c
+++ src/mpi/f082vftr/waitsome_f082vftr.c
@@ -22,7 +22,7 @@
 #include <stdlib.h>
 
 #include "waitsome.h"
-  
+
 void vftr_MPI_Waitsome_f082vftr(MPI_Fint *f_incount, MPI_Fint *f_array_of_requests,
                                 MPI_Fint *f_outcount, MPI_Fint *f_array_of_indices,
                                 MPI_F08_status *f_array_of_statuses, MPI_Fint *f_error) {
