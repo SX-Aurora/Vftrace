2eef07f53e407f0a932d830d659de1ccf207b00a
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:44 +0200

    removed trailing whitespaces in src/mpi/utils/rank_translate.c

--- src/mpi/utils/rank_translate.c
+++ src/mpi/utils/rank_translate.c
@@ -25,7 +25,7 @@ int vftr_local2global_rank(MPI_Comm comm, int local_rank) {
    if (comm == MPI_COMM_WORLD) {
       return local_rank;
    }
-   
+
    // determine global group for the global communicator once
    static MPI_Group global_group = MPI_GROUP_NULL;
    if (global_group == MPI_GROUP_NULL) {
@@ -39,7 +39,7 @@ int vftr_local2global_rank(MPI_Comm comm, int local_rank) {
    // Translate rank from local to global group.
    int global_rank;
    PMPI_Group_translate_ranks(local_group,   // local group
-                              1,             // number of ranks to translate 
+                              1,             // number of ranks to translate
                               &local_rank,   // addr of local rank variable
                               global_group,  // group to translate rank to
                               &global_rank); // addr of global rank variable
@@ -53,7 +53,7 @@ int vftr_remote2global_rank(MPI_Comm comm, int remote_rank) {
    if (comm == MPI_COMM_WORLD) {
       return remote_rank;
    }
-   
+
    // determine global group for the global communicator once
    static MPI_Group global_group = MPI_GROUP_NULL;
    if (global_group == MPI_GROUP_NULL) {
@@ -67,7 +67,7 @@ int vftr_remote2global_rank(MPI_Comm comm, int remote_rank) {
    // Translate rank from remote to global group.
    int global_rank;
    PMPI_Group_translate_ranks(remote_group,  // remote group
-                              1,             // number of ranks to translate 
+                              1,             // number of ranks to translate
                               &remote_rank,  // addr of remote rank variable
                               global_group,  // group to translate rank to
                               &global_rank); // addr of global rank variable
