6808827ef1c364b8df9cdd4938fcaf94b2918fb8
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:46 +0200

    removed trailing whitespaces in src/mpi/vftr_mpi/igather.h

--- src/mpi/vftr_mpi/igather.h
+++ src/mpi/vftr_mpi/igather.h
@@ -24,19 +24,19 @@
 int vftr_MPI_Igather(const void *sendbuf, int sendcount,
                      MPI_Datatype sendtype, void *recvbuf,
                      int recvcount, MPI_Datatype recvtype,
-                     int root, MPI_Comm comm, 
+                     int root, MPI_Comm comm,
                      MPI_Request *request);
 
 int vftr_MPI_Igather_inplace(const void *sendbuf, int sendcount,
                              MPI_Datatype sendtype, void *recvbuf,
                              int recvcount, MPI_Datatype recvtype,
-                             int root, MPI_Comm comm, 
+                             int root, MPI_Comm comm,
                              MPI_Request *request);
 
 int vftr_MPI_Igather_intercom(const void *sendbuf, int sendcount,
                               MPI_Datatype sendtype, void *recvbuf,
                               int recvcount, MPI_Datatype recvtype,
-                              int root, MPI_Comm comm, 
+                              int root, MPI_Comm comm,
                               MPI_Request *request);
 
 #endif
