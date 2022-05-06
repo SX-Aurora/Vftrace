5a45f0c8429419c9507ff6a396e3d0b3a5692840
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:46 +0200

    removed trailing whitespaces in src/mpi/vftr_mpi/raccumulate.c

--- src/mpi/vftr_mpi/raccumulate.c
+++ src/mpi/vftr_mpi/raccumulate.c
@@ -24,7 +24,7 @@
 int vftr_MPI_Raccumulate(const void *origin_addr, int origin_count,
                          MPI_Datatype origin_datatype, int target_rank,
                          MPI_Aint target_disp, int target_count,
-                         MPI_Datatype target_datatype, MPI_Op op, 
+                         MPI_Datatype target_datatype, MPI_Op op,
                          MPI_Win win, MPI_Request *request) {
    long long tstart = vftr_get_runtime_usec();
    int retVal = PMPI_Raccumulate(origin_addr, origin_count, origin_datatype,
