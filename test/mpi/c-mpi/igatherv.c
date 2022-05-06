99f5521972bc51bcdafa13df0a03c92a8ca87df8
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:51 +0200

    removed trailing whitespaces in test/mpi/c-mpi/igatherv.c

--- test/mpi/c-mpi/igatherv.c
+++ test/mpi/c-mpi/igatherv.c
@@ -48,7 +48,7 @@ int main(int argc, char** argv) {
    // Messaging
    MPI_Request myrequest;
    MPI_Igatherv(sbuffer, nints, MPI_INT,
-                rbuffer, recvcounts, displs, MPI_INT, 
+                rbuffer, recvcounts, displs, MPI_INT,
                 rootrank, MPI_COMM_WORLD, &myrequest);
    if (my_rank == rootrank) {
       printf("Gathering messages from all ranks on rank %d\n", my_rank);
