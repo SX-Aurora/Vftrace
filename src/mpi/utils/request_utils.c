8d3f26167cee4e85ab0c4581f49f80ff75642368
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:45 +0200

    removed trailing whitespaces in src/mpi/utils/request_utils.c

--- src/mpi/utils/request_utils.c
+++ src/mpi/utils/request_utils.c
@@ -26,9 +26,9 @@
 bool vftr_mpi_request_is_active(MPI_Request request) {
    // According to the MPI_Standard 3.0 (capter 3.7.3, p.52)
    // a request is active if it is neither a null request
-   // nor returns an empty status for Request_get_status 
+   // nor returns an empty status for Request_get_status
    // (the function returns an empty status if it is inactive)
-   
+
    if (request == MPI_REQUEST_NULL) {
       return false;
    }
