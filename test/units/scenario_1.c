df5e4727f288ec4ce21dafccd31a8baaf81460b3
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:53 +0200

    removed trailing whitespaces in test/units/scenario_1.c

--- test/units/scenario_1.c
+++ test/units/scenario_1.c
@@ -9,7 +9,7 @@ int main (int argc, char **argv) {
 #if defined(_MPI)
   PMPI_Init(&argc, &argv);
   vftr_get_mpi_info (&vftr_mpirank, &vftr_mpisize);
-#else 
+#else
   vftr_mpirank = 0;
   vftr_mpisize = 1;
 #endif
@@ -23,12 +23,12 @@ int main (int argc, char **argv) {
   if (fp_in == NULL) {
     printf ("Scenario file %s not found!\n", argv[1]);
     return 1;
-  } 
+  }
 
   vftr_read_scenario_file ("", fp_in);
   fprintf (stdout, "Registered variables: %d\n", vftr_scenario_expr_n_vars);
   for (int i = 0; i < vftr_scenario_expr_n_vars; i++) {
-  	fprintf (stdout, "%d: name: %s\n", i, te_vars[i].name);		
+  	fprintf (stdout, "%d: name: %s\n", i, te_vars[i].name);
   }
   fprintf (stdout, "Check for the three additional entries: \n");
   fprintf (stdout, "%s\n", te_vars[vftr_scenario_expr_n_vars].name);
