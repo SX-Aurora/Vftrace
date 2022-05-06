46087b372198e8fd11290d1913dea9277a22938b
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:52 +0200

    removed trailing whitespaces in test/mpi/c-mpi/rget_accumulate.c

--- test/mpi/c-mpi/rget_accumulate.c
+++ test/mpi/c-mpi/rget_accumulate.c
@@ -14,7 +14,7 @@ int main(int argc, char** argv) {
    // Get rank of process
    int my_rank;
    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-   
+
    // require at least two processes
    if (comm_size < 2) {
       printf("At least two ranks are required\n");
@@ -68,7 +68,7 @@ int main(int argc, char** argv) {
          for (int i=0; i<nints; i++) {originbuffer[i]+=resultbuffer[i];}
       }
    }
-   
+
    MPI_Win_fence(0, window);
    MPI_Win_free(&window);
 
