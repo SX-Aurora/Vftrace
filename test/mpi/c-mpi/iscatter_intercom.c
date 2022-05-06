07535a00a1cb96b6912a2501b6894b11008e9f0f
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:52 +0200

    removed trailing whitespaces in test/mpi/c-mpi/iscatter_intercom.c

--- test/mpi/c-mpi/iscatter_intercom.c
+++ test/mpi/c-mpi/iscatter_intercom.c
@@ -41,7 +41,7 @@ int main(int argc, char** argv) {
    MPI_Comm int_comm;
    int local_leader = 0;
    int remote_leader = (1-color)*(comm_size+1)/2;
-   MPI_Intercomm_create(sub_comm, 
+   MPI_Intercomm_create(sub_comm,
                         local_leader,
                         MPI_COMM_WORLD,
                         remote_leader, 1,
@@ -52,7 +52,7 @@ int main(int argc, char** argv) {
    int rootrank = 0;
    // allocating send/recv buffer
    int nints = atoi(argv[1]);
-   
+
    int *rbuffer = (int*) malloc(nints*sizeof(int));
    for (int i=0; i<nints; i++) {rbuffer[i]=-1;}
    int *sbuffer = NULL;
