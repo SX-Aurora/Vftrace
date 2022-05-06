c2a5921819281f133eb5a80d50ffae6bdb4ccb3f
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:47 +0200

    removed trailing whitespaces in src/mpi/vftr_mpi/rput.c

--- src/mpi/vftr_mpi/rput.c
+++ src/mpi/vftr_mpi/rput.c
@@ -24,7 +24,7 @@
 int vftr_MPI_Rput(const void *origin_addr, int origin_count,
                   MPI_Datatype origin_datatype, int target_rank,
                   MPI_Aint target_disp, int target_count,
-                  MPI_Datatype target_datatype, MPI_Win win, 
+                  MPI_Datatype target_datatype, MPI_Win win,
                   MPI_Request *request) {
    long long tstart = vftr_get_runtime_usec();
    int retVal = PMPI_Rput(origin_addr, origin_count, origin_datatype,
