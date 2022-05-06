3e6177e961b2919d86aca3f278448025a6e61759
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:44 +0200

    removed trailing whitespaces in src/mpi/f2vftr/startall_f2vftr.c

--- src/mpi/f2vftr/startall_f2vftr.c
+++ src/mpi/f2vftr/startall_f2vftr.c
@@ -22,7 +22,7 @@
 #include <stdlib.h>
 
 #include "startall.h"
-  
+
 void vftr_MPI_Startall_f2vftr(MPI_Fint *f_count, MPI_Fint *f_array_of_requests,
                               MPI_Fint *f_error) {
 
