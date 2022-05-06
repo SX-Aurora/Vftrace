371d45ba388a96b6bbd192bef6991b610e75f4dc
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:48 +0200

    removed trailing whitespaces in src/mpi/vftr_mpi/waitsome.c

--- src/mpi/vftr_mpi/waitsome.c
+++ src/mpi/vftr_mpi/waitsome.c
@@ -23,7 +23,7 @@
 
 #include "request_utils.h"
 #include "requests.h"
-  
+
 int vftr_MPI_Waitsome(int incount, MPI_Request array_of_requests[],
                       int *outcount, int array_of_indices[],
                       MPI_Status array_of_statuses[]) {
