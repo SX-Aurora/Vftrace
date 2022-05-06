ca97e21323175974567e5c6240cf11c1301ba92b
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:47 +0200

    removed trailing whitespaces in src/mpi/vftr_mpi/test.c

--- src/mpi/vftr_mpi/test.c
+++ src/mpi/vftr_mpi/test.c
@@ -19,23 +19,23 @@
 #include <mpi.h>
 
 #include "requests.h"
-  
+
 int vftr_MPI_Test(MPI_Request *request, int *flag, MPI_Status *status) {
    // do not call MPI_Test immediately!
-   // If the communication is successfull MPI_Test destroys the Reqeust 
+   // If the communication is successfull MPI_Test destroys the Reqeust
    // Hence, no chance of properly clearing the communication
    // from the open request list
-   // MPI_Request_get_status is a non destructive check. 
+   // MPI_Request_get_status is a non destructive check.
    int retVal = PMPI_Request_get_status(*request, flag, status);
-   
+
    if (*flag) {
       // Communication is done.
       // Clear finished communications from the open request list
       vftr_clear_completed_requests();
       // Now that the danger of deleating needed requests is banned
-      // actually call MPI_Test   
+      // actually call MPI_Test
       retVal = PMPI_Test(request, flag, status);
    }
-   
+
    return retVal;
 }
