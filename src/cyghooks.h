5cebe389a57258aac1a2bb3a16aa92aef4e5d8df
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-06 10:13:41 +0200

    removed trailing whitespaces in src/cyghooks.h

--- src/cyghooks.h
+++ src/cyghooks.h
@@ -19,7 +19,7 @@
 #ifndef CYGHOOKS_H
 #define CYGHOOKS_H
 
-// Define functions to redirect the function hooks, so to not make the 
+// Define functions to redirect the function hooks, so to not make the
 // function pointers globaly visible
 void vftr_set_enter_func_hook(void (*function_ptr)(void*,void*));
 void vftr_set_exit_func_hook(void (*function_ptr)(void*,void*));
