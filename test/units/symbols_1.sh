8bae8b4b0d57dd86fd9026492742bf4448e5cdfe
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-09 09:31:12 +0200

    removed unused call argument

--- test/units/symbols_1.sh
+++ test/units/symbols_1.sh
@@ -3,7 +3,7 @@ set -x
 test_name=symbols_1
 
 if [ "x$HAS_MPI" == "xYES" ]; then
-   ${MPI_EXEC} ${MPI_OPTS} ${NP} 1 ./${test_name} ${input_file} || exit 1
+   ${MPI_EXEC} ${MPI_OPTS} ${NP} 1 ./${test_name} || exit 1
 else
-   ./${test_name} ${input_file}
+   ./${test_name}
 fi
