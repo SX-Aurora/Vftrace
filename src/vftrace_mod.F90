eec3861034bfd2dfd4e502e34880fe9cf2b170c6
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-10 09:51:03 +0200

    enabled fpause and fget_stack

--- src/vftrace_mod.F90
+++ src/vftrace_mod.F90
@@ -27,49 +27,47 @@
 module vftrace
 
    implicit none
-   
+
    private
 
    public :: vftrace_region_begin, &
              vftrace_region_end
 !   public :: vftrace_allocate, &
 !             vftrace_deallocate
-!   public :: vftrace_get_stack
-!   public :: vftrace_pause, &
-!             vftrace_resume
-!
+   public :: vftrace_get_stack
+   public :: vftrace_pause, &
+             vftrace_resume
+
 !   public :: vftrace_show_callstack
 !   public :: vftrace_get_stacktree_size
-!
+
+   interface
+      ! pause and resume sampling via vftrace in user code
+      subroutine vftrace_pause() &
+         bind(c, name="vftrace_pause")
+         implicit none
+      end subroutine
+
+      subroutine vftrace_resume() &
+         bind(c, name="vftrace_resume")
+         implicit none
+      end subroutine
+   end interface
+
 !   interface
-!
-!      ! pause and resume sampling via vftrace in user code
-!      subroutine vftrace_pause() &
-!         bind(c, name="vftrace_pause")
-!         implicit none
-!      end subroutine
-! 
-!      subroutine vftrace_resume() &
-!         bind(c, name="vftrace_resume")
-!         implicit none
-!      end subroutine
-!
 !      subroutine vftrace_show_callstack () &
 !         bind(c, name="vftrace_show_callstack")
 !         implicit none
-!      end subroutine 
-!
+!      end subroutine
 !   end interface
-!
-!   interface 
-!
+
+!   interface
 !      function vftrace_get_stacktree_size() &
 !         bind(c, name="vftrace_get_stacktree_size")
 !         use iso_c_binding, only: c_int
 !         implicit none
 !         integer(kind=c_int) :: vftrace_get_stacktree_size
 !      end function vftrace_get_stacktree_size
-!
 !   end interface
 
    ! interface for non public regions routines
@@ -82,7 +80,7 @@ module vftrace
          implicit none
          character(c_char), intent(in) :: name(*)
       end subroutine vftrace_region_begin_C
- 
+
       ! Mark end of instrumented region
       ! name ist the Region name to be used in the profile
       subroutine vftrace_region_end_C(name) &
@@ -100,7 +98,7 @@ module vftrace
 !         integer(c_int), intent(in) :: n_elements
 !         integer(c_int), intent(in) :: element_size
 !      end subroutine vftrace_allocate_C
-!  
+!
 !      pure subroutine vftrace_deallocate_C (name) bind(c, name="vftrace_deallocate")
 !         use iso_c_binding, only: c_char
 !         implicit none
@@ -109,24 +107,24 @@ module vftrace
 
    end interface
 
-!   ! interface for non public stack routines
-!   ! used for passing string pointer between fortran and C
-!   interface
-!      function vftrace_get_stack_C_charptr() &
-!         bind(C, name="vftrace_get_stack")
-!         use iso_c_binding, only : c_ptr
-!         implicit none
-!         type(c_ptr) :: vftrace_get_stack_C_charptr
-!      end function vftrace_get_stack_C_charptr
-!
-!      function vftrace_get_stack_string_length_C_int() &
-!         bind(C, name="vftrace_get_stack_string_length")
-!         use iso_c_binding, only : c_int
-!         implicit none
-!         integer(kind=c_int) :: vftrace_get_stack_string_length_C_int
-!      end function vftrace_get_stack_string_length_C_int
-!
-!   end interface
+   ! interface for non public stack routines
+   ! used for passing string pointer between fortran and C
+   interface
+      function vftrace_get_stack_C_charptr() &
+         bind(C, name="vftrace_get_stack")
+         use iso_c_binding, only : c_ptr
+         implicit none
+         type(c_ptr) :: vftrace_get_stack_C_charptr
+      end function vftrace_get_stack_C_charptr
+
+      function vftrace_get_stack_string_length_C_int() &
+         bind(C, name="vftrace_get_stack_string_length")
+         use iso_c_binding, only : c_int
+         implicit none
+         integer(kind=c_int) :: vftrace_get_stack_string_length_C_int
+      end function vftrace_get_stack_string_length_C_int
+
+   end interface
 
 contains
 
@@ -137,7 +135,7 @@ contains
       integer :: name_len
       character(kind=c_char,len=:), allocatable :: c_name
       name_len = len(adjustl(trim(name)))
-      ! null terminator space 
+      ! null terminator space
       name_len = name_len + 1
       allocate(character(len=name_len) :: c_name)
       c_name(:) = adjustl(trim(name))
@@ -153,7 +151,7 @@ contains
       integer :: name_len
       character(kind=c_char,len=:), allocatable :: c_name
       name_len = len(adjustl(trim(name)))
-      ! null terminator space 
+      ! null terminator space
       name_len = name_len + 1
       allocate(character(len=name_len) :: c_name)
       c_name(:) = adjustl(trim(name))
@@ -162,26 +160,26 @@ contains
       deallocate(c_name)
    end subroutine vftrace_region_end
 
-!   function vftrace_get_stack()
-!      use iso_c_binding, only : c_int, &
-!                                c_f_pointer
-!      implicit none
-!
-!      integer(kind=c_int) :: stringlength
-!      character(len=:), pointer :: vftrace_get_stack
-!      character, pointer, dimension(:) :: tmpstring
-!
-!      integer :: i
-!
-!      stringlength = vftrace_get_stack_string_length_C_int()
-!      CALL C_F_POINTER(vftrace_get_stack_C_charptr(), tmpstring, [stringlength])
-!
-!      ALLOCATE(character(len=stringlength) :: vftrace_get_stack)
-!      do i = 1, stringlength
-!         vftrace_get_stack(i:i) = tmpstring(i)
-!      end do
-!   end function vftrace_get_stack
-!
+   function vftrace_get_stack()
+      use iso_c_binding, only : c_int, &
+                                c_f_pointer
+      implicit none
+
+      integer(kind=c_int) :: stringlength
+      character(len=:), pointer :: vftrace_get_stack
+      character, pointer, dimension(:) :: tmpstring
+
+      integer :: i
+
+      stringlength = vftrace_get_stack_string_length_C_int()
+      CALL C_F_POINTER(vftrace_get_stack_C_charptr(), tmpstring, [stringlength])
+
+      ALLOCATE(character(len=stringlength) :: vftrace_get_stack)
+      do i = 1, stringlength
+         vftrace_get_stack(i:i) = tmpstring(i)
+      end do
+   end function vftrace_get_stack
+
 !   pure subroutine vftrace_allocate (name, n_elements, element_size)
 !     use iso_c_binding, only: c_char, c_null_char, c_int
 !     use iso_fortran_env, only: int64
