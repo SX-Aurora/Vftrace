459c2f3b86f82690f4f9b6aa32ddd660d0f9a10c
Felix Uhl <uhl_felix@web.de>
2022-05-23 20:45:22 +0200

    made GET_REGION_ADDRESS macro more robust

--- src/region_address.h
+++ src/region_address.h
@@ -4,9 +4,15 @@
 // Getting the region address is defined here as a macro,
 // so it wont mess up the adresses by changing the function stack
 #ifdef __ve__
-#define GET_REGION_ADDRESS(ADDR) asm volatile ("or %0,0,%%lr" : "=r" (ADDR))
+#define GET_REGION_ADDRESS(ADDR) \
+   do { \
+      asm volatile ("or %0,0,%%lr" : "=r" (ADDR)); \
+   } while (0)
 #else
-#define GET_REGION_ADDRESS(ADDR) asm volatile ("mov 8(%%rbp), %0" : "=r" (ADDR))
+#define GET_REGION_ADDRESS(ADDR) \
+   do { \
+      asm volatile ("mov 8(%%rbp), %0" : "=r" (ADDR)); \
+   } while (0)
 #endif
 
 #endif
