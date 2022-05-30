8df8186c8bc0ed572e45d718d0f66bf48e066f56
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-05-30 12:57:45 +0200

    Use return address of a region instead of frame pointer

--- src/region_address.h
+++ src/region_address.h
@@ -3,16 +3,9 @@
 
 // Getting the region address is defined here as a macro,
 // so it wont mess up the adresses by changing the function stack
-#ifdef __ve__
 #define GET_REGION_ADDRESS(ADDR) \
    do { \
-      asm volatile ("or %0,0,%%lr" : "=r" (ADDR)); \
+      (ADDR) = (__builtin_return_address(0)); \
    } while (0)
-#else
-#define GET_REGION_ADDRESS(ADDR) \
-   do { \
-      asm volatile ("mov 8(%%rbp), %0" : "=r" (ADDR)); \
-   } while (0)
-#endif
 
 #endif
