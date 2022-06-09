84f92dad917ec8386f747db4cba7994913510ebf
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-06-09 10:42:29 +0200

    activated overhead measurement in ibcast.c

--- src/mpi/vftr_mpi/ibcast.c
+++ src/mpi/vftr_mpi/ibcast.c
@@ -20,6 +20,15 @@
 
 #include <stdlib.h>
 
+#include "thread_types.h"
+#include "threads.h"
+#include "threadstack_types.h"
+#include "threadstacks.h"
+#include "stack_types.h"
+#include "stacks.h"
+#include "profiling_types.h"
+#include "profiling.h"
+#include "overheadprofiling.h"
 #include "timer.h"
 #include "collective_requests.h"
 #include "rank_translate.h"
@@ -61,9 +70,14 @@ int vftr_MPI_Ibcast(void *buffer, int count, MPI_Datatype datatype,
       vftr_register_collective_request(recv, 1, &count, &datatype, &root,
                                        comm, *request, 0, NULL, tstart);
    }
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
@@ -114,9 +128,14 @@ int vftr_MPI_Ibcast_intercom(void *buffer, int count, MPI_Datatype datatype,
       vftr_register_collective_request(recv, 1, &count, &datatype, &root,
                                        comm, *request, 0, NULL, tstart);
    }
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
