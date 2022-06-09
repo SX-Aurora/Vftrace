1d1b08f4bc2646ff6ae1ff6a7d2ade7ce2548f8f
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-06-09 10:42:33 +0200

    activated overhead measurement in request_free.c

--- src/mpi/vftr_mpi/request_free.c
+++ src/mpi/vftr_mpi/request_free.c
@@ -20,6 +20,15 @@
 
 #include <stdbool.h>
 
+#include "thread_types.h"
+#include "threads.h"
+#include "threadstack_types.h"
+#include "threadstacks.h"
+#include "stack_types.h"
+#include "stacks.h"
+#include "profiling_types.h"
+#include "profiling.h"
+#include "overheadprofiling.h"
 #include "timer.h"
 #include "requests.h"
 #include "p2p_requests.h"
@@ -35,9 +44,14 @@ int vftr_MPI_Request_free(MPI_Request *request) {
       PMPI_Request_free(request);
    }
    *request = MPI_REQUEST_NULL;
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return 0;
 }
