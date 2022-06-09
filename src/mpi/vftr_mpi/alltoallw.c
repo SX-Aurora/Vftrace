0e2ad69c29c1688f782c1089c27f309c38c8591e
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-06-09 10:42:27 +0200

    activated overhead measurement in alltoallw.c

--- src/mpi/vftr_mpi/alltoallw.c
+++ src/mpi/vftr_mpi/alltoallw.c
@@ -19,6 +19,15 @@
 #include <mpi.h>
 
 #include "rank_translate.h"
+#include "thread_types.h"
+#include "threads.h"
+#include "threadstack_types.h"
+#include "threadstacks.h"
+#include "stack_types.h"
+#include "stacks.h"
+#include "profiling_types.h"
+#include "profiling.h"
+#include "overheadprofiling.h"
 #include "timer.h"
 #include "sync_messages.h"
 
@@ -41,9 +50,14 @@ int vftr_MPI_Alltoallw(const void *sendbuf, const int *sendcounts,
       vftr_store_sync_message_info(recv, recvcounts[i], recvtypes[i],
                                    i, 0, comm, tstart, tend);
    }
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
@@ -78,9 +92,14 @@ int vftr_MPI_Alltoallw_inplace(const void *sendbuf, const int *sendcounts,
       vftr_store_sync_message_info(recv, recvcounts[i], recvtypes[i],
                                    i, 0, comm, tstart, tend);
    }
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
@@ -113,9 +132,14 @@ int vftr_MPI_Alltoallw_intercom(const void *sendbuf, const int *sendcounts,
                                    global_peer_rank, -1, MPI_COMM_WORLD,
                                    tstart, tend);
    }
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
