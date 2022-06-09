eae3803f09e457daf112f64174e9145fbdf6b572
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-06-09 10:42:33 +0200

    activated overhead measurement in recv_init.c

--- src/mpi/vftr_mpi/recv_init.c
+++ src/mpi/vftr_mpi/recv_init.c
@@ -18,6 +18,15 @@
 
 #include <mpi.h>
 
+#include "thread_types.h"
+#include "threads.h"
+#include "threadstack_types.h"
+#include "threadstacks.h"
+#include "stack_types.h"
+#include "stacks.h"
+#include "profiling_types.h"
+#include "profiling.h"
+#include "overheadprofiling.h"
 #include "timer.h"
 #include "p2p_requests.h"
 
@@ -27,9 +36,14 @@ int vftr_MPI_Recv_init(void *buf, int count, MPI_Datatype datatype,
 
    long long t2start = vftr_get_runtime_usec();
    vftr_register_pers_p2p_request(recv, count, datatype, source, tag, comm, *request);
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
