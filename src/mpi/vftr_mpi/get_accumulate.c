b25fb1bd823842b8ddfa412e1ada3364b8ec8a66
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-06-09 10:42:28 +0200

    activated overhead measurement in get_accumulate.c

--- src/mpi/vftr_mpi/get_accumulate.c
+++ src/mpi/vftr_mpi/get_accumulate.c
@@ -18,6 +18,15 @@
 
 #include <mpi.h>
 
+#include "thread_types.h"
+#include "threads.h"
+#include "threadstack_types.h"
+#include "threadstacks.h"
+#include "stack_types.h"
+#include "stacks.h"
+#include "profiling_types.h"
+#include "profiling.h"
+#include "overheadprofiling.h"
 #include "timer.h"
 #include "sync_messages.h"
 
@@ -53,9 +62,14 @@ int vftr_MPI_Get_accumulate(const void *origin_addr, int origin_count,
    vftr_store_sync_message_info(send, origin_count, origin_datatype,
                                 global_rank, -1, MPI_COMM_WORLD, tstart, tend);
 
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
