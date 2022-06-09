573577b23da24daa1b0350ba19d09513b5642e11
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-06-09 10:42:28 +0200

    activated overhead measurement in iallgather.c

--- src/mpi/vftr_mpi/iallgather.c
+++ src/mpi/vftr_mpi/iallgather.c
@@ -21,6 +21,15 @@
 #include <stdlib.h>
 
 #include "rank_translate.h"
+#include "thread_types.h"
+#include "threads.h"
+#include "threadstack_types.h"
+#include "threadstacks.h"
+#include "stack_types.h"
+#include "stacks.h"
+#include "profiling_types.h"
+#include "profiling.h"
+#include "overheadprofiling.h"
 #include "timer.h"
 #include "collective_requests.h"
 
@@ -63,9 +72,14 @@ int vftr_MPI_Iallgather(const void *sendbuf, int sendcount,
    tmptype = NULL;
    free(peer_ranks);
    peer_ranks = NULL;
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
@@ -120,9 +134,14 @@ int vftr_MPI_Iallgather_inplace(const void *sendbuf, int sendcount,
       free(peer_ranks);
       peer_ranks = NULL;
    }
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
@@ -175,9 +194,14 @@ int vftr_MPI_Iallgather_intercom(const void *sendbuf, int sendcount,
    tmptype = NULL;
    free(peer_ranks);
    peer_ranks = NULL;
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
