2a738b8aa8249e55951dd6a969dcd0129ffe2577
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-06-09 10:42:32 +0200

    activated overhead measurement in iscatterv.c

--- src/mpi/vftr_mpi/iscatterv.c
+++ src/mpi/vftr_mpi/iscatterv.c
@@ -21,6 +21,15 @@
 #include <stdlib.h>
 
 #include "rank_translate.h"
+#include "thread_types.h"
+#include "threads.h"
+#include "threadstack_types.h"
+#include "threadstacks.h"
+#include "stack_types.h"
+#include "stacks.h"
+#include "profiling_types.h"
+#include "profiling.h"
+#include "overheadprofiling.h"
 #include "timer.h"
 #include "collective_requests.h"
 
@@ -78,9 +87,14 @@ int vftr_MPI_Iscatterv(const void *sendbuf, const int *sendcounts,
       vftr_register_collective_request(recv, 1, &recvcount, &recvtype, &root,
                                        comm, *request, 0, NULL, tstart);
    }
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
@@ -147,9 +161,14 @@ int vftr_MPI_Iscatterv_inplace(const void *sendbuf, const int *sendcounts,
       vftr_register_collective_request(recv, 1, &recvcount, &recvtype, &root,
                                        comm, *request, 0, NULL, tstart);
    }
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
@@ -210,9 +229,14 @@ int vftr_MPI_Iscatterv_intercom(const void *sendbuf, const int *sendcounts,
       vftr_register_collective_request(recv, 1, &recvcount, &recvtype, &root,
                                        comm, *request, 0, NULL, tstart);
    }
+
+   thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
+   threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
+   stack_t *my_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
+   profile_t *my_profile = vftr_get_my_profile(my_stack, my_thread);
    long long t2end = vftr_get_runtime_usec();
 
-   //TODO: vftr_mpi_overhead_usec += t2end - t2start;
+   vftr_accumulate_mpi_overheadprofiling(&(my_profile->overheadProf), t2end-t2start);
 
    return retVal;
 }
