89433b441cebda36e8d44f0dac28c39212e6a3bb
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-06-30 14:10:11 +0200

    extended mpi-profiling type

--- src/mpi/utils/mpiprofiling.c
+++ src/mpi/utils/mpiprofiling.c
@@ -3,8 +3,13 @@
 
 mpiProfile_t vftr_new_mpiprofiling() {
    mpiProfile_t prof;
+   prof.nmessages = 0ll;
    prof.send_bytes = 0ll;
    prof.recv_bytes = 0ll;
+   prof.acc_send_bw = 0.0;
+   prof.acc_recv_bw = 0.0;
+   prof.total_time_usec = 0ll;
+
    return prof;
 }
 
@@ -19,11 +24,19 @@ void vftr_accumulate_message_info(mpiProfile_t *prof_ptr,
    (void) tstart;
    (void) tend;
 
+   int nbytes = count * type_size;
+   long long time = tend - tstart;
+   double bw = nbytes * 1.0e-6 / time;
+
+   prof_ptr->nmessages++;
    if (dir == send) {
-      prof_ptr->send_bytes += count * type_size;
+      prof_ptr->send_bytes += nbytes;
+      prof_ptr->acc_send_bw += bw;
    } else {
-      prof_ptr->recv_bytes += count * type_size;
+      prof_ptr->recv_bytes += nbytes;
+      prof_ptr->acc_recv_bw += bw;
    }
+   prof_ptr->total_time_usec += time;
 }
 
 void vftr_mpiprofiling_free(mpiProfile_t *prof_ptr) {
--- src/mpi/utils/mpiprofiling_types.h
+++ src/mpi/utils/mpiprofiling_types.h
@@ -2,8 +2,15 @@
 #define MPIPROFILING_TYPES_H
 
 typedef struct {
+   // number of messages
+   long long nmessages;
    // amount of bytes send/recv by this stack
    long long send_bytes;
    long long recv_bytes;
+   // accumulated bandwidth
+   double acc_send_bw;
+   double acc_recv_bw;
+   // accumulated communication time
+   long long total_time_usec;
 } mpiProfile_t;
 #endif
