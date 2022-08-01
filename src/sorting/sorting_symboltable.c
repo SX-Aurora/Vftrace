39dd969f9205739870b556b76f3b84f1b6884d0e
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-08-01 10:57:57 +0200

    replaced symbol radixsort by quicksort

--- src/sorting/sorting_symboltable.c
+++ src/sorting/sorting_symboltable.c
@@ -3,30 +3,20 @@
 #include "custom_types.h"
 #include "symbols.h"
 
-// sort the symboltable with a linear scaling sort
+// sort the symboltable with a quick-sort
 void vftr_sort_symboltable(unsigned int nsymb, symbol_t *symbols) {
-   // create the fuckets for sorting
-   // use one array to store both buckets
-   symbol_t *buckets = (symbol_t*) malloc(2*nsymb*sizeof(symbol_t));
-   // loop over the bits of the symbols address which is (void*)
-   int nbits = 8*sizeof(uintptr_t);
-   for (int ibit=0; ibit<nbits; ibit++) {
-      int idx[2] = {0,0};
-      // sort the symbols into buckets base on thei ibit-th bit
-      for (unsigned int i=0; i<nsymb; i++) {
-         // extract the ibit-th bit of the i-th symbols address
-         int bit = (symbols[i].addr >> ibit) & 1llu;
-         // add the symbol to the selected bucket
-         buckets[bit*nsymb+idx[bit]] = symbols[i];
-         idx[bit]++;
-      }
-      // copy the presorted symbols back to the original list
-      for (int i=0; i<idx[0]; i++) {
-         symbols[i] = buckets[0*nsymb+i];
-      }
-      for (int i=0; i<idx[1]; i++) {
-         symbols[idx[0]+i] = buckets[1*nsymb+i];
-      }
+   if (nsymb < 2) return;
+   uintptr_t pivot = symbols[nsymb/2].addr;
+   int left, right;
+   for (left=0, right=nsymb-1; ; left++, right--) {
+      while (symbols[left].addr < pivot) left++;
+      while (symbols[right].addr > pivot) right--;
+      if (left >= right) break;
+      symbol_t tmp = symbols[left];
+      symbols[left] = symbols[right];
+      symbols[right] = tmp;
    }
-   free(buckets);
+
+   vftr_sort_symboltable(left, symbols);
+   vftr_sort_symboltable(nsymb-left, symbols+left);
 }
