918313d0aaeae9389f4c681776675dda7f73f0d1
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-08-15 08:08:19 +0200

    added writing of global logfile

---
+++ src/logfile_header.c
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "size_types.h"
+#include "timer_types.h"
+#include "table_types.h"
+#include "environment_types.h"
+#include "vftrace_state.h"
+
+#include "filenames.h"
+#include "license.h"
+#include "config.h"
+#include "environment.h"
+#include "stacks.h"
+#include "collate_stacks.h"
+#include "profiling_types.h"
+#include "profiling.h"
+#include "callprofiling.h"
+#include "mpiprofiling.h"
+#include "tables.h"
+#include "misc_utils.h"
+
+void vftr_write_logfile_header(FILE *fp, time_strings_t timestrings) {
+   fprintf(fp, "%s\n", PACKAGE_STRING);
+   fprintf(fp, "Runtime profile for application:\n");
+   fprintf(fp, "Start Date: %s\n", timestrings.start_time);
+   fprintf(fp, "End Date:   %s\n\n", timestrings.end_time);
+   vftr_print_licence(fp);
+}
+
+void vftr_write_logfile_summary(FILE *fp, process_t process,
+                                    vftr_size_t vftrace_size,
+                                    long long runtime) {
+   double runtime_sec = runtime * 1.0e-6;
+
+   // The application runtime is the runtime minus the
+   long long total_master_overhead = 0ll;
+   long long call_overhead =
+      vftr_get_total_collated_call_overhead(process.collated_stacktree);
+#ifdef _MPI
+   long long mpi_overhead =
+      vftr_get_total_collated_mpi_overhead(process.collated_stacktree);
+#endif
+#ifdef _OMP
+   long long omp_overhead =
+      vftr_get_total_collated_omp_overhead(process.collated_stacktree);
+#endif
+      total_master_overhead += call_overhead;
+#ifdef _MPI
+      total_master_overhead += mpi_overhead;
+#endif
+#ifdef _OMP
+      total_master_overhead += omp_overhead;
+#endif
+   double total_master_overhead_sec = total_master_overhead*1.0e-6;
+   double apptime_sec = runtime_sec - total_master_overhead_sec;
+
+   fprintf(fp, "\n");
+#ifdef _MPI
+   fprintf(fp, "Nr. of MPI ranks:     %8d\n", process.nprocesses);
+#endif
+   fprintf(fp, "Total runtime:        %8.2lf s\n", runtime_sec);
+   fprintf(fp, "Application time:     %8.2lf s\n", apptime_sec);
+   fprintf(fp, "Overhead:             %8.2lf s\n", total_master_overhead_sec);
+   fprintf(fp, "   Function hooks:    %8.2lf s\n", call_overhead*1.0e-6);
+#ifdef _MPI
+   fprintf(fp, "   MPI wrappers:      %8.2lf s\n", mpi_overhead*1.0e-6);
+#endif
+#ifdef _OMP
+   fprintf(fp, "   OMP callbacks:     %8.2lf s\n", omp_overhead*1.0e-6);
+#endif
+
+   char *unit = vftr_byte_unit(vftrace_size.total);
+   double vftrace_size_double = (double) vftrace_size.total;
+   while (vftrace_size_double > 1024.0) {vftrace_size_double /= 1024;}
+   fprintf(fp, "Vftrace used memory:   %7.2lf %s\n", vftrace_size_double, unit);
+   free(unit);
+}
---
+++ src/logfile_header.h
@@ -0,0 +1,17 @@
+#ifndef LOGFILE_HEADER_H
+#define LOGFILE_HEADER_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "size_types.h"
+#include "timer_types.h"
+#include "process_types.h"
+
+void vftr_write_logfile_header(FILE *fp, time_strings_t timestrings);
+
+void vftr_write_logfile_summary(FILE *fp, process_t process,
+                                vftr_size_t vftrace_size,
+                                long long runtime);
+
+#endif
