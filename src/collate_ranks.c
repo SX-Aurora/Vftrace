459d38ad60b77b86b45badcb593913712989882d
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-07 11:47:00 +0200

    added self profiling to collate_ranks.c

--- src/collate_ranks.c
+++ src/collate_ranks.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 
+#include "self_profile.h"
 #include "vftrace_state.h"
 #include "collate_stacks.h"
 #include "collate_profiles.h"
 #include "collate_vftr_size.h"
 
 void vftr_collate_ranks(vftrace_t *vftrace_ptr) {
+   SELF_PROFILE_START_FUNCTION;
    vftrace_ptr->process.collated_stacktree =
       vftr_collate_stacks(&(vftrace_ptr->process.stacktree));
 
@@ -14,4 +16,5 @@ void vftr_collate_ranks(vftrace_t *vftrace_ptr) {
 
 
    vftr_collate_vftr_size(vftrace_ptr);
+   SELF_PROFILE_END_FUNCTION;
 }
