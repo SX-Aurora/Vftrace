e2bba2ebcb044432b0ece94636e15bb55211e8ef
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-07 11:47:02 +0200

    added self profiling to user_regions.c

--- src/user_regions.c
+++ src/user_regions.c
@@ -3,6 +3,7 @@
 
 #include <string.h>
 
+#include "self_profile.h"
 #include "stack_types.h"
 #include "thread_types.h"
 #include "threadstack_types.h"
@@ -18,6 +19,7 @@
 #include "region_address.h"
 
 void vftr_user_region_begin(const char *name, void *addr) {
+   SELF_PROFILE_START_FUNCTION;
    long long region_begin_time_begin = vftr_get_runtime_usec();
    // Get the thread that called the region
    thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
@@ -64,9 +66,11 @@ void vftr_user_region_begin(const char *name, void *addr) {
    // No calls after this overhead handling!
    vftr_accumulate_callprofiling_overhead(&(my_profile->callProf),
       vftr_get_runtime_usec() - region_begin_time_begin);
+   SELF_PROFILE_END_FUNCTION;
 }
 
 void vftr_user_region_end() {
+   SELF_PROFILE_START_FUNCTION;
    long long region_end_time_begin = vftr_get_runtime_usec();
 
    thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
@@ -99,20 +103,25 @@ void vftr_user_region_end() {
    vftr_accumulate_callprofiling_overhead(
       &(my_profile->callProf),
       vftr_get_runtime_usec() - region_end_time_begin);
+   SELF_PROFILE_END_FUNCTION;
 }
 
 //These regions are for users to be used only.
 void vftrace_region_begin(const char *name) {
+   SELF_PROFILE_START_FUNCTION;
    if (vftrace.state == on) {
       void *addr;
       GET_REGION_ADDRESS(addr);
       vftr_user_region_begin(name, addr);
    }
+   SELF_PROFILE_END_FUNCTION;
 }
 
 void vftrace_region_end(const char *name) {
+   SELF_PROFILE_START_FUNCTION;
    (void) name;
    if (vftrace.state == on) {
       vftr_user_region_end();
    }
+   SELF_PROFILE_END_FUNCTION;
 }
