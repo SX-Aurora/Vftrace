464c2ee09cf9f3f2b90a1461fcf311891b75858b
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-07 11:49:10 +0200

    added self profiling to sorting_stacks.c test

--- test/units/sorting_stacks.c
+++ test/units/sorting_stacks.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "self_profile.h"
 #include "environment_types.h"
 #include "environment.h"
 #include "symbol_types.h"
@@ -22,6 +23,7 @@
 #endif
 
 int main(int argc, char **argv) {
+   INIT_SELF_PROF_VFTRACE;
 #if defined(_MPI)
    PMPI_Init(&argc, &argv);
 #else
@@ -177,5 +179,6 @@ int main(int argc, char **argv) {
    PMPI_Finalize();
 #endif
 
+   FINALIZE_SELF_PROF_VFTRACE;
    return 0;
 }
