6dddc026aff4d4c015f015c252dab55360b94c84
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:23 +0200

    added self profiling to rget.c

--- src/mpi/vftr_mpi/rget.c
+++ src/mpi/vftr_mpi/rget.c
@@ -18,6 +18,7 @@
 
 #include <mpi.h>
 
+#include "self_profile.h"
 #include "thread_types.h"
 #include "threads.h"
 #include "threadstack_types.h"
@@ -40,6 +41,7 @@ int vftr_MPI_Rget(void *origin_addr, int origin_count,
                           target_rank, target_disp, target_count,
                           target_datatype, win, request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    // Need to figure out the partner rank in a known communicator to store info
    MPI_Group local_group;
@@ -66,5 +68,6 @@ int vftr_MPI_Rget(void *origin_addr, int origin_count,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
