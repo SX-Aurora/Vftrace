72d5362f63d75bee5319db2f0625aeb7dced9614
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:22 +0200

    added self profiling to iprobe.c

--- src/mpi/vftr_mpi/iprobe.c
+++ src/mpi/vftr_mpi/iprobe.c
@@ -18,8 +18,12 @@
 
 #include <mpi.h>
 
+#include "self_profile.h"
 int vftr_MPI_Iprobe(int source, int tag, MPI_Comm comm, int *flag, MPI_Status *status) {
+   SELF_PROFILE_START_FUNCTION;
    // there is no timing needed but we keep the wrapper here
    // for the instrumentation
-   return PMPI_Iprobe(source, tag, comm, flag, status);
+   int retVal = PMPI_Iprobe(source, tag, comm, flag, status);
+   SELF_PROFILE_END_FUNCTION;
+   return retVal;
 }
