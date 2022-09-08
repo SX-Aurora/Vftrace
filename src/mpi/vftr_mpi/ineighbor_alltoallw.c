e17f19d99e23c940802a2baf1cd01a9bab118547
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:22 +0200

    added self profiling to ineighbor_alltoallw.c

--- src/mpi/vftr_mpi/ineighbor_alltoallw.c
+++ src/mpi/vftr_mpi/ineighbor_alltoallw.c
@@ -19,6 +19,7 @@
 
 #include <mpi.h>
 
+#include "self_profile.h"
 #include "rank_translate.h"
 #include "thread_types.h"
 #include "threads.h"
@@ -43,6 +44,7 @@ int vftr_MPI_Ineighbor_alltoallw_graph(const void *sendbuf, const int *sendcount
                                          recvbuf, recvcounts, rdispls, recvtypes,
                                          comm, request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    int rank;
    PMPI_Comm_rank(comm, &rank);
@@ -94,6 +96,7 @@ int vftr_MPI_Ineighbor_alltoallw_graph(const void *sendbuf, const int *sendcount
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
 
@@ -107,6 +110,7 @@ int vftr_MPI_Ineighbor_alltoallw_cart(const void *sendbuf, const int *sendcounts
                                          recvbuf, recvcounts, rdispls, recvtypes,
                                          comm, request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    int rank;
    PMPI_Comm_rank(comm, &rank);
@@ -156,6 +160,7 @@ int vftr_MPI_Ineighbor_alltoallw_cart(const void *sendbuf, const int *sendcounts
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
 
@@ -169,6 +174,7 @@ int vftr_MPI_Ineighbor_alltoallw_dist_graph(const void *sendbuf, const int *send
                                          recvbuf, recvcounts, rdispls, recvtypes,
                                          comm, request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    // first obtain the distributed graph info for this process
    int ninneighbors;
@@ -236,5 +242,6 @@ int vftr_MPI_Ineighbor_alltoallw_dist_graph(const void *sendbuf, const int *send
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
