4d4e6c2c645ec8ec9fd0915fa9ecedc86c5dd5f9
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:22 +0200

    added self profiling to init.c

--- src/mpi/vftr_mpi/init.c
+++ src/mpi/vftr_mpi/init.c
@@ -18,12 +18,13 @@
 
 #include <mpi.h>
 
+#include "self_profile.h"
 #include "vftrace_state.h"
 #include "mpiprofiling.h"
 
 int vftr_MPI_Init(int *argc, char ***argv) {
-
-   int returnValue = PMPI_Init(argc, argv);
+   SELF_PROFILE_START_FUNCTION;
+   int retVal = PMPI_Init(argc, argv);
 
    PMPI_Comm_size(MPI_COMM_WORLD, &vftrace.process.nprocesses);
    PMPI_Comm_rank(MPI_COMM_WORLD, &vftrace.process.processID);
@@ -31,6 +32,6 @@ int vftr_MPI_Init(int *argc, char ***argv) {
    vftr_create_profiled_ranks_list(vftrace.environment,
                                    vftrace.process,
                                    &vftrace.mpi_state);
-
-   return returnValue;
+   SELF_PROFILE_END_FUNCTION;
+   return retVal;
 }
