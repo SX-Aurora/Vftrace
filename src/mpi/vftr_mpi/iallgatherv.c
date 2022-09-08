3fdbaa897e68be7de4e47340c2c77e92a001889c
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:21 +0200

    added self profiling to iallgatherv.c

--- src/mpi/vftr_mpi/iallgatherv.c
+++ src/mpi/vftr_mpi/iallgatherv.c
@@ -20,6 +20,7 @@
 
 #include <stdlib.h>
 
+#include "self_profile.h"
 #include "rank_translate.h"
 #include "thread_types.h"
 #include "threads.h"
@@ -43,6 +44,7 @@ int vftr_MPI_Iallgatherv(const void *sendbuf, int sendcount,
                                  recvcounts, displs, recvtype, comm,
                                  request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    int size;
    PMPI_Comm_size(comm, &size);
@@ -88,6 +90,7 @@ int vftr_MPI_Iallgatherv(const void *sendbuf, int sendcount,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
 
@@ -101,6 +104,7 @@ int vftr_MPI_Iallgatherv_inplace(const void *sendbuf, int sendcount,
                                  recvcounts, displs, recvtype, comm,
                                  request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    int size;
    PMPI_Comm_size(comm, &size);
@@ -167,6 +171,7 @@ int vftr_MPI_Iallgatherv_inplace(const void *sendbuf, int sendcount,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
 
@@ -180,6 +185,7 @@ int vftr_MPI_Iallgatherv_intercom(const void *sendbuf, int sendcount,
                                  recvcounts, displs, recvtype, comm,
                                  request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    // Every process of group A sends sendcount data to and
    // receives recvcounts[i] data from the i-th process in group B
@@ -234,5 +240,6 @@ int vftr_MPI_Iallgatherv_intercom(const void *sendbuf, int sendcount,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
