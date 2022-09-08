ce02159649bb0a1cd53458c58202cf2b4906b546
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:21 +0200

    added self profiling to ialltoallw.c

--- src/mpi/vftr_mpi/ialltoallw.c
+++ src/mpi/vftr_mpi/ialltoallw.c
@@ -20,6 +20,7 @@
 
 #include <stdlib.h>
 
+#include "self_profile.h"
 #include "rank_translate.h"
 #include "thread_types.h"
 #include "threads.h"
@@ -43,6 +44,7 @@ int vftr_MPI_Ialltoallw(const void *sendbuf, const int *sendcounts,
                                 recvbuf, recvcounts, rdispls, recvtypes,
                                 comm, request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    int size;
    PMPI_Comm_size(comm, &size);
@@ -92,6 +94,7 @@ int vftr_MPI_Ialltoallw(const void *sendbuf, const int *sendcounts,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
 
@@ -105,6 +108,7 @@ int vftr_MPI_Ialltoallw_inplace(const void *sendbuf, const int *sendcounts,
                                 recvbuf, recvcounts, rdispls, recvtypes,
                                 comm, request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    int size;
    PMPI_Comm_size(comm, &size);
@@ -162,6 +166,7 @@ int vftr_MPI_Ialltoallw_inplace(const void *sendbuf, const int *sendcounts,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
 
@@ -175,6 +180,7 @@ int vftr_MPI_Ialltoallw_intercom(const void *sendbuf, const int *sendcounts,
                                 recvbuf, recvcounts, rdispls, recvtypes,
                                 comm, request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    // Every process of group A sends sendcounts[i] sendtypes[i] to
    // and receives recvcounts[i] recvtypes[i] from
@@ -237,5 +243,6 @@ int vftr_MPI_Ialltoallw_intercom(const void *sendbuf, const int *sendcounts,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
