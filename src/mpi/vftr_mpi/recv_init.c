71a1f6f6e73fe02cfbc6067e4ad35d86bba0c2d3
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:22 +0200

    added self profiling to recv_init.c

--- src/mpi/vftr_mpi/recv_init.c
+++ src/mpi/vftr_mpi/recv_init.c
@@ -18,6 +18,7 @@
 
 #include <mpi.h>
 
+#include "self_profile.h"
 #include "thread_types.h"
 #include "threads.h"
 #include "threadstack_types.h"
@@ -34,6 +35,7 @@ int vftr_MPI_Recv_init(void *buf, int count, MPI_Datatype datatype,
                        int source, int tag, MPI_Comm comm, MPI_Request *request) {
    int retVal = PMPI_Recv_init(buf, count, datatype, source, tag, comm, request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    vftr_register_pers_p2p_request(recv, count, datatype, source, tag, comm, *request);
 
@@ -45,5 +47,6 @@ int vftr_MPI_Recv_init(void *buf, int count, MPI_Datatype datatype,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
