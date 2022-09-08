51b6893a2a88bb4b86b9f4c7be46d222d51a2ec0
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:21 +0200

    added self profiling to iexscan.c

--- src/mpi/vftr_mpi/iexscan.c
+++ src/mpi/vftr_mpi/iexscan.c
@@ -18,6 +18,7 @@
 
 #include <mpi.h>
 
+#include "self_profile.h"
 #include "thread_types.h"
 #include "threads.h"
 #include "threadstack_types.h"
@@ -37,6 +38,7 @@ int vftr_MPI_Iexscan(const void *sendbuf, void *recvbuf, int count,
    long long tstart = vftr_get_runtime_usec();
    int retVal = PMPI_Iexscan(sendbuf, recvbuf, count, datatype, op, comm, request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    // Only intra-communicators, as the standard specifically states
    // that the scan operation is invalid for intercommunicators
@@ -113,5 +115,6 @@ int vftr_MPI_Iexscan(const void *sendbuf, void *recvbuf, int count,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
