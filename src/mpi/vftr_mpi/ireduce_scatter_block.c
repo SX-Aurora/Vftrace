ccc029023a3f7db20b79fac803c2c0529ce368e4
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:22 +0200

    added self profiling to ireduce_scatter_block.c

--- src/mpi/vftr_mpi/ireduce_scatter_block.c
+++ src/mpi/vftr_mpi/ireduce_scatter_block.c
@@ -20,6 +20,7 @@
 
 #include <mpi.h>
 
+#include "self_profile.h"
 #include "rank_translate.h"
 #include "thread_types.h"
 #include "threads.h"
@@ -40,6 +41,7 @@ int vftr_MPI_Ireduce_scatter_block(const void *sendbuf, void *recvbuf,
    long long tstart = vftr_get_runtime_usec();
    int retVal = PMPI_Ireduce_scatter_block(sendbuf, recvbuf, recvcount, datatype, op, comm, request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    int size;
    PMPI_Comm_size(comm, &size);
@@ -109,6 +111,7 @@ int vftr_MPI_Ireduce_scatter_block(const void *sendbuf, void *recvbuf,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
 
@@ -119,6 +122,7 @@ int vftr_MPI_Ireduce_scatter_block_inplace(const void *sendbuf, void *recvbuf,
    long long tstart = vftr_get_runtime_usec();
    int retVal = PMPI_Ireduce_scatter_block(sendbuf, recvbuf, recvcount, datatype, op, comm, request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    int size;
    PMPI_Comm_size(comm, &size);
@@ -186,6 +190,7 @@ int vftr_MPI_Ireduce_scatter_block_inplace(const void *sendbuf, void *recvbuf,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
 
@@ -196,6 +201,7 @@ int vftr_MPI_Ireduce_scatter_block_intercom(const void *sendbuf, void *recvbuf,
    long long tstart = vftr_get_runtime_usec();
    int retVal = PMPI_Ireduce_scatter_block(sendbuf, recvbuf, recvcount, datatype, op, comm, request);
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    // Every process of group A performs the reduction within the group A
    // and stores the result on everyp process of group B and vice versa
@@ -305,5 +311,6 @@ int vftr_MPI_Ireduce_scatter_block_intercom(const void *sendbuf, void *recvbuf,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
