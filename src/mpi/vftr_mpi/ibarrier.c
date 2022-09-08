01f5850953a0e90699878d004b18fa142fb6b61a
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:21 +0200

    added self profiling to ibarrier.c

--- src/mpi/vftr_mpi/ibarrier.c
+++ src/mpi/vftr_mpi/ibarrier.c
@@ -19,8 +19,13 @@
 
 #include <mpi.h>
 
+#include "self_profile.h"
+
 int vftr_MPI_Ibarrier(MPI_Comm comm, MPI_Request *request) {
+   SELF_PROFILE_START_FUNCTION;
    // there is no timing needed but we keep the wrapper here
    // for the instrumentation
-   return PMPI_Ibarrier(comm, request);
+   int retVal = PMPI_Ibarrier(comm, request);
+   SELF_PROFILE_END_FUNCTION;
+   return retVal;
 }
