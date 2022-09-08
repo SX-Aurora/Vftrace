ccc2e5e428e9fbe8232101a35d6f93de673fb901
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:20 +0200

    added self profiling to barrier.c

--- src/mpi/vftr_mpi/barrier.c
+++ src/mpi/vftr_mpi/barrier.c
@@ -19,8 +19,13 @@
 
 #include <mpi.h>
 
+#include "self_profile.h"
+
 int vftr_MPI_Barrier(MPI_Comm comm) {
+   SELF_PROFILE_START_FUNCTION;
    // there is no timing needed but we keep the wrapper here
    // for the instrumentation
-   return PMPI_Barrier(comm);
+   int retVal = PMPI_Barrier(comm);
+   SELF_PROFILE_END_FUNCTION;
+   return retVal;
 }
