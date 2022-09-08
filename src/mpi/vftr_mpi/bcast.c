35732f58ff1004b779eb1415e1176b238d4b0f7a
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:20 +0200

    added self profiling to bcast.c

--- src/mpi/vftr_mpi/bcast.c
+++ src/mpi/vftr_mpi/bcast.c
@@ -18,6 +18,7 @@
 
 #include <mpi.h>
 
+#include "self_profile.h"
 #include "thread_types.h"
 #include "threads.h"
 #include "threadstack_types.h"
@@ -38,6 +39,7 @@ int vftr_MPI_Bcast(void *buffer, int count, MPI_Datatype datatype,
    int retVal = PMPI_Bcast(buffer, count, datatype, root, comm);
    long long tend = vftr_get_runtime_usec();
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = tend;
    // in intracommunicators the expected behaviour is to
    // gather from root to all other processes in the communicator
@@ -63,6 +65,7 @@ int vftr_MPI_Bcast(void *buffer, int count, MPI_Datatype datatype,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
 
@@ -73,6 +76,7 @@ int vftr_MPI_Bcast_intercom(void *buffer, int count, MPI_Datatype datatype,
    int retVal = PMPI_Bcast(buffer, count, datatype, root, comm);
    long long tend = vftr_get_runtime_usec();
 
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = tend;
    // in intercommunicators the behaviour is more complicated
    // There are two groups A and B
@@ -111,5 +115,6 @@ int vftr_MPI_Bcast_intercom(void *buffer, int count, MPI_Datatype datatype,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
