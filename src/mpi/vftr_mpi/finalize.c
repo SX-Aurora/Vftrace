d835822df629dcdc4b5258cad5c342e259a8e0d7
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:21 +0200

    added self profiling to finalize.c

--- src/mpi/vftr_mpi/finalize.c
+++ src/mpi/vftr_mpi/finalize.c
@@ -18,15 +18,17 @@
 
 #include <mpi.h>
 
+#include "self_profile.h"
 #include "vftrace_state.h"
 #include "vftr_finalize.h"
 #include "mpiprofiling.h"
 #include "requests.h"
 
 int vftr_MPI_Finalize() {
-
+   SELF_PROFILE_START_FUNCTION;
    vftr_free_request_list(&vftrace.mpi_state);
    vftr_free_profiled_ranks_list(&vftrace.mpi_state);
+   SELF_PROFILE_END_FUNCTION;
    // it is neccessary to finalize vftrace here, in order to properly communicat stack ids
    // between processes. After MPI_Finalize communication between processes is prohibited
    vftr_finalize();
