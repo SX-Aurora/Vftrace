3da7a3e03eaf3bea34d24f229aeee87c57c5b2e6
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:22 +0200

    added self profiling to pcontrol.c

--- src/mpi/vftr_mpi/pcontrol.c
+++ src/mpi/vftr_mpi/pcontrol.c
@@ -18,16 +18,20 @@
 
 #include "vftrace_state.h"
 
+#include "self_profile.h"
 // vftrace internal routine to control the profiling level
 int vftr_MPI_Pcontrol(const int level) {
+   SELF_PROFILE_START_FUNCTION;
    // level == 0 Profiling is disabled
    // level == 1 Profiling is enabled at a normal default level of detail
    // lebel == 2 Buffers are flushed, which may be a no-op in some profilers
    // All other values have profile library defined effects and additional arguments
+   int retVal = 1;
    if ((level >=0) && (level <=2)) {
       vftrace.mpi_state.pcontrol_level = level;
-      return 0;
+      retVal = 0;
    }
 
-   return 1;
+   SELF_PROFILE_END_FUNCTION;
+   return retVal;
 }
