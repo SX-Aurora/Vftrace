41f45f9846a135798affe3dbe97100f5c9b3e5fa
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:22 +0200

    added self profiling to ireduce.c

--- src/mpi/vftr_mpi/ireduce.c
+++ src/mpi/vftr_mpi/ireduce.c
@@ -20,6 +20,7 @@
 
 #include <stdlib.h>
 
+#include "self_profile.h"
 #include "rank_translate.h"
 #include "thread_types.h"
 #include "threads.h"
@@ -39,6 +40,7 @@ int vftr_MPI_Ireduce(const void *sendbuf, void *recvbuf, int count,
    long long tstart = vftr_get_runtime_usec();
    int retVal = PMPI_Ireduce(sendbuf, recvbuf, count, datatype, op, root, comm,
                              request);
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    // in intracommunicators the expected behaviour is to
    // bcast from root to all other processes in the communicator
@@ -82,6 +84,7 @@ int vftr_MPI_Ireduce(const void *sendbuf, void *recvbuf, int count,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
 
@@ -92,6 +95,7 @@ int vftr_MPI_Ireduce_inplace(const void *sendbuf, void *recvbuf, int count,
    long long tstart = vftr_get_runtime_usec();
    int retVal = PMPI_Ireduce(sendbuf, recvbuf, count, datatype, op, root, comm,
                              request);
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    // in intracommunicators the expected behaviour is to
    // bcast from root to all other processes in the communicator
@@ -144,6 +148,7 @@ int vftr_MPI_Ireduce_inplace(const void *sendbuf, void *recvbuf, int count,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
 
@@ -153,6 +158,7 @@ int vftr_MPI_Ireduce_intercom(const void *sendbuf, void *recvbuf, int count,
    long long tstart = vftr_get_runtime_usec();
    int retVal = PMPI_Ireduce(sendbuf, recvbuf, count, datatype, op, root, comm,
                              request);
+   SELF_PROFILE_START_FUNCTION;
    long long t2start = vftr_get_runtime_usec();
    // in intercommunicators the behaviour is more complicated
    // There are two groups A and B
@@ -201,5 +207,6 @@ int vftr_MPI_Ireduce_intercom(const void *sendbuf, void *recvbuf, int count,
 
    vftr_accumulate_mpiprofiling_overhead(&(my_profile->mpiProf), t2end-t2start);
 
+   SELF_PROFILE_END_FUNCTION;
    return retVal;
 }
