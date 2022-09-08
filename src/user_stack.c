24cd5f6e52409156e775fc4774d64f9b69c3396b
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:39:52 +0200

    refined self profiling in user_stack.c

--- src/user_stack.c
+++ src/user_stack.c
@@ -1,12 +1,14 @@
 #include "thread_types.h"
 #include "stack_types.h"
 
+#include "self_profile.h"
 #include "vftrace_state.h"
 #include "threads.h"
 #include "threadstacks.h"
 #include "stacks.h"
 
 int vftrace_get_stack_string_length() {
+   SELF_PROFILE_START_FUNCTION;
    // Get the thread that called the function
    thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
    threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
@@ -14,11 +16,14 @@ int vftrace_get_stack_string_length() {
    //       whether to inherit the parentthreads stack + the function, or
    //       to inherit it as soon as a task is created. for non-OMP code the master
    //       thread is created with _init as lowest stacklist entry
-   return vftr_get_stack_string_length(vftrace.process.stacktree,
-                                       my_threadstack->stackID, false);
+   int length = vftr_get_stack_string_length(vftrace.process.stacktree,
+                                             my_threadstack->stackID, false);
+   SELF_PROFILE_END_FUNCTION;
+   return length;
 }
 
 char *vftrace_get_stack() {
+   SELF_PROFILE_START_FUNCTION;
    // Get the thread that called the function
    thread_t *my_thread = vftr_get_my_thread(&(vftrace.process.threadtree));
    threadstack_t *my_threadstack = vftr_get_my_threadstack(my_thread);
@@ -26,6 +31,8 @@ char *vftrace_get_stack() {
    //       whether to inherit the parentthreads stack + the function, or
    //       to inherit it as soon as a task is created. for non-OMP code the master
    //       thread is created with _init as lowest stacklist entry
-   return vftr_get_stack_string(vftrace.process.stacktree,
-                                my_threadstack->stackID, false);
+   char *stackstr = vftr_get_stack_string(vftrace.process.stacktree,
+                                          my_threadstack->stackID, false);
+   SELF_PROFILE_END_FUNCTION;
+   return stackstr;
 }
