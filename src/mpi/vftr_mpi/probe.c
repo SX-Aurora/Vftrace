cbe12ea1baf94a69151c0b092a27006c454e07a2
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-08 11:40:22 +0200

    added self profiling to probe.c

--- src/mpi/vftr_mpi/probe.c
+++ src/mpi/vftr_mpi/probe.c
@@ -18,8 +18,12 @@
 
 #include <mpi.h>
 
+#include "self_profile.h"
 int vftr_MPI_Probe(int source, int tag, MPI_Comm comm, MPI_Status *status) {
+   SELF_PROFILE_START_FUNCTION;
    // there is no timing needed but we keep the wrapper here
    // for the instrumentation
-   return PMPI_Probe(source, tag, comm, status);
+   int retVal = PMPI_Probe(source, tag, comm, status);
+   SELF_PROFILE_END_FUNCTION;
+   return retVal;
 }
