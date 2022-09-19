fe8f8463930da8dcd29d939cf63d79c35b7c3343
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-09-19 08:49:44 +0200

    added f08-wrapper for init_thread

---
+++ src/mpi/f08-wrapper/init_thread.F90
@@ -0,0 +1,34 @@
+! This file is part of Vftrace.
+!
+! Vftrace is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! Vftrace is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License along
+! with this program; if not, write to the Free Software Foundation, Inc.,
+! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#ifdef _MPI
+
+SUBROUTINE MPI_Init_thread_f08(required, provided, error)
+   USE vftr_mpi_init_thread_f082vftr_f08i, &
+      ONLY : vftr_mpi_init_thread_f082vftr
+   IMPLICIT NONE
+   INTEGER, INTENT(IN) :: required
+   INTEGER, INTENT(OUT) :: provided
+   INTEGER, OPTIONAL, INTENT(OUT) :: error
+   INTEGER :: tmperror
+
+   CALL vftr_mpi_init_thread_f082vftr(required, provided, tmperror)
+
+   IF (PRESENT(error)) error = tmperror
+
+END SUBROUTINE MPI_Init_thread_f08
+
+#endif
