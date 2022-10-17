7500c50e90e7e63278ade8fc5a69cc51d3420a5e
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-10-17 13:20:55 +0200

    added routines to add collated profiles

--- src/collated_profiling.h
+++ src/collated_profiling.h
@@ -7,6 +7,9 @@
 
 collated_profile_t vftr_new_collated_profile();
 
+collated_profile_t vftr_add_collated_profiles(collated_profile_t profA,
+                                              collated_profile_t profB);
+
 void vftr_collated_profile_free(collated_profile_t* profile_ptr);
 
 #endif
