cff4f888bae59c54821cbc8862cf34d0b655028b
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-11-07 15:23:52 +0100

    made config tools

--- tools/config_tools/vftrace_check_config.c
+++ tools/config_tools/vftrace_check_config.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
 #include "configuration_types.h"
+#include "configuration.h"
 #include "configuration_defaults.h"
-#include "configuration_print.h"
+#include "configuration_parse.h"
+#include "configuration_assert.h"
+#include "misc_utils.h"
 
 int main(int argc, char **argv) {
 
+   if (argc < 2) {
+      fprintf(stderr, "./vftrace_check_config <config.json>");
+      return 1;
+   }
+
    config_t config = vftr_set_config_default();
-   vftr_print_config(stdout, config, false);
+   char *config_string = vftr_read_file_to_string(argv[1]);
+   vftr_parse_config(config_string, &config);
+   free(config_string);
+   vftr_config_assert(stderr, config);
+   vftr_config_free(&config);
 }
