424e41690e4e9a15dab01608a36573aaf065d278
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-11-11 12:54:29 +0100

    removed no longer needed comments

--- src/internal_regions.c
+++ src/internal_regions.c
@@ -93,9 +93,6 @@ void vftr_internal_region_end(const char *name) {
       // if not recursive pop the function from the threads stacklist
       my_threadstack = vftr_threadstack_pop(&(my_thread->stacklist));
 
-      // TODO stack_t *my_new_stack = vftrace.process.stacktree.stacks+my_threadstack->stackID;
-      // TODO profile_t *my_new_profile = vftr_get_my_profile(my_new_stack, my_thread);
-
       // TODO Add accumulation of profiling data
       vftr_sample_function_exit(&(vftrace.sampling),
                                 *my_stack,
