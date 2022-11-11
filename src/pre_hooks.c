a5206aeade0bc875031b9b7304725e9c908b8570
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-11-11 10:21:30 +0100

    made pre-hook function name comparison case insensitive

--- src/pre_hooks.c
+++ src/pre_hooks.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include <string.h>
+#include <ctype.h>
 
 #include "vftrace_state.h"
 #include "symbols.h"
@@ -11,18 +12,28 @@
 void vftr_pre_hook_function_entry(void *func, void *call_site) {
    uintptr_t func_addr = (uintptr_t) func;
    char *func_name = vftr_get_name_from_address(vftrace.symboltable, func_addr);
-   // skip everything in the programs execution until
-   // main is called.
-   // There are c++ programs that execute a lot of inconsitently
-   // instrumented code before calling main.
-   // This skips all this, as it can break vftrace.
-   if (func_name != NULL && !strcmp(func_name, "main")) {
-      // assign the appropriate function hooks to handle sampling.
-      vftr_set_enter_func_hook(vftr_function_entry);
-      vftr_set_exit_func_hook(vftr_function_exit);
+   if (func_name != NULL) {
+      // Make the function name comparison case insensitive
+      // to capture fortran case insensitive MAIN symbols
+      // by converting the function name to lowercase
+      char *tmpstr = func_name;
+      while (*tmpstr != '\0') {
+         *tmpstr = tolower(*tmpstr);
+         tmpstr++;
+      }
+      // skip everything in the programs execution until
+      // main is called.
+      // There are c++ programs that execute a lot of inconsitently
+      // instrumented code before calling main.
+      // This skips all this, as it can break vftrace.
+      if (!strcmp(func_name, "main")) {
+         // assign the appropriate function hooks to handle sampling.
+         vftr_set_enter_func_hook(vftr_function_entry);
+         vftr_set_exit_func_hook(vftr_function_exit);
 
-      // execute the actual function entry hook.
-      vftr_function_entry(func, call_site);
+         // execute the actual function entry hook.
+         vftr_function_entry(func, call_site);
+      }
    }
 }
 
