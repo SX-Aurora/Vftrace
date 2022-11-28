5039fbcd5199af9ff0cbc949feeddfe361a5b74e
Felix Uhl <Felix.Uhl@emea.nec.com>
2022-11-28 07:43:29 +0100

    added header file

---
+++ src/veda/veda_callback_mem_cpy_HtoD.h
@@ -0,0 +1,12 @@
+#ifndef VEDA_CALLBACK_MEM_CPY_HTOD_H
+#define VEDA_CALLBACK_MEM_CPY_HTOD_H
+
+#include <stdlib.h>
+
+#include <veda.h>
+
+void vftr_veda_callback_mem_cpy_htod_enter(VEDAprofiler_data* data);
+
+void vftr_veda_callback_mem_cpy_htod_exit(VEDAprofiler_data* data);
+
+#endif
