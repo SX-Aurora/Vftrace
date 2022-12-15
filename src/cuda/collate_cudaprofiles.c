6f7151c6e45397c44a59e076c37a1cb745b6d23f
MeisterEule <christian.weiss@emea.nec.com>
2022-12-15 11:20:35 +0100

    Fix stack_t

--- src/cuda/collate_cudaprofiles.c
+++ src/cuda/collate_cudaprofiles.c
@@ -9,7 +9,7 @@ void vftr_collate_cudaprofiles (collated_stacktree_t *collstacktree_ptr,
   				 stacktree_t *stacktree_ptr,
  				 int myrank, int nranks, int *nremote_profiles) {
 	for (int istack = 0; istack < stacktree_ptr->nstacks; istack++) {
-		stack_t *stack = stacktree_ptr->stacks + istack;
+		vftr_stack_t *stack = stacktree_ptr->stacks + istack;
 		int i_collstack = stack->gid;
 		collated_stack_t *collstack = collstacktree_ptr->stacks + i_collstack;
                 // CUDA profiling is only supported for one thread (i_prof = 0).
