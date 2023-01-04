086fc8a7778db3e9887fd3a148ddf3ffbc1fe90e
MeisterEule <christian.weiss@emea.nec.com>
2023-01-04 12:12:40 +0100

    Fix Fortran MPI test

--- test/mpi/testscripts/derived_ftypes.sh
+++ test/mpi/testscripts/derived_ftypes.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+source ${srcdir}/../../environment/filenames.sh
+
 vftr_binary=derived_ftypes
 configfile=${vftr_binary}.json
 nprocs=2
+
+determine_bin_prefix $vftr_binary
 
 echo "{\"sampling\": {\"active\": true}}" > ${configfile}
 export VFTR_CONFIG=${configfile}
@@ -13,10 +17,10 @@ mpitype=MPI_DERIVED_TYPE
 
 for ivfd in $(seq 0 1 $(bc <<< "${nprocs}-1"));
 do
+   vfdfile=$(get_vfdfile_name ${vftr_binary} ${ivfd})
+   ../../../tools/vftrace_vfd_dump ${vfdfile}
 
-   ../../../tools/vftrace_vfd_dump ${vftr_binary}_${ivfd}.vfd
-
-   tmptype=$(../../../tools/vftrace_vfd_dump ${vftr_binary}_${ivfd}.vfd | \
+   tmptype=$(../../../tools/vftrace_vfd_dump ${vfdfile} | \
              awk '($2=="send" || $2=="recv") && $3!="end"{getline;print;}' | \
              sed 's/=/ /g;s/(/ /g' | \
              awk '{print $4}' | \
