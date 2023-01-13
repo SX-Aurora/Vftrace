e9baf400767c2129ea919692c496071d449442b4
MeisterEule <christian.weiss@emea.nec.com>
2023-01-13 12:01:28 +0100

    Fix makefile and tests

--- test/hwprof/calculator.sh
+++ test/hwprof/calculator.sh
@@ -4,4 +4,10 @@ source ${srcdir}/../environment/filenames.sh
 
 set -x
 
+test_name=calculator
+output_file=${test_name}.out
+ref_file=${srcdir}/ref_output/${output_file}
+
 ./calculator > calculator.out
+
+diff ${output_file} ${ref_file} || exit 1
