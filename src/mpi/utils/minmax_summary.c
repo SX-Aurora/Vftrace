3ab1104376cc67272c07b0cee8efaf3cd2c5df7b
MeisterEule <christian.weiss@emea.nec.com>
2023-01-30 13:54:04 +0100

    Write table with minimal and maximal times

---
+++ src/mpi/utils/minmax_summary.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include "vftrace_state.h"
+#include "table_types.h"
+#include "tables.h"
+#include "sorting.h"
+#include "collated_stack_types.h"
+
+void vftr_write_minmax_summary (FILE *fp, vftrace_t vftrace) {
+   collated_stack_t **sorted_stacks =
+      vftr_sort_collated_stacks_for_prof(vftrace.config, vftrace.process.collated_stacktree);
+
+   int nstacks = vftrace.process.collated_stacktree.nstacks;
+   table_t table = vftr_new_table();
+   vftr_table_set_nrows(&table, nstacks);      
+
+   int *stack_ids = (int*)malloc(nstacks * sizeof(int));
+   char **function_names = (char**)malloc(nstacks * sizeof(char*)); 
+   double *t_max_s = (double*)malloc(nstacks * sizeof(double));
+   double *t_min_s = (double*)malloc(nstacks * sizeof(double));
+   int *rank_max = (int*)malloc(nstacks * sizeof(int));
+   int *rank_min = (int*)malloc(nstacks * sizeof(int));
+   for (int i = 0; i < nstacks; i++) {
+      stack_ids[i] = i;
+      collated_stack_t *this_stack = sorted_stacks[i];
+      function_names[i] = this_stack->name;
+      t_max_s[i] = (double)this_stack->profile.callprof.max_time_nsec / 1e9;
+      t_min_s[i] = (double)this_stack->profile.callprof.min_time_nsec / 1e9;
+      rank_max[i] = this_stack->profile.callprof.max_on_rank;
+      rank_min[i] = this_stack->profile.callprof.min_on_rank;
+   }
+
+   vftr_table_add_column (&table, col_int, "STID", "%d", 'c', 'r', (void*)stack_ids);
+   vftr_table_add_column (&table, col_string, "Function", "%s", 'c', 'r', (void*)function_names);
+   vftr_table_add_column (&table, col_double, "t_max[s]", "%.3f", 'c', 'r', (void*)t_max_s);
+   vftr_table_add_column (&table, col_int, "rank", "%d", 'c', 'r', (void*)rank_max);
+   vftr_table_add_column (&table, col_double, "t_min[s]", "%.3f", 'c', 'r', (void*)t_min_s);
+   vftr_table_add_column (&table, col_int, "rank", "%d", 'c', 'r', (void*)rank_min);
+
+   vftr_print_table (fp, table);
+
+   free(stack_ids);
+   free(function_names);
+   free(t_max_s);   
+   free(t_min_s);
+   free(rank_max);
+   free(rank_min);
+}
---
+++ src/mpi/utils/minmax_summary.h
@@ -0,0 +1,8 @@
+#ifndef MINMAX_SUMMARY_H
+#define MINMAX_SUMMARY_H
+
+#include "vftrace_state.h"
+
+void vftr_write_minmax_summary (FILE *fp, vftrace_t vftrace);
+
+#endif
