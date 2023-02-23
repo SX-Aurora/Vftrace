6c7cda294268abe49e30646c2427aa675ad8c26b
MeisterEule <christian.weiss@emea.nec.com>
2023-02-23 13:20:32 +0100

    Suppress MCA warning

--- test/environment/set_openmpi.sh
+++ test/environment/set_openmpi.sh
@@ -11,3 +11,20 @@ if [ "x${OMPI_VERSION_LT5}" == "xYES" ]; then
 else
    export MPI_OPTS="--map-by :OVERSUBSCRIBE"
 fi
+
+# Some systems have been observed to show this error message:
+#
+# "A high-performance Open MPI point-to-point messaging module
+#  was unable to find any relevant network interfaces"
+#  [...]
+# Another transport will be used instead, although this may result in
+# lower performance.
+# 
+# This is an issue to be resolved within the MPI environment. As the
+# message indicates, this issue is not relevant for the functionality
+# tests, since it only affects the choice of the interconnect. However,
+# the warning message appears in stderr. Tests which redirect stderr
+# and compare it to a reference file will therefore fail. The following
+# environment variable switches off the warning.
+export OMPI_MCA_btl_base_warn_component_unused=0
+
