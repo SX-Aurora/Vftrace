d403ab9ae2417fa712a520286137cfd3a3dd43d6
MeisterEule <christian.weiss@emea.nec.com>
2023-11-29 11:13:41 +0100

    Add missing files

---
+++ src/logfiles/ranklogfile_stacklist.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "self_profile.h"
+#include "table_types.h"
+#include "stack_types.h"
+
+#include "stacks.h"
+#include "tables.h"
+
+void vftr_write_ranklogfile_global_stack_list(FILE *fp, stacktree_t stacktree) {
+   SELF_PROFILE_START_FUNCTION;
+   fprintf(fp, "\nGlobal call stacks:\n");
+
+   table_t table = vftr_new_table();
+   vftr_table_set_nrows(&table, stacktree.nstacks);
+   vftr_table_left_outline(&table, false);
+   vftr_table_right_outline(&table, false);
+   vftr_table_columns_separating_line(&table, false);
+
+   // first column with the StackIDs
+   int *IDs = (int*) malloc(stacktree.nstacks*sizeof(int));
+   for (int istack = 0; istack < stacktree.nstacks; istack++) {
+      IDs[istack] = istack;
+   }
+   vftr_table_add_column(&table, col_int, "STID", "STID%d", 'r', 'r', (void*) IDs);
+
+   // second column with the stack strings
+   char **stacks = (char**) malloc(stacktree.nstacks*sizeof(char*));
+   for (int istack = 0; istack < stacktree.nstacks; istack++) {
+      stacks[istack] = vftr_get_stack_string(stacktree, istack, false);
+   }
+   vftr_table_add_column(&table, col_string,
+                         "Call stack", "%s", 'l', 'l', (void*) stacks);
+
+   vftr_print_table(fp, table);
+   fprintf(fp, "\n");
+
+   vftr_table_free(&table);
+   free(IDs);
+   for (int istack = 0; istack < stacktree.nstacks; istack++) {
+      free(stacks[istack]);
+   }
+   free(stacks);
+   SELF_PROFILE_END_FUNCTION;
+}
---
+++ src/logfiles/ranklogfile_stacklist.h
@@ -0,0 +1,11 @@
+#ifndef LOGFILE_STACKLIST_H
+#define LOGFILE_STACKLIST_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "stack_types.h"
+
+void vftr_write_ranklogfile_global_stack_list(FILE *fp, stacktree_t stacktree);
+
+#endif
