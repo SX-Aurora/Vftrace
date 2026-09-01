03989271b2862e08835d75d72247c169faf779cd
MeisterEule <christian.weiss@emea.nec.com>
2026-09-01 10:38:07 +0200

    Add a boostrap script

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+git submodule update --init
+autoreconf -i
+sed -i \
+    -e 's/test x-L = "\$p"/test "\$p" = "-L"/' \
+    -e 's/test x-R = "\$p"/test "\$p" = "-R"/' \ 
+    -e 's/test "\$p" = "-R"; then/test "\$p" = "-R" ||\n          test "\$p" = "-l"; then/' \
+    configure
