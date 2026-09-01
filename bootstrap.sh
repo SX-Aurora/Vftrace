e6fe18cde333c6e8bfec878872eb7b33eb9f4869
MeisterEule <christian.weiss@emea.nec.com>
2026-09-01 10:49:02 +0200

    Remove errorneous whitespace

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,6 @@ git submodule update --init
 autoreconf -i
 sed -i \
     -e 's/test x-L = "\$p"/test "\$p" = "-L"/' \
-    -e 's/test x-R = "\$p"/test "\$p" = "-R"/' \ 
+    -e 's/test x-R = "\$p"/test "\$p" = "-R"/' \
     -e 's/test "\$p" = "-R"; then/test "\$p" = "-R" ||\n          test "\$p" = "-l"; then/' \
     configure
