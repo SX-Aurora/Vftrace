fc5a9f367773f91ac5ad4e4a60fd4b713c6609f3
MeisterEule <christian.weiss@emea.nec.com>
2026-09-15 11:04:23 +0200

    Remove global stacktree in another test

--- test/hwprof/collate_hwprofiles_parallel_2.c
+++ test/hwprof/collate_hwprofiles_parallel_2.c
@@ -19,29 +19,29 @@
 #include "hwprof_logfile.h"
 #include "hwprof_ranklogfile.h"
 
-stacktree_t dummy_stacktree;
 static uintptr_t base_addr = 123456;
 
-void vftr_init_dummy_stacktree (uint64_t t_call) {
-   dummy_stacktree = vftr_new_stacktree();
+stacktree_t vftr_init_dummy_stacktree (uint64_t t_call) {
+   stacktree_t dummy_stacktree = vftr_new_stacktree();
    char *rootname = "init";
    dummy_stacktree.stacks[0].hash = vftr_jenkins_murmur_64_hash (strlen(rootname), (uint8_t*)rootname);
    profile_t *profile = dummy_stacktree.stacks[0].profiling.profiles;
    vftr_accumulate_callprofiling(&(profile->callprof), 1, t_call);
+   return dummy_stacktree;
 }
 
-int get_stack_idx (uint64_t hash) {
+int get_stack_idx (stacktree_t *stacktree, uint64_t hash) {
    if (hash == 0) return 0;
-   for (int i = 0; i < dummy_stacktree.nstacks; i++) {
-      if (dummy_stacktree.stacks[i].hash == hash) return dummy_stacktree.stacks[i].lid;
+   for (int i = 0; i < stacktree->nstacks; i++) {
+      if (stacktree->stacks[i].hash == hash) return stacktree->stacks[i].lid;
    }
    return -1;
 }
 
-void vftr_register_dummy_call_stack (char *stackstring, uint64_t t_call) {
+void vftr_register_dummy_call_stack (stacktree_t *dummy_stacktree, char *stackstring, uint64_t t_call) {
    int slen = strlen(stackstring);
    uint64_t new_hash = vftr_jenkins_murmur_64_hash (slen, (uint8_t*)stackstring); 
-   int this_stack_idx = get_stack_idx (new_hash);
+   int this_stack_idx = get_stack_idx (dummy_stacktree, new_hash);
    bool stack_already_present = this_stack_idx >= 0;
    if (!stack_already_present) {
       char *s = (char*)malloc(slen*sizeof(char));
@@ -50,25 +50,25 @@ void vftr_register_dummy_call_stack (char *stackstring, uint64_t t_call) {
       char *stack_bottom = strtok(NULL, "");
 
       uint64_t hash_bottom = vftr_jenkins_murmur_64_hash (strlen(stack_bottom), (uint8_t*)stack_bottom);
-      int stack_idx_orig = get_stack_idx (hash_bottom);
-      int offset = dummy_stacktree.nstacks - 1;
-      this_stack_idx = vftr_new_stack(stack_idx_orig, &dummy_stacktree, stack_top, stack_top,
+      int stack_idx_orig = get_stack_idx (dummy_stacktree, hash_bottom);
+      int offset = dummy_stacktree->nstacks - 1;
+      this_stack_idx = vftr_new_stack(stack_idx_orig, dummy_stacktree, stack_top, stack_top,
                                       base_addr + offset, false);
   
-      dummy_stacktree.stacks[this_stack_idx].hash = new_hash;
+      dummy_stacktree->stacks[this_stack_idx].hash = new_hash;
    }
 
    int iprof = 0;
    if (!stack_already_present)
-      iprof = vftr_new_profile_in_list(0, &(dummy_stacktree.stacks[this_stack_idx]).profiling);
-   profile_t *profile = dummy_stacktree.stacks[this_stack_idx].profiling.profiles + iprof;
+      iprof = vftr_new_profile_in_list(0, &(dummy_stacktree->stacks[this_stack_idx]).profiling);
+   profile_t *profile = dummy_stacktree->stacks[this_stack_idx].profiling.profiles + iprof;
    vftr_accumulate_callprofiling(&(profile->callprof), 1, t_call);
 }
 
-void vftr_register_dummy_hwprof_stack (char *stackstring, long long *counters) {
+void vftr_register_dummy_hwprof_stack (stacktree_t *stacktree, char *stackstring, long long *counters) {
    int slen = strlen(stackstring);
    uint64_t new_hash = vftr_jenkins_murmur_64_hash (slen, (uint8_t*)stackstring); 
-   int this_stack_idx = get_stack_idx (new_hash);
+   int this_stack_idx = get_stack_idx (stacktree, new_hash);
    bool stack_already_registered = this_stack_idx >= 0;
    if (!stack_already_registered) {
       char *s = (char*)malloc(slen*sizeof(char));
@@ -77,25 +77,25 @@ void vftr_register_dummy_hwprof_stack (char *stackstring, long long *counters) {
       char *stack_bottom = strtok(NULL, "");
 
       uint64_t hash_bottom = vftr_jenkins_murmur_64_hash (strlen(stack_bottom), (uint8_t*)stack_bottom);
-      int stack_idx_orig = get_stack_idx (hash_bottom);
-      int offset = dummy_stacktree.nstacks - 1;
-      this_stack_idx = vftr_new_stack(stack_idx_orig, &dummy_stacktree, stack_top, stack_top,
+      int stack_idx_orig = get_stack_idx (stacktree, hash_bottom);
+      int offset = stacktree->nstacks - 1;
+      this_stack_idx = vftr_new_stack(stack_idx_orig, stacktree, stack_top, stack_top,
                                       base_addr + offset, false);
   
-      dummy_stacktree.stacks[this_stack_idx].hash = new_hash;
+      stacktree->stacks[this_stack_idx].hash = new_hash;
    }
 
    int iprof = 0;
    if (!stack_already_registered)
-      iprof = vftr_new_profile_in_list (0, &(dummy_stacktree.stacks[this_stack_idx].profiling));
-   profile_t *profile = dummy_stacktree.stacks[this_stack_idx].profiling.profiles + iprof;
+      iprof = vftr_new_profile_in_list (0, &(stacktree->stacks[this_stack_idx].profiling));
+   profile_t *profile = stacktree->stacks[this_stack_idx].profiling.profiles + iprof;
    vftr_accumulate_callprofiling(&(profile->callprof), 1, 0);
    vftr_accumulate_hwprofiling(&(profile->hwprof), counters, false);
 }
 
-void vftr_update_dummy_observables () {
-   int nstacks = dummy_stacktree.nstacks;
-   vftr_stack_t *stacks = dummy_stacktree.stacks; 
+void vftr_update_dummy_observables (stacktree_t *stacktree) {
+   int nstacks = stacktree->nstacks;
+   vftr_stack_t *stacks = stacktree->stacks; 
    for (int istack = 1; istack < nstacks; istack++) {
       vftr_stack_t *this_stack = stacks + istack;
       for (int iprof = 0; iprof < this_stack->profiling.nprofiles; iprof++) {
@@ -104,13 +104,6 @@ void vftr_update_dummy_observables () {
          hwprof->observables[0] = 2 * (double)(hwprof->counters_excl[0]);
       }
    }
-}
-
-stacktree_t vftr_get_dummy_stacktree () {
-   vftr_update_stacks_exclusive_time(&dummy_stacktree);
-   vftr_update_stacks_exclusive_counters (&dummy_stacktree);
-   vftr_update_dummy_observables();
-   return dummy_stacktree;
 }
 
 int main (int argc, char **argv) {
@@ -130,23 +123,26 @@ int main (int argc, char **argv) {
    vftrace.hwprof_state.observables[0].name = "dummy_obs";
    vftrace.hwprof_state.observables[0].unit = "dummy_unit";
 
-   vftr_init_dummy_stacktree (10);
+   stacktree_t dummy_stacktree = vftr_init_dummy_stacktree (10);
     
-   vftr_register_dummy_call_stack ("func0<init", 1);
-   vftr_register_dummy_call_stack ("hwfunc1<init", 2);
+   vftr_register_dummy_call_stack (&dummy_stacktree, "func0<init", 1);
+   vftr_register_dummy_call_stack (&dummy_stacktree, "hwfunc1<init", 2);
    long long c1[] = {1000};
-   vftr_register_dummy_hwprof_stack ("hwfunc1<init", c1);
+   vftr_register_dummy_hwprof_stack (&dummy_stacktree, "hwfunc1<init", c1);
    long long c2[] = {1500};
-   vftr_register_dummy_hwprof_stack ("hwfunc2<func0<init", c2);
+   vftr_register_dummy_hwprof_stack (&dummy_stacktree, "hwfunc2<func0<init", c2);
 
-   stacktree_t stacktree = vftr_get_dummy_stacktree();
-   collated_stacktree_t collated_stacktree = vftr_collate_stacks(&stacktree);
-   vftr_collate_profiles (&collated_stacktree, &stacktree);
+   vftr_update_stacks_exclusive_time (&dummy_stacktree);
+   vftr_update_stacks_exclusive_counters (&dummy_stacktree);
+   vftr_update_dummy_observables (&dummy_stacktree);
+
+   collated_stacktree_t collated_stacktree = vftr_collate_stacks(&dummy_stacktree);
+   vftr_collate_profiles (&collated_stacktree, &dummy_stacktree);
 
    for (int i = 0; i < nranks; i++) {
       if (myrank == i) {
         fprintf (stdout, "Ranklogfile for rank %d: \n", i);
-        vftr_write_ranklogfile_hwprof_obs_table(stdout, stacktree, vftrace.config);
+        vftr_write_ranklogfile_hwprof_obs_table(stdout, dummy_stacktree, vftrace.config);
       }
       fflush(stdout);
       PMPI_Barrier(MPI_COMM_WORLD);
